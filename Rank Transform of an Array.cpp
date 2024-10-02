c3dafcb90ef9239f6bb07a4cd9715022ae94ea4d
Harshit Gupta <harshitgupta3017@gmail.com>
2024-10-02 10:52:02 +0530

    Create Rank Transform of an Array.cpp

---
+++ Rank Transform of an Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> copy = arr;
+        sort(copy.begin(), copy.end());
+        unordered_map<int, int> mp;
+        int rank = 1;
+        for (const int& num: copy) {
+            if (mp.find(num) == mp.end()) mp[num] = rank++;
+        }
+        for (int i = 0; i < arr.size(); i++) arr[i] = mp[arr[i]];
+        return arr;
+    }
+};
