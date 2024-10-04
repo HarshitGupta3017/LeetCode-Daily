6280a9c0c8fd62297505fcb9219aeda54036c16f
Harshit Gupta <harshitgupta3017@gmail.com>
2024-10-04 12:11:19 +0530

    Create Divide Players Into Teams of Equal Skill.cpp

---
+++ Divide Players Into Teams of Equal Skill.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        sort(skill.begin(), skill.end());
+        int i = 0, j = skill.size() - 1;
+        int sum = skill[i] + skill[j];
+        long long res = 0;
+        while (i < j) {
+            if (skill[i] + skill[j] == sum) {
+                res += (skill[i] * skill[j]);
+                i++, j--;
+            } else return -1;
+        }
+        return res;
+    }
+};
