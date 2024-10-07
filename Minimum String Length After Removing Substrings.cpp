6af4df9e8e16b15390a358ccdb92da578b273523
Harshit Gupta <harshitgupta3017@gmail.com>
2024-10-07 16:13:54 +0530

    Create Minimum String Length After Removing Substrings.cpp

---
+++ Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(const auto& c: s) {
+            if (!st.empty()) {
+                if ((st.top() == 'A' && c == 'B') || (st.top() == 'C' && c == 'D')) {
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(c);
+        }
+        return st.size();
+    }
+};
