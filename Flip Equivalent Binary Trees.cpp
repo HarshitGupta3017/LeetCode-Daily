3dd32b458662b92623b8883d1410fc8b003c15ad
Harshit Gupta <harshitgupta3017@gmail.com>
2024-10-24 10:00:37 +0530

    Update Flip Equivalent Binary Trees.cpp

--- Flip Equivalent Binary Trees.cpp
+++ Flip Equivalent Binary Trees.cpp
@@ -12,10 +12,17 @@
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        // simple ques, just need to check if 2 trees are equivalent or not
+        // if both roots are null then they are equivalent
         if (!root1 && !root2) return true;
+        // if any one is null and other is root then not equi
         if (!root1 || !root2) return false;
+        // if value is not same then again not equivalent
         if (root1->val != root2->val) return false;
+        // now we will just check before flipping and after it
+        // without will be same as currently
         bool withoutflip = flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
+        // but after flip left node of first tree will be right of second one and vice versa
         bool withflip = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
         return withoutflip || withflip;
     }
