17fff2fb0d69201499cb0200622e84c0333d9376
Harshit Gupta <harshitgupta3017@gmail.com>
2024-11-09 13:34:48 +0530

    Update Minimum Array End.cpp

--- Minimum Array End.cpp
+++ Minimum Array End.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         long long res = x;
-        for (int i = 1; i < n; i++) res = (res + 1) | x;
+        // for getting a number through which if we do binary AND operation we get x
+        // we need to do binary OR of a number with x to get that unique number
+        for (int i = 1; i < n; i++) {
+            res = (res + 1) | x;
+        }
         return res;
     }
 };
