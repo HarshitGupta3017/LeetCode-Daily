d4e9fb1a4531f3c66062c5de71cc229ac9cc8828
Harshit Gupta <harshitgupta3017@gmail.com>
2024-12-02 12:17:38 +0530

    Update Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp

--- Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
+++ Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        istringstream st(sentence);
+        // Hello guyz, iam back with a simple question
+        // we can simply make stream of words using istringstream - inbuilt method in cpp
+        istringstream stream(sentence);
         string word;
-        int index = 1;
-        while (st >> word) {
+        int index = 1; // in question its stated that there is 1-based indexing i.e. index starts with 1
+        while (stream >> word) {
+            // we can check each word now if it startswith searchWord or not
             if (word.substr(0, searchWord.size()) == searchWord) return index;
             index++;
         }
