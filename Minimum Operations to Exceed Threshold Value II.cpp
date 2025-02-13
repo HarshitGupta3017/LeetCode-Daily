7eb42bed38d14eeb26a619cb98288424c7a274f3
Harshit Gupta <harshitgupta3017@gmail.com>
2025-02-13 10:54:05 +0530

    Create Minimum Operations to Exceed Threshold Value II.cpp

---
+++ Minimum Operations to Exceed Threshold Value II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long, vector<long>, greater<long>> pq(nums.begin(), nums.end());
+        int count = 0;
+        while (!pq.empty() && pq.top() < k) {
+            long smallest = pq.top();
+            pq.pop();
+            long secondSmallest = pq.top();
+            pq.pop();
+            pq.push(smallest * 2 + secondSmallest);
+            count++;
+        }
+        return count;
+    }
+};
