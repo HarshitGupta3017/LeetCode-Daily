e69f45352b775604a114f1b29882ba1f2ef58c11
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-09 14:32:55 +0530

    Update Alternating Groups II.py

--- Alternating Groups II/Alternating Groups II.py
+++ Alternating Groups II/Alternating Groups II.py
@@ -1,1 +1,18 @@
-// Solution for Alternating Groups II in PY
+# Solution for Alternating Groups II in PY
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        n = len(colors)
+        N = n + (k - 1)
+        newColors = colors + colors[:k - 1]
+        i, j, result = 0, 1, 0
+        while j < N:
+            if newColors[j] == newColors[j - 1]:
+                i = j
+                j += 1
+                continue
+            if j - i + 1 == k:
+                result += 1
+                i += 1
+            j += 1
+        return result
