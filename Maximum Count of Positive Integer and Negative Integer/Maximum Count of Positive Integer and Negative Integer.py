ec55fb1c5cc12f818996b5a4fb64e7c88d4228a6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-12 20:48:39 +0530

    Update Maximum Count of Positive Integer and Negative Integer.py

--- Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.py
+++ Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.py
@@ -1,1 +1,7 @@
 # Solution for Maximum Count of Positive Integer and Negative Integer in PY
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        pos = len(nums) - bisect.bisect_left(nums, 1)
+        neg = bisect.bisect_right(nums, -1)
+        return max(pos, neg)
