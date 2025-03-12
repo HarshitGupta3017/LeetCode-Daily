7bb043b1d4498b9d11a1e8c7861946bc6935508c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-12 20:48:12 +0530

    Update Maximum Count of Positive Integer and Negative Integer.cpp

--- Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.cpp
+++ Maximum Count of Positive Integer and Negative Integer/Maximum Count of Positive Integer and Negative Integer.cpp
@@ -1,1 +1,10 @@
 // Solution for Maximum Count of Positive Integer and Negative Integer in CPP
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = nums.end() - lower_bound(nums.begin(), nums.end(), 1);
+        int neg = upper_bound(nums.begin(), nums.end(), -1) - nums.begin();
+        return max(pos, neg);
+    }
+};
