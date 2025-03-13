5167d45263869ce956f6bde718a8f9c1fb722084
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-13 12:11:14 +0530

    Update Zero Array Transformation II.py

--- Zero Array Transformation II/Zero Array Transformation II.py
+++ Zero Array Transformation II/Zero Array Transformation II.py
@@ -1,1 +1,33 @@
 # Solution for Zero Array Transformation II in PY
+
+class Solution:
+
+    def checkDiffArray(self, nums: List[int], queries: List[List[int]], k: int, n: int, q: int) -> bool:
+        diff = [0] * n
+        for i in range(k + 1):
+            l, r, x = queries[i]
+            diff[l] += x
+            if r + 1 < n:
+                diff[r + 1] -= x
+        
+        cumSum = 0
+        for i in range(n):
+            cumSum += diff[i]
+            diff[i] = cumSum
+            if nums[i] - diff[i] > 0:
+                return False
+        return True
+
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        n, q = len(nums), len(queries)
+        if all(x == 0 for x in nums): 
+            return 0
+        l, r, k = 0, q - 1, -1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if self.checkDiffArray(nums, queries, mid, n, q):
+                k = mid + 1
+                r = mid - 1
+            else:
+                l = mid + 1
+        return k
