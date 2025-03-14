6f743d32596a64fa32249027e705beca1df72127
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-14 10:55:05 +0530

    Update Maximum Candies Allocated to K Children.py

--- Maximum Candies Allocated to K Children/Maximum Candies Allocated to K Children.py
+++ Maximum Candies Allocated to K Children/Maximum Candies Allocated to K Children.py
@@ -1,1 +1,17 @@
 # Solution for Maximum Candies Allocated to K Children in PY
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        l, r, res = 1, max(candies), 0
+        while l <= r:
+            mid = l + (r - l) // 2
+            count = 0
+            for c in candies:
+                count += (c // mid)
+                if count >= k: break
+            if count >= k:
+                res = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
