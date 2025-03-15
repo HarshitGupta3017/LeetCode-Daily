5900d5c7f9a41c9f8fac8a6f012e431c17b1cca4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-15 15:47:32 +0530

    Update House Robber IV.cpp

--- House Robber IV/House Robber IV.cpp
+++ House Robber IV/House Robber IV.cpp
@@ -1,1 +1,33 @@
 // Solution for House Robber IV in CPP
+
+class Solution {
+public:
+
+    bool isPossible(vector<int>& nums, int mid, int k, int n) {
+        int house = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= mid) {
+                house++; // included
+                i++; // moved to i + 2
+            }
+        }
+        return house >= k; // got atleast k houses
+    }
+
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = *min_element(nums.begin(), nums.end());
+        int r = *max_element(nums.begin(), nums.end());
+        int res = r;
+        while (l <= r) {
+            int mid = l + (r - l) / 2; // capability
+            if (isPossible(nums, mid, k, n)) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return res;
+    }
+};
