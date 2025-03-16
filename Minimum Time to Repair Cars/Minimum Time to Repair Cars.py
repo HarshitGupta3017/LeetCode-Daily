d962d3a04c12c75c5e3dc65d2ac2e30002806c05
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-16 15:29:32 +0530

    Update Minimum Time to Repair Cars.py

--- Minimum Time to Repair Cars/Minimum Time to Repair Cars.py
+++ Minimum Time to Repair Cars/Minimum Time to Repair Cars.py
@@ -1,1 +1,21 @@
 # Solution for Minimum Time to Repair Cars in PY
+
+class Solution:
+
+    def isPossible(self, ranks: List[int], cars: int, mid: int) -> bool:
+        carsFixed = 0
+        for i in range(len(ranks)):
+            carsFixed += isqrt(mid // ranks[i])
+        return carsFixed >= cars
+
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        maxR = max(ranks)
+        l, r, res = 1, maxR * cars * cars, -1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if self.isPossible(ranks, cars, mid):
+                res = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res
