5b59d13729831082dfae5aa7916ec19d0bf5fc24
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-17 10:11:03 +0530

    Update Divide Array Into Equal Pairs.cpp

--- Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.cpp
+++ Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.cpp
@@ -1,1 +1,13 @@
 // Solution for Divide Array Into Equal Pairs in CPP
+
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for (const auto& num: nums) mp[num]++;
+        for (const auto& it: mp) {
+            if (it.second % 2 != 0) return false;
+        }
+        return true;
+    }
+};
