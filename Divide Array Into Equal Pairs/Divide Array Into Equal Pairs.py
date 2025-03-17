f0f9bb0c88ea326461a9c66da8d0c94fca657fb8
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-17 10:10:36 +0530

    Update Divide Array Into Equal Pairs.py

--- Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.py
+++ Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.py
@@ -1,1 +1,11 @@
 # Solution for Divide Array Into Equal Pairs in PY
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        mp = {}
+        for num in nums:
+            mp[num] = mp.get(num, 0) + 1
+        for it in mp:
+            if mp[it] % 2 != 0:
+                return False
+        return True
