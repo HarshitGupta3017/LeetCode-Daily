ea79ae10b66dcb29427422a2f3a03b1c0e23c532
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-17 10:10:48 +0530

    Update Divide Array Into Equal Pairs.java

--- Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.java
+++ Divide Array Into Equal Pairs/Divide Array Into Equal Pairs.java
@@ -1,1 +1,12 @@
 // Solution for Divide Array Into Equal Pairs in JAVA
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int num: nums) mp.put(num, mp.getOrDefault(num, 0) + 1);
+        for (Map.Entry<Integer, Integer> it: mp.entrySet()) {
+            if (it.getValue() % 2 != 0) return false;
+        }
+        return true;
+    }
+}
