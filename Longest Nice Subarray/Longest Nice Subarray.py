514f23a96bbaac6f0de95ece1f6fbcc82f4304c5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-18 10:53:58 +0530

    Update Longest Nice Subarray.py

--- Longest Nice Subarray/Longest Nice Subarray.py
+++ Longest Nice Subarray/Longest Nice Subarray.py
@@ -1,1 +1,13 @@
 # Solution for Longest Nice Subarray in PY
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        n, i, j, mask, res = len(nums), 0, 0, 0, 1
+        while j < n:
+            while mask & nums[j] != 0:
+                mask ^= nums[i]
+                i += 1
+            res = max(res, j - i + 1)
+            mask |= nums[j]
+            j += 1
+        return res
