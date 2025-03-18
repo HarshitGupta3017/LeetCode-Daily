66fe292b1389619b6f9c8b39c142a4497c00be72
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-18 10:53:43 +0530

    Update Longest Nice Subarray.java

--- Longest Nice Subarray/Longest Nice Subarray.java
+++ Longest Nice Subarray/Longest Nice Subarray.java
@@ -1,1 +1,16 @@
 // Solution for Longest Nice Subarray in JAVA
+
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length;
+        int i = 0, j = 0, mask = 0, res = 1;
+        while (j < n) {
+            while ((mask & nums[j]) != 0){
+                mask ^= nums[i++];
+            }
+            res = Math.max(res, j - i + 1);
+            mask |= nums[j++];
+        }
+        return res;
+    }
+}
