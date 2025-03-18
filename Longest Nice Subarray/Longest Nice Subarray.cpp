abd26b06830bdd2b004b49d710a64ce361f8db1a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-18 10:53:30 +0530

    Update Longest Nice Subarray.cpp

--- Longest Nice Subarray/Longest Nice Subarray.cpp
+++ Longest Nice Subarray/Longest Nice Subarray.cpp
@@ -1,1 +1,17 @@
 // Solution for Longest Nice Subarray in CPP
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 0, mask = 0, res = 1;
+        while (j < n) {
+            while ((mask & nums[j]) != 0){
+                mask ^= nums[i++];
+            }
+            res = max(res, j - i + 1);
+            mask |= nums[j++];
+        }
+        return res;
+    }
+};
