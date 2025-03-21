a0c20461625775dc0c69085cdd19ddb0647066b1
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-21 12:20:11 +0530

    Update Find All Possible Recipes from Given Supplies.py

--- Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.py
+++ Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.py
@@ -1,1 +1,18 @@
 # Solution for Find All Possible Recipes from Given Supplies in PY
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        st = set(supplies)
+        res = []
+        found = True
+        while found:
+            found = False
+            for i in range(len(recipes)):
+                if recipes[i] in st:
+                    continue
+                canMake = True
+                if all(ingr in st for ingr in ingredients[i]):
+                    st.add(recipes[i])
+                    res.append(recipes[i])
+                    found = True
+        return res
