4b6d54aad2924c9640e0b27f57e7296c717f6950
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-21 12:20:45 +0530

    Update Find All Possible Recipes from Given Supplies.cpp

--- Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.cpp
+++ Find All Possible Recipes from Given Supplies/Find All Possible Recipes from Given Supplies.cpp
@@ -1,1 +1,29 @@
 // Solution for Find All Possible Recipes from Given Supplies in CPP
+
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_set<string> st(supplies.begin(), supplies.end());
+        vector<string> res;
+        bool found = true;
+        while (found) {
+            found = false;
+            for (int i = 0; i < recipes.size(); i++) {
+                if (st.count(recipes[i])) continue;
+                bool canMake = true;
+                for (const string& ingr: ingredients[i]) {
+                    if (st.count(ingr) == 0) {
+                        canMake = false;
+                        break;
+                    }
+                }
+                if (canMake) {
+                    st.insert(recipes[i]);
+                    res.push_back(recipes[i]);
+                    found = true;
+                }
+            }
+        }
+        return res;
+    }
+};
