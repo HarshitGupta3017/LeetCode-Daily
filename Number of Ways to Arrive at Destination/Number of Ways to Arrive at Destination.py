d04c566428ea94e72ae0b040b5796f4c095d5688
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-23 13:53:11 +0530

    Update Number of Ways to Arrive at Destination.py

--- Number of Ways to Arrive at Destination/Number of Ways to Arrive at Destination.py
+++ Number of Ways to Arrive at Destination/Number of Ways to Arrive at Destination.py
@@ -1,1 +1,30 @@
 # Solution for Number of Ways to Arrive at Destination in PY
+
+class Solution:
+    MOD = 10**9 + 7
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        adj = defaultdict(list)
+        for u, v, time in roads:
+            adj[u].append([v, time])
+            adj[v].append([u, time])
+
+        pq = []
+        res = [sys.maxsize] * n
+        pathCount = [0] * n
+        res[0] = 0
+        pathCount[0] = 1
+        heapq.heappush(pq, (0, 0))
+        
+        while pq:
+            currTime, currNode = heapq.heappop(pq)
+            if currTime > res[currNode]:
+                continue
+            for ngbr, roadTime in adj[currNode]:
+                if currTime + roadTime < res[ngbr]:
+                    res[ngbr] = currTime + roadTime
+                    heapq.heappush(pq, (res[ngbr], ngbr))
+                    pathCount[ngbr] = pathCount[currNode]    
+                elif currTime + roadTime == res[ngbr]:
+                    pathCount[ngbr] = (pathCount[ngbr] + pathCount[currNode]) % self.MOD
+
+        return pathCount[n - 1]
