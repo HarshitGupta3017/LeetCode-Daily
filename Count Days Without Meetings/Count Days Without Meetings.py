af44e8810d0fed1e5b9f7c7e7c7f161ab07e5648
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-24 11:24:00 +0530

    Update Count Days Without Meetings.py

--- Count Days Without Meetings/Count Days Without Meetings.py
+++ Count Days Without Meetings/Count Days Without Meetings.py
@@ -1,1 +1,13 @@
 # Solution for Count Days Without Meetings in PY
+
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort(key = lambda x: x[0])
+        res, merge_end = 0, 0
+        for start, end in meetings:
+            if start > merge_end + 1:
+                res += (start - merge_end - 1)
+            merge_end = max(merge_end, end)
+        if merge_end < days:
+            res += days - merge_end
+        return res
