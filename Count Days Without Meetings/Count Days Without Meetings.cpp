0e46325bf35634d19e132bb674a2fb54d645fe47
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-24 11:23:32 +0530

    Update Count Days Without Meetings.cpp

--- Count Days Without Meetings/Count Days Without Meetings.cpp
+++ Count Days Without Meetings/Count Days Without Meetings.cpp
@@ -1,1 +1,21 @@
 // Solution for Count Days Without Meetings in CPP
+
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int res = 0;
+        int merge_end = 0;
+        for (const auto& meeting: meetings) {
+            int start = meeting[0], end = meeting[1];
+            if (start > merge_end + 1) {
+                res += (start - merge_end - 1);
+            }
+            merge_end = max(merge_end, end);
+        }
+        if (merge_end < days) {
+            res += days - merge_end;
+        }
+        return res;
+    }
+};
