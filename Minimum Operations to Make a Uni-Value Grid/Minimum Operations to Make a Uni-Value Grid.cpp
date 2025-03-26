411c3ad93e1a816d696039a5f16523601070cd64
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-26 13:52:35 +0530

    Update Minimum Operations to Make a Uni-Value Grid.cpp

--- Minimum Operations to Make a Uni-Value Grid/Minimum Operations to Make a Uni-Value Grid.cpp
+++ Minimum Operations to Make a Uni-Value Grid/Minimum Operations to Make a Uni-Value Grid.cpp
@@ -1,1 +1,24 @@
 // Solution for Minimum Operations to Make a Uni-Value Grid in CPP
+
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> nums;
+        for (const auto& row: grid) {
+            for (const auto& col: row) {
+                nums.push_back(col);
+            }
+        }
+        int remainder = nums[0] % x;
+        for (const auto& num: nums) {
+            if (num % x != remainder) return -1;
+        }
+        sort(nums.begin(), nums.end());
+        int ops = 0;
+        int median = nums[nums.size() / 2];
+        for (const auto& num: nums) {
+            ops += abs(num - median) / x;
+        }
+        return ops;
+    }
+};
