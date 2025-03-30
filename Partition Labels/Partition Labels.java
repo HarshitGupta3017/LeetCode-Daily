db913fdcdbbc8a6e61465d5359863020754d991c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-30 11:17:48 +0530

    Update Partition Labels.java

--- Partition Labels/Partition Labels.java
+++ Partition Labels/Partition Labels.java
@@ -1,1 +1,22 @@
 // Solution for Partition Labels in JAVA
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+        int[] mp = new int[26];
+        for (int i = 0; i < n; i++) {
+            mp[s.charAt(i) - 'a'] = i;
+        }
+        int i = 0, start = 0, end = 0;
+        List<Integer> result = new ArrayList<>();
+        while (i < n) {
+            end = Math.max(end, mp[s.charAt(i) - 'a']);
+            if (i == end) {
+                result.add(end - start + 1);
+                start = end + 1;
+            }
+            i++;
+        }
+        return result;
+    }
+}
