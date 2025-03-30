9c6af67dcbb2927c768aacae378b05ee3528ecf0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-30 11:18:00 +0530

    Update Partition Labels.cpp

--- Partition Labels/Partition Labels.cpp
+++ Partition Labels/Partition Labels.cpp
@@ -1,1 +1,23 @@
 // Solution for Partition Labels in CPP
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.length();
+        vector<int> mp(26, -1);
+        for (int i = 0; i < n; i++) {
+            mp[s[i] - 'a'] = i;
+        }
+        int i = 0, start = 0, end = 0;
+        vector<int> result;
+        while (i < n) {
+            end = max(end, mp[s[i] - 'a']);
+            if (i == end) {
+                result.push_back(end - start + 1);
+                start = end + 1;
+            }
+            i++;
+        }
+        return result;
+    }
+};
