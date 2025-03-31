00b342fe7ce73ed926a24bfdd4d5573ddaacc567
Harshit Gupta <harshitgupta3017@gmail.com>
2025-03-31 12:16:07 +0530

    Update Put Marbles in Bags.cpp

--- Put Marbles in Bags/Put Marbles in Bags.cpp
+++ Put Marbles in Bags/Put Marbles in Bags.cpp
@@ -1,1 +1,19 @@
 // Solution for Put Marbles in Bags in CPP
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        vector<int> pairSum(n - 1, 0);
+        for (int i = 0; i < n - 1; i++) {
+            pairSum[i] = weights[i] + weights[i + 1];
+        }
+        sort(pairSum.begin(), pairSum.end());
+        long long minSum = 0, maxSum = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minSum += pairSum[i];
+            maxSum += pairSum[n - 1 - i - 1];
+        }
+        return maxSum - minSum;
+    }
+};
