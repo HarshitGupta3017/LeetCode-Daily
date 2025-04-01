a54d484908e25858f46a467a6eb5f53b47a80ec9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-01 12:50:15 +0530

    Update Solving Questions With Brainpower.py

--- Solving Questions With Brainpower/Solving Questions With Brainpower.py
+++ Solving Questions With Brainpower/Solving Questions With Brainpower.py
@@ -1,1 +1,18 @@
 # Solution for Solving Questions With Brainpower in PY
+
+class Solution:
+
+    def solve(self, i: int, q: List[List[int]], t: List[int], n: int) -> int:
+        if i >= n:
+            return 0
+        if t[i] != -1: 
+            return t[i]
+        taken = q[i][0] + self.solve(i + q[i][1] + 1, q, t, n)
+        not_taken = self.solve(i + 1, q, t, n)
+        t[i] = max(taken, not_taken)
+        return t[i]
+
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        t = [-1] * (n + 1)
+        return self.solve(0, questions, t, n)
