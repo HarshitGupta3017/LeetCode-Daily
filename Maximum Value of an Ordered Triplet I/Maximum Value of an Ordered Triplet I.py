64e1d9f3ece91d4c81262c03e524679bd7c72ab9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-02 11:13:25 +0530

    Update Maximum Value of an Ordered Triplet I.py

--- Maximum Value of an Ordered Triplet I/Maximum Value of an Ordered Triplet I.py
+++ Maximum Value of an Ordered Triplet I/Maximum Value of an Ordered Triplet I.py
@@ -1,1 +1,15 @@
 # Solution for Maximum Value of an Ordered Triplet I in PY
+
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxPrefix = nums[0]
+        maxSuffix = [0] * n
+        maxSuffix[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            maxSuffix[i] = max(maxSuffix[i + 1], nums[i])
+        maxVal = 0
+        for j in range(1, n - 1):
+            maxVal = max(maxVal, (maxPrefix - nums[j]) * maxSuffix[j + 1])
+            maxPrefix = max(maxPrefix, nums[j])
+        return maxVal
