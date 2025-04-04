1037fc1e66b90561dc9432e29c1468edfbffbace
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-04 16:11:15 +0530

    Update Lowest Common Ancestor of Deepest Leaves.cpp

--- Lowest Common Ancestor of Deepest Leaves/Lowest Common Ancestor of Deepest Leaves.cpp
+++ Lowest Common Ancestor of Deepest Leaves/Lowest Common Ancestor of Deepest Leaves.cpp
@@ -1,1 +1,33 @@
 // Solution for Lowest Common Ancestor of Deepest Leaves in CPP
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    pair<int, TreeNode*> solve(TreeNode* root) {
+        if (!root) return {0, NULL};
+        auto l = solve(root->left);
+        auto r = solve(root->right);
+        if (l.first == r.first) {
+            return {l.first + 1, root};
+        } else if (l.first > r.first) {
+            return {l.first + 1, l.second};
+        } else {
+            return {r.first + 1, r.second};
+        }
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return solve(root).second;
+    }
+};
