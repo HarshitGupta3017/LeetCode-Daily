2ea47f9a340393a759e034965c437efbdefb8257
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-05 11:48:23 +0530

    Update Sum of All Subset XOR Totals.py

--- Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.py
+++ Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.py
@@ -1,1 +1,8 @@
 # Solution for Sum of All Subset XOR Totals in PY
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res |= num
+        return res << (len(nums) - 1)
