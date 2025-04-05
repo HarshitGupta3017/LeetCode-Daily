4dd3e7f135fb263b330f557143a3ec51a460620a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-05 11:48:53 +0530

    Update Sum of All Subset XOR Totals.cpp

--- Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.cpp
+++ Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.cpp
@@ -1,1 +1,10 @@
 // Solution for Sum of All Subset XOR Totals in CPP
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int res = 0;
+        for (const int& num: nums) res |= num;
+        return res << (nums.size() - 1);
+    }
+};
