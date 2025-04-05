18650e169c888b745fe882f14c4f69466bf8ea85
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-05 11:48:35 +0530

    Update Sum of All Subset XOR Totals.java

--- Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.java
+++ Sum of All Subset XOR Totals/Sum of All Subset XOR Totals.java
@@ -1,1 +1,9 @@
 // Solution for Sum of All Subset XOR Totals in JAVA
+
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int res = 0;
+        for (int num: nums) res |= num;
+        return res << (nums.length - 1);
+    }
+}
