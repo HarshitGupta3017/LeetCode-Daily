09c7be3c9845416363987851512ed5acb7ce8fd6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-08 11:17:38 +0530

    Update Minimum Number of Operations to Make Elements in Array Distinct.java

--- Minimum Number of Operations to Make Elements in Array Distinct/Minimum Number of Operations to Make Elements in Array Distinct.java
+++ Minimum Number of Operations to Make Elements in Array Distinct/Minimum Number of Operations to Make Elements in Array Distinct.java
@@ -1,1 +1,12 @@
 // Solution for Minimum Number of Operations to Make Elements in Array Distinct in JAVA
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        boolean[] seen = new boolean[101];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (seen[nums[i]]) return i / 3 + 1;
+            seen[nums[i]] = true;
+        }
+        return 0;
+    }
+}
