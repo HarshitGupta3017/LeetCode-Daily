f3e5d27a1c58102ae8b4d4ac2f5aaf8693b89143
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-09 13:43:21 +0530

    Update Minimum Operations to Make Array Values Equal to K.java

--- Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.java
+++ Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.java
@@ -1,1 +1,12 @@
 // Solution for Minimum Operations to Make Array Values Equal to K in JAVA
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Set<Integer> st = new HashSet<>();
+        for (int num: nums) {
+            if (num < k) return -1;
+            else if (num > k) st.add(num);
+        }
+        return st.size();
+    }
+}
