3dd2c3e8cefc551c6989b9dcaa7f0ee64250a1cc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-09 13:43:07 +0530

    Update Minimum Operations to Make Array Values Equal to K.cpp

--- Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.cpp
+++ Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.cpp
@@ -1,1 +1,13 @@
 // Solution for Minimum Operations to Make Array Values Equal to K in CPP
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> st;
+        for (const auto& num: nums) {
+            if (num < k) return -1;
+            else if (num > k) st.insert(num);
+        }
+        return st.size();
+    }
+};
