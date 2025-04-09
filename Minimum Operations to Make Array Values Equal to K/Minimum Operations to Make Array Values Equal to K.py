ae6bd672e1c61d016ae71071f6fefdfc055b6bdf
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-09 13:43:33 +0530

    Update Minimum Operations to Make Array Values Equal to K.py

--- Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.py
+++ Minimum Operations to Make Array Values Equal to K/Minimum Operations to Make Array Values Equal to K.py
@@ -1,1 +1,11 @@
 # Solution for Minimum Operations to Make Array Values Equal to K in PY
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        st = set()
+        for num in nums:
+            if num < k:
+                return -1
+            elif num > k:
+                st.add(num)
+        return len(st)
