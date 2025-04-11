6512de8e48a6c23d824b19c235ea32a798452ebd
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-11 11:23:58 +0530

    Update Count Symmetric Integers.py

--- Count Symmetric Integers/Count Symmetric Integers.py
+++ Count Symmetric Integers/Count Symmetric Integers.py
@@ -1,1 +1,16 @@
 # Solution for Count Symmetric Integers in PY
+
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        count = 0
+        for num in range(low, high + 1):
+            if num < 100 and num % 11 == 0:
+                count += 1
+            elif 1000 <= num <= 9999:
+                first = num // 1000
+                second = (num // 100) % 10
+                third = (num // 10) % 10
+                fourth = num % 10
+                if first + second == third + fourth:
+                    count += 1
+        return count
