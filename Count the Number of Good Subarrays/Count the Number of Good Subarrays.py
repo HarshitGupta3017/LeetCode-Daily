703a0c6868b5b96df72cc6b7f1d8982f2d107ed5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-16 10:28:29 +0530

    Update Count the Number of Good Subarrays.py

--- Count the Number of Good Subarrays/Count the Number of Good Subarrays.py
+++ Count the Number of Good Subarrays/Count the Number of Good Subarrays.py
@@ -1,1 +1,17 @@
 # Solution for Count the Number of Good Subarrays in PY
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        i, j, res, pairs = 0, 0, 0, 0
+        mp = {}
+        n = len(nums)
+        while j < n:
+            pairs += mp.get(nums[j], 0)
+            mp[nums[j]] = mp.get(nums[j], 0) + 1
+            while pairs >= k:
+                res += n - j
+                mp[nums[i]] -= 1
+                pairs -= mp[nums[i]]
+                i += 1
+            j += 1
+        return res
