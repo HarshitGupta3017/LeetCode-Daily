7c0cf8ea5201c5843bbf49a66f235353318f1356
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-19 12:13:36 +0530

    Update Count the Number of Fair Pairs.py

--- Count the Number of Fair Pairs/Count the Number of Fair Pairs.py
+++ Count the Number of Fair Pairs/Count the Number of Fair Pairs.py
@@ -1,1 +1,11 @@
 # Solution for Count the Number of Fair Pairs in PY
+
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        res = 0
+        for i in range(len(nums)):
+            x = bisect_left(nums, lower - nums[i], i + 1)
+            y = bisect_right(nums, upper - nums[i], i + 1)
+            res += (y - x)
+        return res
