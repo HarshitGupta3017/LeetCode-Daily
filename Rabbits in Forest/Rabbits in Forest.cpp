ddceb842867ee1cff27b203437afb47d80f07394
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-20 12:17:40 +0530

    Update Rabbits in Forest.cpp

--- Rabbits in Forest/Rabbits in Forest.cpp
+++ Rabbits in Forest/Rabbits in Forest.cpp
@@ -1,1 +1,18 @@
 // Solution for Rabbits in Forest in CPP
+
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int, int> count;
+        int result = 0;
+        for (int ans : answers) {
+            count[ans]++;
+        }
+        for (auto& [k, freq] : count) {
+            int groupSize = k + 1;
+            int groups = ceil((double) freq / groupSize);
+            result += groups * groupSize;
+        }
+        return result;
+    }
+};
