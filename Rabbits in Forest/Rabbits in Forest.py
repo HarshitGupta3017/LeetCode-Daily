cc04978bf0401cfdae2070684760551d70ffcd16
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-20 12:18:00 +0530

    Update Rabbits in Forest.py

--- Rabbits in Forest/Rabbits in Forest.py
+++ Rabbits in Forest/Rabbits in Forest.py
@@ -1,1 +1,11 @@
 # Solution for Rabbits in Forest in PY
+
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        count = Counter(answers)
+        result = 0
+        for k, freq in count.items():
+            group_size = k + 1
+            groups = math.ceil(freq / group_size)
+            result += groups * group_size
+        return result
