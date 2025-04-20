720e0ff0565e531cb3ac80767cb9f68f4a9596a1
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-20 12:17:49 +0530

    Update Rabbits in Forest.java

--- Rabbits in Forest/Rabbits in Forest.java
+++ Rabbits in Forest/Rabbits in Forest.java
@@ -1,1 +1,19 @@
 // Solution for Rabbits in Forest in JAVA
+
+class Solution {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> count = new HashMap<>();
+        int result = 0;
+        for (int ans : answers) {
+            count.put(ans, count.getOrDefault(ans, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            int k = entry.getKey();
+            int freq = entry.getValue();
+            int groupSize = k + 1;
+            int groups = (int) Math.ceil((double) freq / groupSize);
+            result += groups * groupSize;
+        }
+        return result;
+    }
+}
