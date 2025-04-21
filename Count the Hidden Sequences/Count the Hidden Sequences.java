51963e7bd683adb838a091cc51731d0e7cffd047
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-21 15:12:15 +0530

    Update Count the Hidden Sequences.java

--- Count the Hidden Sequences/Count the Hidden Sequences.java
+++ Count the Hidden Sequences/Count the Hidden Sequences.java
@@ -1,1 +1,14 @@
 // Solution for Count the Hidden Sequences in JAVA
+
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        int curr = 0, mn = 0, mx = 0;
+        for (int d: differences) {
+            curr += d;
+            mn = Math.min(mn, curr);
+            mx = Math.max(mx, curr);
+            if ((upper - mx) - (lower - mn) + 1 <= 0) return 0;
+        }
+        return (upper - mx) - (lower - mn) + 1;
+    }
+}
