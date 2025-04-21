e75edf7c2b8586a9fe3fa04e2fc93f289156a84b
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-21 15:12:26 +0530

    Update Count the Hidden Sequences.cpp

--- Count the Hidden Sequences/Count the Hidden Sequences.cpp
+++ Count the Hidden Sequences/Count the Hidden Sequences.cpp
@@ -1,1 +1,15 @@
 // Solution for Count the Hidden Sequences in CPP
+
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int curr = 0, mn = 0, mx = 0;
+        for (int d: differences) {
+            curr += d;
+            mn = min(mn, curr);
+            mx = max(mx, curr);
+            if ((upper - mx) - (lower - mn) + 1 <= 0) return 0;
+        }
+        return (upper - mx) - (lower - mn) + 1;
+    }
+};
