1c2436d4721f62025b931922767869f093897277
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-21 15:12:00 +0530

    Update Count the Hidden Sequences.py

--- Count the Hidden Sequences/Count the Hidden Sequences.py
+++ Count the Hidden Sequences/Count the Hidden Sequences.py
@@ -1,1 +1,12 @@
 # Solution for Count the Hidden Sequences in PY
+
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        curr, mn, mx = 0, 0, 0
+        for d in differences:
+            curr += d
+            mn = min(mn, curr)
+            mx = max(mx, curr)
+            if (upper - mx) - (lower - mn) + 1 <= 0:
+                return 0
+        return (upper - mx) - (lower - mn) + 1
