d52a362e3a2d62a0c85d929419174899fde46201
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-23 11:03:37 +0530

    Update Count Largest Group.py

--- Count Largest Group/Count Largest Group.py
+++ Count Largest Group/Count Largest Group.py
@@ -1,1 +1,23 @@
 # Solution for Count Largest Group in PY
+
+class Solution:
+
+    def findDS(self, num: int) -> int:
+        sum = 0
+        while num != 0:
+            sum += num % 10
+            num //= 10
+        return sum
+
+    def countLargestGroup(self, n: int) -> int:
+        mp = defaultdict(int)
+        count, maxSize = 0, 0
+        for num in range(1, n + 1):
+            digitSum = self.findDS(num)
+            mp[digitSum] += 1
+            if mp[digitSum] == maxSize:
+                count += 1
+            elif mp[digitSum] > maxSize:
+                maxSize = mp[digitSum]
+                count = 1
+        return count
