7a76adde73185b4d94480f0ce98b6b8ccc1405ac
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-23 11:03:23 +0530

    Update Count Largest Group.java

--- Count Largest Group/Count Largest Group.java
+++ Count Largest Group/Count Largest Group.java
@@ -1,1 +1,29 @@
 // Solution for Count Largest Group in JAVA
+
+class Solution {
+
+    private int findDS(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public int countLargestGroup(int n) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int count = 0, maxSize = 0;
+        for (int num = 1; num <= n; num++) {
+            int digitSum = findDS(num);
+            mp.put(digitSum, mp.getOrDefault(digitSum, 0) + 1);
+            if (mp.get(digitSum) == maxSize) {
+                count++;
+            } else if (mp.get(digitSum) > maxSize) {
+                maxSize = mp.get(digitSum);
+                count = 1;
+            }
+        }
+        return count;
+    }
+}
