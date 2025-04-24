ad542b248f8dfb9b61ebb4d8b548017194e700bf
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-24 11:08:58 +0530

    Update Count Complete Subarrays in an Array.java

--- Count Complete Subarrays in an Array/Count Complete Subarrays in an Array.java
+++ Count Complete Subarrays in an Array/Count Complete Subarrays in an Array.java
@@ -1,1 +1,21 @@
 // Solution for Count Complete Subarrays in an Array in JAVA
+
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        Set<Integer> st = new HashSet<>();
+        for (int num : nums) st.add(num);
+        int n = nums.length, i = 0, j = 0, res = 0, unique_ch = st.size();
+        Map<Integer, Integer> mp = new HashMap<>();
+        while (j < n) {
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            while (mp.size() == unique_ch) {
+                res += n - j;
+                mp.put(nums[i], mp.get(nums[i]) - 1);
+                if (mp.get(nums[i]) == 0) mp.remove(nums[i]);
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+}
