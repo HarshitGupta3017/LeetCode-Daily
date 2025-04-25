e3de94e942c633a7cf76c666e59c01a437351b5e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-25 15:13:35 +0530

    Update Count of Interesting Subarrays.java

--- Count of Interesting Subarrays/Count of Interesting Subarrays.java
+++ Count of Interesting Subarrays/Count of Interesting Subarrays.java
@@ -1,1 +1,18 @@
 // Solution for Count of Interesting Subarrays in JAVA
+
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+        int prefix = 0;
+        long res = 0;
+        Map<Integer, Long> freq = new HashMap<>();
+        freq.put(0, 1L);
+        for (int num: nums) {
+            if (num % modulo == k) prefix++;
+            int modPrefix = prefix % modulo;
+            int prevPrefix = (modPrefix - k + modulo) % modulo;
+            if (freq.containsKey(prevPrefix)) res += freq.get(prevPrefix);
+            freq.put(modPrefix, freq.getOrDefault(modPrefix, 0L) + 1);
+        }
+        return res;
+    }
+}
