f80a5e56cf6aa361bfc2a55e633d2f529996de35
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-25 15:13:22 +0530

    Update Count of Interesting Subarrays.cpp

--- Count of Interesting Subarrays/Count of Interesting Subarrays.cpp
+++ Count of Interesting Subarrays/Count of Interesting Subarrays.cpp
@@ -1,1 +1,19 @@
 // Solution for Count of Interesting Subarrays in CPP
+
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        int prefix = 0;
+        long long res = 0;
+        unordered_map<int, long long> freq;
+        freq[0] = 1;
+        for (int num: nums) {
+            if (num % modulo == k) prefix++;
+            int modPrefix = prefix % modulo;
+            int prevPrefix = (modPrefix - k + modulo) % modulo;
+            if (freq.count(prevPrefix)) res += freq[prevPrefix];
+            freq[modPrefix]++;
+        }
+        return res;
+    }
+};
