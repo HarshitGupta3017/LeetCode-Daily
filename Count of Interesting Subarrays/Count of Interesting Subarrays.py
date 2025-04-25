8be08bfb7ff7e9f0cead2f008de0e4ab71544567
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-25 15:13:52 +0530

    Update Count of Interesting Subarrays.py

--- Count of Interesting Subarrays/Count of Interesting Subarrays.py
+++ Count of Interesting Subarrays/Count of Interesting Subarrays.py
@@ -1,1 +1,16 @@
 # Solution for Count of Interesting Subarrays in PY
+
+class Solution:
+    def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
+        prefix, res = 0, 0
+        freq = defaultdict(int)
+        freq[0] = 1
+        for num in nums:
+            if num % modulo == k:
+                prefix += 1
+            modPrefix = prefix % modulo
+            prevPrefix = (modPrefix - k + modulo) % modulo
+            if prevPrefix in freq:
+                res += freq[prevPrefix]
+            freq[modPrefix] += 1
+        return res
