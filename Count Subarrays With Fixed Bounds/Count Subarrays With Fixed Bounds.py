0057dce35e431c83817b0affbc6e2bf0a4626bb6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-26 12:22:08 +0530

    Update Count Subarrays With Fixed Bounds.py

--- Count Subarrays With Fixed Bounds/Count Subarrays With Fixed Bounds.py
+++ Count Subarrays With Fixed Bounds/Count Subarrays With Fixed Bounds.py
@@ -1,1 +1,16 @@
 # Solution for Count Subarrays With Fixed Bounds in PY
+
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        minKidx, maxKidx, culpritIdx, res = -1, -1, -1, 0
+        for i in range(len(nums)):
+            if (nums[i] < minK or nums[i] > maxK):
+                culpritIdx = i
+            if (nums[i] == minK):
+                minKidx = i
+            if (nums[i] == maxK):
+                maxKidx = i
+            smaller = min(maxKidx, minKidx)
+            temp = smaller - culpritIdx
+            res += 0 if (temp <= 0) else temp
+        return res
