10d06988c2bd0593690de652af12ca3b1a73d176
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-26 12:21:56 +0530

    Update Count Subarrays With Fixed Bounds.java

--- Count Subarrays With Fixed Bounds/Count Subarrays With Fixed Bounds.java
+++ Count Subarrays With Fixed Bounds/Count Subarrays With Fixed Bounds.java
@@ -1,1 +1,21 @@
 // Solution for Count Subarrays With Fixed Bounds in JAVA
+
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int minKidx = -1, maxKidx = -1, culpritIdx = -1;
+        int n = nums.length;
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < minK || nums[i] > maxK) 
+                culpritIdx = i;
+            if (nums[i] == minK)
+                minKidx = i;
+            if (nums[i] == maxK)
+                maxKidx = i;
+            long smaller = Math.min(maxKidx, minKidx);
+            long temp = smaller - culpritIdx;
+            res += (temp <= 0) ? 0 : temp;
+        }
+        return res;
+    }
+}
