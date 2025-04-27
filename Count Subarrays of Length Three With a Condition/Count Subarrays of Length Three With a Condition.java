42938f58c0ef8945b4afdcfc06dc45e412c334af
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-27 21:49:32 +0530

    Update Count Subarrays of Length Three With a Condition.java

--- Count Subarrays of Length Three With a Condition/Count Subarrays of Length Three With a Condition.java
+++ Count Subarrays of Length Three With a Condition/Count Subarrays of Length Three With a Condition.java
@@ -1,1 +1,15 @@
 // Solution for Count Subarrays of Length Three With a Condition in JAVA
+
+class Solution {
+
+    public int countSubarrays(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            if (nums[i] == (nums[i - 1] + nums[i + 1]) * 2) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
