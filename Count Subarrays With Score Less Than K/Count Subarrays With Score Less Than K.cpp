9351f42a0b2b5bd14bbb6a54228162bed174ed26
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-28 12:33:28 +0530

    Update Count Subarrays With Score Less Than K.cpp

--- Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.cpp
+++ Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.cpp
@@ -1,1 +1,20 @@
 // Solution for Count Subarrays With Score Less Than K in CPP
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        long long res = 0, sum = 0;
+        while (j < n) {
+            sum += nums[j];
+            while (i <= j && sum * (j - i + 1) >= k) {
+                sum -= nums[i];
+                i++;
+            }
+            res += (j - i + 1);
+            j++;
+        }
+        return res;
+    }
+};
