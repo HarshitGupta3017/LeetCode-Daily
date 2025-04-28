106d680836362354651d91f4a76f16bcf5ee9de6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-28 12:33:49 +0530

    Update Count Subarrays With Score Less Than K.py

--- Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.py
+++ Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.py
@@ -1,1 +1,13 @@
 # Solution for Count Subarrays With Score Less Than K in PY
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        n, i, j, res, sum = len(nums), 0, 0, 0, 0
+        while j < n:
+            sum += nums[j]
+            while i <= j and sum * (j - i + 1) >= k:
+                sum -= nums[i]
+                i += 1
+            res += (j - i + 1)
+            j += 1
+        return res
