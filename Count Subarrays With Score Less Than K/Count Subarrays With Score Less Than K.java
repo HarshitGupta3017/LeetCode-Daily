2347e544f3a399422f2ce150ffc7bb67fec9f06a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-28 12:33:38 +0530

    Update Count Subarrays With Score Less Than K.java

--- Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.java
+++ Count Subarrays With Score Less Than K/Count Subarrays With Score Less Than K.java
@@ -1,1 +1,19 @@
 // Solution for Count Subarrays With Score Less Than K in JAVA
+
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        int n = nums.length;
+        int i = 0, j = 0;
+        long res = 0, sum = 0;
+        while (j < n) {
+            sum += nums[j];
+            while (i <= j && sum * (j - i + 1) >= k) {
+                sum -= nums[i];
+                i++;
+            }
+            res += (j - i + 1);
+            j++;
+        }
+        return res;
+    }
+}
