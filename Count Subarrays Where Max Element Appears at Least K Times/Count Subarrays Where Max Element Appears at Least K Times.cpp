4aa45dc3083529cce09d4398c8a3ecff8f09591d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-29 12:34:13 +0530

    Update Count Subarrays Where Max Element Appears at Least K Times.cpp

--- Count Subarrays Where Max Element Appears at Least K Times/Count Subarrays Where Max Element Appears at Least K Times.cpp
+++ Count Subarrays Where Max Element Appears at Least K Times/Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -1,1 +1,21 @@
 // Solution for Count Subarrays Where Max Element Appears at Least K Times in CPP
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int i = 0, j = 0, n = nums.size();
+        long long res = 0;
+        int mx = *max_element(nums.begin(), nums.end());
+        int count = 0;
+        while (j < n) {
+            if (nums[j] == mx) count++;
+            while (count >= k) {
+                res += (n - j);
+                if (nums[i] == mx) count--;
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+};
