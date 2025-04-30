94804ac9af7b461d611e3c70e690dcf97ab50120
Harshit Gupta <harshitgupta3017@gmail.com>
2025-04-30 14:38:16 +0530

    Update Find Numbers with Even Number of Digits.cpp

--- Find Numbers with Even Number of Digits/Find Numbers with Even Number of Digits.cpp
+++ Find Numbers with Even Number of Digits/Find Numbers with Even Number of Digits.cpp
@@ -1,1 +1,14 @@
 // Solution for Find Numbers with Even Number of Digits in CPP
+
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for (int num: nums) {
+            if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000){
+                count++;
+            }
+        }
+        return count;
+    }
+};
