94e1efbd1c2e4c0d7d753330fd6da0b414bb1215
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-03 12:21:17 +0530

    Update Minimum Domino Rotations For Equal Row.cpp

--- Minimum Domino Rotations For Equal Row/Minimum Domino Rotations For Equal Row.cpp
+++ Minimum Domino Rotations For Equal Row/Minimum Domino Rotations For Equal Row.cpp
@@ -1,1 +1,29 @@
 // Solution for Minimum Domino Rotations For Equal Row in CPP
+
+class Solution {
+public:
+
+    int find(vector<int>& tops, vector<int>& bottoms, int val) {
+        int swapTop = 0, swapBot = 0;
+        for (int i = 0; i < tops.size(); i++) {
+            if (tops[i] != val && bottoms[i] != val) {
+                return -1;
+            } else if (tops[i] != val) {
+                swapTop++;
+            } else if (bottoms[i] != val) {
+                swapBot++;
+            }
+        }
+        return min(swapTop, swapBot);
+    }
+
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int res = INT_MAX;
+        for (int val = 1; val <= 6; val++) {
+            int steps = find(tops, bottoms, val);
+            if (steps != -1) 
+                res = min(res, steps);
+        }
+        return res == INT_MAX ? -1 : res;
+    }
+};
