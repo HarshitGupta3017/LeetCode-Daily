075e855837bde47342704075c19fafa511288c63
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-03 12:20:59 +0530

    Update Minimum Domino Rotations For Equal Row.java

--- Minimum Domino Rotations For Equal Row/Minimum Domino Rotations For Equal Row.java
+++ Minimum Domino Rotations For Equal Row/Minimum Domino Rotations For Equal Row.java
@@ -1,1 +1,28 @@
 // Solution for Minimum Domino Rotations For Equal Row in JAVA
+
+class Solution {
+
+    private int find(int[] tops, int[] bottoms, int val) {
+        int swapTop = 0, swapBot = 0;
+        for (int i = 0; i < tops.length; i++) {
+            if (tops[i] != val && bottoms[i] != val) {
+                return -1;
+            } else if (tops[i] != val) {
+                swapTop++;
+            } else if (bottoms[i] != val) {
+                swapBot++;
+            }
+        }
+        return Math.min(swapTop, swapBot);
+    }
+
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int res = Integer.MAX_VALUE;
+        for (int val = 1; val <= 6; val++) {
+            int steps = find(tops, bottoms, val);
+            if (steps != -1) 
+                res = Math.min(res, steps);
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+}
