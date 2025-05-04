6937915581c3d4072428e099b2619226524290df
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-04 12:03:04 +0530

    Update Number of Equivalent Domino Pairs.java

--- Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.java
+++ Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.java
@@ -1,1 +1,14 @@
 // Solution for Number of Equivalent Domino Pairs in JAVA
+
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int res = 0;
+        for (int[] d: dominoes) {
+            int key = Math.min(d[0], d[1]) * 10 + Math.max(d[0], d[1]);
+            res += mp.getOrDefault(key, 0);
+            mp.put(key, mp.getOrDefault(key, 0) + 1);
+        }
+        return res; 
+    }
+}
