f6fc899eb42df604e81c2552f759969020e9e2f5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-04 12:02:54 +0530

    Update Number of Equivalent Domino Pairs.py

--- Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.py
+++ Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.py
@@ -1,1 +1,11 @@
 # Solution for Number of Equivalent Domino Pairs in PY
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        mp = defaultdict(int)
+        res = 0
+        for d in dominoes:
+            key = min(d[0], d[1]) * 10 + max(d[0], d[1])
+            res += mp[key]
+            mp[key] += 1
+        return res
