468bbf536b12ace02f5228744935f6e10de66592
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-04 12:03:15 +0530

    Update Number of Equivalent Domino Pairs.cpp

--- Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.cpp
+++ Number of Equivalent Domino Pairs/Number of Equivalent Domino Pairs.cpp
@@ -1,1 +1,15 @@
 // Solution for Number of Equivalent Domino Pairs in CPP
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> mp;
+        int res = 0;
+        for (const auto& d: dominoes) {
+            int key = min(d[0], d[1]) * 10 + max(d[0], d[1]);
+            res += mp[key];
+            mp[key]++;
+        }
+        return res;
+    }
+};
