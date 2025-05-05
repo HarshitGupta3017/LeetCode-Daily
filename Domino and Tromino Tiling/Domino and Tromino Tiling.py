48a5e973525b6f7459f226f17ae3e3cde9607ea1
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-05 11:33:51 +0530

    Update Domino and Tromino Tiling.py

--- Domino and Tromino Tiling/Domino and Tromino Tiling.py
+++ Domino and Tromino Tiling/Domino and Tromino Tiling.py
@@ -1,1 +1,16 @@
 # Solution for Domino and Tromino Tiling in PY
+
+class Solution:
+    def numTilings(self, n: int) -> int:
+        M = 1000000007
+        if n == 1 or n == 2:
+            return n
+        if n == 3:
+            return 5
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] % M + dp[i - 3] % M) % M
+        return dp[n]
