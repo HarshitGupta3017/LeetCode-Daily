6c2f07d0b1cb1e5832f54b06cc41a078cf6ce13f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-05 11:34:04 +0530

    Update Domino and Tromino Tiling.java

--- Domino and Tromino Tiling/Domino and Tromino Tiling.java
+++ Domino and Tromino Tiling/Domino and Tromino Tiling.java
@@ -1,1 +1,15 @@
 // Solution for Domino and Tromino Tiling in JAVA
+
+class Solution {
+    public int numTilings(int n) {
+        int M = 1000000007;
+        if (n == 1 || n == 2) return n;
+        if (n == 3) return 5;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        for (int i = 4; i <= n; i++) dp[i] = (2 * dp[i - 1] % M + dp[i - 3] % M) % M;
+        return dp[n];  
+    }
+}
