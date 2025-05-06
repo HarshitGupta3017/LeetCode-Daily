5a4a8752972e2e20b78c2707369431599910797f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-06 15:27:43 +0530

    Update Build Array from Permutation.java

--- Build Array from Permutation/Build Array from Permutation.java
+++ Build Array from Permutation/Build Array from Permutation.java
@@ -1,1 +1,14 @@
 // Solution for Build Array from Permutation in JAVA
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] + (nums[nums[i]] % n) * n;
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] / n;
+        }
+        return nums;
+    }
+}
