7bfc91583c054f23a23aa4684f7861a7284546d2
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-11 14:01:46 +0530

    Update Three Consecutive Odds.java

--- Three Consecutive Odds/Three Consecutive Odds.java
+++ Three Consecutive Odds/Three Consecutive Odds.java
@@ -1,1 +1,16 @@
 // Solution for Three Consecutive Odds in JAVA
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int oddCount = 0;
+        for (int num: arr) {
+            if (num % 2 == 1) 
+                oddCount++;
+            else 
+                oddCount = 0;
+            if (oddCount == 3)
+                return true;
+        }
+        return false;
+    }
+}
