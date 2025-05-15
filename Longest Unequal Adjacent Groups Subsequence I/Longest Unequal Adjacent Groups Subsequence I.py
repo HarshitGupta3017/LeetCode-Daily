9d3af50db4fcd5e0054c017ee8a2658b928e20ee
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-15 10:25:43 +0530

    Update Longest Unequal Adjacent Groups Subsequence I.py

--- Longest Unequal Adjacent Groups Subsequence I/Longest Unequal Adjacent Groups Subsequence I.py
+++ Longest Unequal Adjacent Groups Subsequence I/Longest Unequal Adjacent Groups Subsequence I.py
@@ -1,1 +1,9 @@
 # Solution for Longest Unequal Adjacent Groups Subsequence I in PY
+
+class Solution:
+    def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+        res = []
+        for i in range(len(words)):
+            if i == 0 or groups[i] != groups[i - 1]:
+                res.append(words[i])
+        return res
