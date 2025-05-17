f64912c4ec7da0e00954bdf4f9d8931443a47280
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-17 14:16:03 +0530

    Update Sort Colors.java

--- Sort Colors/Sort Colors.java
+++ Sort Colors/Sort Colors.java
@@ -1,1 +1,23 @@
 // Solution for Sort Colors in JAVA
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int i = 0, j = 0, k = nums.length - 1;
+        while (j <= k) {
+            if (nums[j] == 1) {
+                j++;
+            } else if (nums[j] == 2) {
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                k--;
+            } else {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
+            }
+        }   
+    }
+}
