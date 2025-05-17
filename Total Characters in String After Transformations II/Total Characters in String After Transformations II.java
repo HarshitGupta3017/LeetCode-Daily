8b9224816e5e90b305c5b6db97ee19ec19a8b0c8
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-17 16:08:38 +0530

    Update Total Characters in String After Transformations II.java

--- Total Characters in String After Transformations II/Total Characters in String After Transformations II.java
+++ Total Characters in String After Transformations II/Total Characters in String After Transformations II.java
@@ -1,79 +1,71 @@
 // Solution for Total Characters in String After Transformations II in JAVA
 
 class Solution {
-
-    private static final int MOD = (int) 1e9 + 7;
-    private static final int L = 26;
-
-    private static class Mat {
-
-        int[][] a = new int[L][L];
-
-        Mat() {}
-
-        Mat(Mat copyFrom) {
-            for (int i = 0; i < L; i++) {
-                System.arraycopy(copyFrom.a[i], 0, this.a[i], 0, L);
-            }
-        }
-
-        Mat mul(Mat other) {
-            Mat result = new Mat();
-            for (int i = 0; i < L; i++) {
-                for (int j = 0; j < L; j++) {
-                    for (int k = 0; k < L; k++) {
-                        result.a[i][j] = (int) ((result.a[i][j] +
-                                (long) this.a[i][k] * other.a[k][j]) %
-                            MOD);
-                    }
-                }
-            }
-            return result;
-        }
-    }
-
-    /* identity matrix */
-    private Mat I() {
-        Mat m = new Mat();
-        for (int i = 0; i < L; i++) {
-            m.a[i][i] = 1;
-        }
-        return m;
-    }
-
-    /* matrix exponentiation by squaring */
-    private Mat quickmul(Mat x, int y) {
-        Mat ans = I();
-        Mat cur = new Mat(x);
-        while (y > 0) {
-            if ((y & 1) == 1) {
-                ans = ans.mul(cur);
-            }
-            cur = cur.mul(cur);
-            y >>= 1;
-        }
-        return ans;
-    }
+    static final int MOD = 1_000_000_007;
+    static final int SIZE = 26;
 
     public int lengthAfterTransformations(String s, int t, List<Integer> nums) {
-        Mat T = new Mat();
-        for (int i = 0; i < L; i++) {
-            for (int j = 1; j <= nums.get(i); j++) {
-                T.a[(i + j) % L][i] = 1;
+        // Step 1: Initial frequency vector
+        int[] freq = new int[SIZE];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        // Step 2: Build transformation matrix T
+        int[][] T = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int add = 1; add <= nums.get(i); add++) {
+                T[(i + add) % SIZE][i]++;
             }
         }
 
-        Mat res = quickmul(T, t);
-        int[] f = new int[L];
-        for (char ch : s.toCharArray()) {
-            f[ch - 'a']++;
-        }
-        int ans = 0;
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < L; j++) {
-                ans = (int) ((ans + (long) res.a[i][j] * f[j]) % MOD);
+        // Step 3: Compute T^t
+        int[][] result = matrixExponentiation(T, t);
+
+        // Step 4: Multiply result * freq
+        int[] finalFreq = new int[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                finalFreq[i] = (int) ((finalFreq[i] + 1L * result[i][j] * freq[j]) % MOD);
             }
         }
+
+        // Step 5: Sum final frequencies
+        int ans = 0;
+        for (int val : finalFreq) {
+            ans = (ans + val) % MOD;
+        }
+
         return ans;
+    }
+
+    private int[][] matrixExponentiation(int[][] base, int exponent) {
+        int[][] identity = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            identity[i][i] = 1;
+        }
+
+        if (exponent == 0) return identity;
+
+        int[][] half = matrixExponentiation(base, exponent / 2);
+        int[][] result = matrixMultiplication(half, half);
+
+        if (exponent % 2 == 1) {
+            result = matrixMultiplication(result, base);
+        }
+
+        return result;
+    }
+
+    private int[][] matrixMultiplication(int[][] A, int[][] B) {
+        int[][] C = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                for (int k = 0; k < SIZE; k++) {
+                    C[i][j] = (int) ((C[i][j] + 1L * A[i][k] * B[k][j]) % MOD);
+                }
+            }
+        }
+        return C;
     }
 }
