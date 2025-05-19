d07954807a95c529d8c75b09a58b5a5eb0e0fa07
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-19 11:04:03 +0530

    Update Type of Triangle.java

--- Type of Triangle/Type of Triangle.java
+++ Type of Triangle/Type of Triangle.java
@@ -1,1 +1,13 @@
 // Solution for Type of Triangle in JAVA
+
+class Solution {
+    public String triangleType(int[] nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0])
+            return "none";
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral";
+        else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
+            return "scalene";
+        return "isosceles";
+    }
+}
