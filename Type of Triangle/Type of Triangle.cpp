585c24614ff3e18bddc568283440ace272b3a0a7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-19 11:03:46 +0530

    Update Type of Triangle.cpp

--- Type of Triangle/Type of Triangle.cpp
+++ Type of Triangle/Type of Triangle.cpp
@@ -1,1 +1,14 @@
 // Solution for Type of Triangle in CPP
+
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0])
+            return "none";
+        if (nums[0] == nums[1] && nums[1] == nums[2])
+            return "equilateral";
+        else if (nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
+            return "scalene";
+        return "isosceles";
+    }
+};
