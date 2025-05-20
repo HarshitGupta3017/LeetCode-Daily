0ed9285a4355041ea5c9c32cee68115062a4d8de
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-20 12:30:10 +0530

    Update Zero Array Transformation I.py

--- Zero Array Transformation I/Zero Array Transformation I.py
+++ Zero Array Transformation I/Zero Array Transformation I.py
@@ -1,1 +1,22 @@
 # Solution for Zero Array Transformation I in PY
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(nums)
+        diff = [0] * n
+        for q in queries:
+            start, end, x = q[0], q[1], 1
+            diff[start] += 1
+            if (end + 1 < n): 
+                diff[end + 1] -= 1
+        
+        res = [0] * n
+        cumSum = 0
+        for i in range(n):
+            cumSum += diff[i]
+            res[i] = cumSum
+        
+        for i in range(n):
+            if (res[i] < nums[i]):
+                return False
+        return True
