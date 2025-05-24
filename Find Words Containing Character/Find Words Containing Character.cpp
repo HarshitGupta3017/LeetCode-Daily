1c68855386ac5e54e2c34846a4ac84491e232e3b
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-24 10:43:34 +0530

    Update Find Words Containing Character.cpp

--- Find Words Containing Character/Find Words Containing Character.cpp
+++ Find Words Containing Character/Find Words Containing Character.cpp
@@ -1,1 +1,14 @@
 // Solution for Find Words Containing Character in CPP
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res;
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                res.push_back(i);
+            }
+        }
+        return res;
+    }
+};
