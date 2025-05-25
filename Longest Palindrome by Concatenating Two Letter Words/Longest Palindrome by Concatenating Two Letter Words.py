cc4a640917faeff5cf3da93775e965906c1e786e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-25 14:59:49 +0530

    Update Longest Palindrome by Concatenating Two Letter Words.py

--- Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.py
+++ Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.py
@@ -1,1 +1,23 @@
 # Solution for Longest Palindrome by Concatenating Two Letter Words in PY
+
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        mp = Counter(words)
+        res = 0
+        center_used = False
+        for word in words:
+            rev = word[::-1]
+            if word != rev:
+                if mp[word] > 0 and mp[rev] > 0:
+                    mp[word] -= 1
+                    mp[rev] -= 1
+                    res += 4
+            else:
+                if mp[word] >= 2:
+                    mp[word] -= 2
+                    res += 4
+                elif mp[word] == 1 and not center_used:
+                    mp[word] -= 1
+                    center_used = True
+                    res += 2
+        return res
