0f869586696ba623527ab92a3cd5bd439ddef833
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-25 14:53:46 +0530

    Update Longest Palindrome by Concatenating Two Letter Words.cpp

--- Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.cpp
+++ Longest Palindrome by Concatenating Two Letter Words/Longest Palindrome by Concatenating Two Letter Words.cpp
@@ -1,1 +1,34 @@
 // Solution for Longest Palindrome by Concatenating Two Letter Words in CPP
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> mp;
+        for (const auto& word: words) {
+            mp[word]++;
+        }
+        int res = 0;
+        bool centerUsed = false;
+        for (const auto& word: words) {
+            string rev = word;
+            reverse(rev.begin(), rev.end());
+            if (word != rev) {
+                if (mp[word] > 0 && mp[rev] > 0) {
+                    mp[word]--;
+                    mp[rev]--;
+                    res += 4;
+                }
+            } else {
+                if (mp[word] >= 2) {
+                    mp[word] -= 2;
+                    res += 4;
+                } else if (mp[word] == 1 && !centerUsed) {
+                    mp[word]--;
+                    centerUsed = true;
+                    res += 2;
+                }
+            }
+        }
+        return res;
+    }
+};
