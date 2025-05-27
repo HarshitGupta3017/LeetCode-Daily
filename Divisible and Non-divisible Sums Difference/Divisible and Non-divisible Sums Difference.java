504e2c14a852cbd0ac2d211da7cdc92b777436c2
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-27 11:27:06 +0530

    Update Divisible and Non-divisible Sums Difference.java

--- Divisible and Non-divisible Sums Difference/Divisible and Non-divisible Sums Difference.java
+++ Divisible and Non-divisible Sums Difference/Divisible and Non-divisible Sums Difference.java
@@ -1,1 +1,10 @@
 // Solution for Divisible and Non-divisible Sums Difference in JAVA
+
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int totalSum = n * (n + 1) / 2;
+        int countDivisibleByM = n / m;
+        int sumDivisibleByM = m * countDivisibleByM * (countDivisibleByM + 1) / 2;
+        return totalSum - 2 * sumDivisibleByM;
+    }
+}
