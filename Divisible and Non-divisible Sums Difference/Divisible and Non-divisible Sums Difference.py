43255f3a4703cc0099f5c5d112491076166db2af
Harshit Gupta <harshitgupta3017@gmail.com>
2025-05-27 11:26:53 +0530

    Update Divisible and Non-divisible Sums Difference.py

--- Divisible and Non-divisible Sums Difference/Divisible and Non-divisible Sums Difference.py
+++ Divisible and Non-divisible Sums Difference/Divisible and Non-divisible Sums Difference.py
@@ -1,1 +1,8 @@
 # Solution for Divisible and Non-divisible Sums Difference in PY
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        totalSum = n * (n + 1) // 2
+        countDivisibleByM = n // m
+        sumDivisibleByM = m * countDivisibleByM * (countDivisibleByM + 1) // 2
+        return totalSum - 2 * sumDivisibleByM
