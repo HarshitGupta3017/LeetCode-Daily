71ed16241334773fc1b0005e79a90a94c461283b
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-03 15:36:05 +0530

    Update Distribute Candies Among Children II.java

--- Distribute Candies Among Children II/Distribute Candies Among Children II.java
+++ Distribute Candies Among Children II/Distribute Candies Among Children II.java
@@ -1,1 +1,21 @@
 // Solution for Distribute Candies Among Children II in JAVA
+
+class Solution {
+    public long distributeCandies(int totalCandies, int limitPerChild) {
+        long totalWays = 0;
+
+        int minCandiesChild1 = Math.max(0, totalCandies - 2 * limitPerChild);
+        int maxCandiesChild1 = Math.min(totalCandies, limitPerChild);
+
+        for (int candiesChild1 = minCandiesChild1; candiesChild1 <= maxCandiesChild1; candiesChild1++) {
+            int remainingCandies = totalCandies - candiesChild1;
+
+            int minCandiesChild2 = Math.max(0, remainingCandies - limitPerChild);
+            int maxCandiesChild2 = Math.min(remainingCandies, limitPerChild);
+
+            totalWays += (maxCandiesChild2 - minCandiesChild2 + 1);
+        }
+
+        return totalWays;
+    }
+}
