15e1ad5f5f3e873234ba6e42754db644c9e0aed7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-03 15:36:35 +0530

    Update Distribute Candies Among Children II.py

--- Distribute Candies Among Children II/Distribute Candies Among Children II.py
+++ Distribute Candies Among Children II/Distribute Candies Among Children II.py
@@ -1,1 +1,18 @@
 # Solution for Distribute Candies Among Children II in PY
+
+class Solution:
+    def distributeCandies(self, totalCandies: int, limitPerChild: int) -> int:
+        totalWays = 0
+
+        minCandiesChild1 = max(0, totalCandies - 2 * limitPerChild)
+        maxCandiesChild1 = min(totalCandies, limitPerChild)
+
+        for candiesChild1 in range(minCandiesChild1, maxCandiesChild1 + 1):
+            remainingCandies = totalCandies - candiesChild1
+
+            minCandiesChild2 = max(0, remainingCandies - limitPerChild)
+            maxCandiesChild2 = min(remainingCandies, limitPerChild)
+
+            totalWays += (maxCandiesChild2 - minCandiesChild2 + 1)
+
+        return totalWays
