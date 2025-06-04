54f9038d02f7107f70f5b4e1a0fa22129f15bdaf
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-04 15:00:56 +0530

    Update Find the Lexicographically Largest String From the Box I.java

--- Find the Lexicographically Largest String From the Box I/Find the Lexicographically Largest String From the Box I.java
+++ Find the Lexicographically Largest String From the Box I/Find the Lexicographically Largest String From the Box I.java
@@ -1,1 +1,21 @@
 // Solution for Find the Lexicographically Largest String From the Box I in JAVA
+
+class Solution {
+    public String answerString(String word, int numFriends) {
+        if (numFriends == 1) return word;
+
+        int totalLength = word.length();
+        int maxChunkLength = totalLength - (numFriends - 1);
+        String maxString = "";
+
+        for (int start = 0; start < totalLength; start++) {
+            int currentChunkLength = Math.min(maxChunkLength, totalLength - start);
+            String currentSubstring = word.substring(start, start + currentChunkLength);
+            if (currentSubstring.compareTo(maxString) > 0) {
+                maxString = currentSubstring;
+            }
+        }
+
+        return maxString;
+    }
+}
