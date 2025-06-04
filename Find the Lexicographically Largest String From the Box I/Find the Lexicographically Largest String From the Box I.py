1996edd380a8033fc9edb4c2193317393e599d37
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-04 15:00:25 +0530

    Update Find the Lexicographically Largest String From the Box I.py

--- Find the Lexicographically Largest String From the Box I/Find the Lexicographically Largest String From the Box I.py
+++ Find the Lexicographically Largest String From the Box I/Find the Lexicographically Largest String From the Box I.py
@@ -1,1 +1,18 @@
 # Solution for Find the Lexicographically Largest String From the Box I in PY
+
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1: 
+            return word
+
+        totalLength = len(word)
+        maxChunkLength = totalLength - (numFriends - 1)
+        maxString = ""
+
+        for start in range(totalLength):
+            currentChunkLength = min(maxChunkLength, totalLength - start)
+            currentSubstring = word[start:start + currentChunkLength]
+            if currentSubstring > maxString:
+                maxString = currentSubstring     
+
+        return maxString
