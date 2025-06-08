b47d9a50bb448fdd0f19fe54d7b84099740934d9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-08 11:29:11 +0530

    Update Lexicographical Numbers.py

--- Lexicographical Numbers/Lexicographical Numbers.py
+++ Lexicographical Numbers/Lexicographical Numbers.py
@@ -1,1 +1,21 @@
 # Solution for Lexicographical Numbers in PY
+
+class Solution:
+    def lexicalOrder(self, n: int) -> list[int]:
+        def generateLexical(currentNum: int):
+            if currentNum > n:
+                return
+
+            result.append(currentNum)
+
+            for digit in range(10):
+                nextNum = currentNum * 10 + digit
+                if nextNum > n:
+                    return
+                generateLexical(nextNum)
+
+        result = []
+        for start in range(1, 10):
+            generateLexical(start)
+
+        return result
