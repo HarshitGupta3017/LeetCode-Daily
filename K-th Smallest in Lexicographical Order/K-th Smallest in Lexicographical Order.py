6adcaf3cb0b569d2574abd891bfec330c050a43e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-09 18:51:47 +0530

    Update K-th Smallest in Lexicographical Order.py

--- K-th Smallest in Lexicographical Order/K-th Smallest in Lexicographical Order.py
+++ K-th Smallest in Lexicographical Order/K-th Smallest in Lexicographical Order.py
@@ -1,1 +1,25 @@
 # Solution for K-th Smallest in Lexicographical Order in PY
+
+class Solution:
+    def countNumbersWithPrefix(self, prefix: int, nextPrefix: int, n: int) -> int:
+        totalCount = 0
+        while prefix <= n:
+            totalCount += min(n + 1, nextPrefix) - prefix
+            prefix *= 10
+            nextPrefix *= 10
+        return totalCount
+
+    def findKthNumber(self, n: int, k: int) -> int:
+        current = 1
+        k -= 1
+
+        while k > 0:
+            count = self.countNumbersWithPrefix(current, current + 1, n)
+            if count <= k:
+                current += 1
+                k -= count
+            else:
+                current *= 10
+                k -= 1
+
+        return current
