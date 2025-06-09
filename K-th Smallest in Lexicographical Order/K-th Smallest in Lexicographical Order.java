d9b8d969bf5b9f1ac55c26b6b7bde6baeb8ca239
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-09 18:51:36 +0530

    Update K-th Smallest in Lexicographical Order.java

--- K-th Smallest in Lexicographical Order/K-th Smallest in Lexicographical Order.java
+++ K-th Smallest in Lexicographical Order/K-th Smallest in Lexicographical Order.java
@@ -1,1 +1,31 @@
 // Solution for K-th Smallest in Lexicographical Order in JAVA
+
+class Solution {
+    public int countNumbersWithPrefix(long prefix, long nextPrefix, int n) {
+        int totalCount = 0;
+        while (prefix <= n) {
+            totalCount += Math.min((long) n + 1, nextPrefix) - prefix;
+            prefix *= 10;
+            nextPrefix *= 10;
+        }
+        return totalCount;
+    }
+
+    public int findKthNumber(int n, int k) {
+        int current = 1;
+        k--;
+
+        while (k > 0) {
+            int count = countNumbersWithPrefix(current, current + 1, n);
+            if (count <= k) {
+                current++;
+                k -= count;
+            } else {
+                current *= 10;
+                k--;
+            }
+        }
+
+        return current;
+    }
+}
