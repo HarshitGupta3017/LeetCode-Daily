c00ef0569809acae2b8782c1419e9c80fed9fad4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-10 18:35:55 +0530

    Update Maximum Difference Between Even and Odd Frequency I.py

--- Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.py
+++ Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.py
@@ -1,1 +1,21 @@
 # Solution for Maximum Difference Between Even and Odd Frequency I in PY
+
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        charFreq = [0] * 26
+        for ch in s:
+            charFreq[ord(ch) - ord('a')] += 1
+        
+        maxOddFreq = 0
+        minEvenFreq = sys.maxsize
+        
+        for i in range(26):
+            if charFreq[i] == 0:
+                continue
+            
+            if charFreq[i] % 2 == 0:
+                minEvenFreq = min(minEvenFreq, charFreq[i])
+            else:
+                maxOddFreq = max(maxOddFreq, charFreq[i])
+        
+        return maxOddFreq - minEvenFreq
