f88ed14180394f6f3e4c1d2c9a1d7d4e7d322f67
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-10 18:35:41 +0530

    Update Maximum Difference Between Even and Odd Frequency I.java

--- Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.java
+++ Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.java
@@ -1,1 +1,25 @@
 // Solution for Maximum Difference Between Even and Odd Frequency I in JAVA
+
+class Solution {
+    public int maxDifference(String s) {
+        int[] charFreq = new int[26];
+        for (char ch : s.toCharArray()) {
+            charFreq[ch - 'a']++;
+        }
+        
+        int maxOddFreq = 0;
+        int minEvenFreq = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < 26; i++) {
+            if (charFreq[i] == 0) continue; 
+            
+            if (charFreq[i] % 2 == 0) {
+                minEvenFreq = Math.min(minEvenFreq, charFreq[i]);
+            } else {
+                maxOddFreq = Math.max(maxOddFreq, charFreq[i]);
+            }
+        }
+        
+        return maxOddFreq - minEvenFreq;
+    }
+}
