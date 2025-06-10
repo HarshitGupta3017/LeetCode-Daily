62e0286ba102cca50aeab9def3364abded52e310
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-10 18:29:58 +0530

    Update Maximum Difference Between Even and Odd Frequency I.cpp

--- Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.cpp
+++ Maximum Difference Between Even and Odd Frequency I/Maximum Difference Between Even and Odd Frequency I.cpp
@@ -1,1 +1,31 @@
 // Solution for Maximum Difference Between Even and Odd Frequency I in CPP
+
+class Solution {
+public:
+    int maxDifference(string s) {
+        // Count frequency of each character (a-z)
+        vector<int> charFreq(26, 0);
+        for (char ch : s) {
+            charFreq[ch - 'a']++;
+        }
+        
+        // Track maximum odd frequency and minimum even frequency
+        int maxOddFreq = 0;
+        int minEvenFreq = INT_MAX;
+        
+        for (int i = 0; i < 26; i++) {
+            if (charFreq[i] == 0) continue; // Skip characters not in string
+            
+            if (charFreq[i] % 2 == 0) {
+                // Update minimum even frequency
+                minEvenFreq = min(minEvenFreq, charFreq[i]);
+            } else {
+                // Update maximum odd frequency
+                maxOddFreq = max(maxOddFreq, charFreq[i]);
+            }
+        }
+        
+        // Return difference: max odd frequency - min even frequency
+        return maxOddFreq - minEvenFreq;
+    }
+};
