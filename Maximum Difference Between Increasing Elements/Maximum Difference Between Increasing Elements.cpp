970ac4e1999276a743a03b87adb91a6ff2588168
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-16 11:42:06 +0530

    Update Maximum Difference Between Increasing Elements.cpp

--- Maximum Difference Between Increasing Elements/Maximum Difference Between Increasing Elements.cpp
+++ Maximum Difference Between Increasing Elements/Maximum Difference Between Increasing Elements.cpp
@@ -1,1 +1,18 @@
 // Solution for Maximum Difference Between Increasing Elements in CPP
+
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int n = nums.size();
+        int minEle = nums[0];
+        int maxDiff = -1;
+        for (int j = 1; j < n; j++) {
+            if (nums[j] > minEle) {
+                maxDiff = max(maxDiff, nums[j] - minEle);
+            } else {
+                minEle = nums[j];
+            }
+        }
+        return maxDiff;
+    }
+};
