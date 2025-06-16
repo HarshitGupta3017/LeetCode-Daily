8fb8a696487f3e9fc6dbbf772e3e855ce3b55df7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-16 11:41:52 +0530

    Update Maximum Difference Between Increasing Elements.java

--- Maximum Difference Between Increasing Elements/Maximum Difference Between Increasing Elements.java
+++ Maximum Difference Between Increasing Elements/Maximum Difference Between Increasing Elements.java
@@ -1,1 +1,17 @@
 // Solution for Maximum Difference Between Increasing Elements in JAVA
+
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int n = nums.length;
+        int minEle = nums[0];
+        int maxDiff = -1;
+        for (int j = 1; j < n; j++) {
+            if (nums[j] > minEle) {
+                maxDiff = Math.max(maxDiff, nums[j] - minEle);
+            } else {
+                minEle = nums[j];
+            }
+        }
+        return maxDiff;
+    }
+}
