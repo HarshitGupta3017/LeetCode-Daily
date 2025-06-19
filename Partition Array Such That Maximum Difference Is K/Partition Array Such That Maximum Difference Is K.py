369ab2ae77f306d2b6a4f787c3bae8a550e2f47c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-19 17:20:45 +0530

    Update Partition Array Such That Maximum Difference Is K.py

--- Partition Array Such That Maximum Difference Is K/Partition Array Such That Maximum Difference Is K.py
+++ Partition Array Such That Maximum Difference Is K/Partition Array Such That Maximum Difference Is K.py
@@ -1,1 +1,15 @@
 # Solution for Partition Array Such That Maximum Difference Is K in PY
+
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+
+        minSubsequences = 1
+        minInSubsequence = nums[0]
+
+        for ele in nums:
+            if ele - minInSubsequence > k:
+                minSubsequences += 1
+                minInSubsequence = ele
+
+        return minSubsequences
