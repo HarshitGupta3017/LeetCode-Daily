2ad796276c4898a641379ad8a825a745328dc468
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-21 17:23:47 +0530

    Update Minimum Deletions to Make String K-Special.py

--- Minimum Deletions to Make String K-Special/Minimum Deletions to Make String K-Special.py
+++ Minimum Deletions to Make String K-Special/Minimum Deletions to Make String K-Special.py
@@ -1,1 +1,25 @@
 # Solution for Minimum Deletions to Make String K-Special in PY
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freq = [0] * 26
+
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        freq.sort()
+        min_deletions = float('inf')
+        cumulative_deletions = 0
+
+        for i in range(26):
+            deletions_needed = cumulative_deletions
+
+            for j in range(25, i, -1):
+                if freq[j] - freq[i] <= k:
+                    break
+                deletions_needed += freq[j] - freq[i] - k
+
+            min_deletions = min(min_deletions, deletions_needed)
+            cumulative_deletions += freq[i]
+
+        return min_deletions
