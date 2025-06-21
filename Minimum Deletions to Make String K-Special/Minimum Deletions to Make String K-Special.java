320015401b782c7e143ad9f2a4aa434896465f70
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-21 17:24:07 +0530

    Update Minimum Deletions to Make String K-Special.java

--- Minimum Deletions to Make String K-Special/Minimum Deletions to Make String K-Special.java
+++ Minimum Deletions to Make String K-Special/Minimum Deletions to Make String K-Special.java
@@ -1,1 +1,29 @@
 // Solution for Minimum Deletions to Make String K-Special in JAVA
+
+class Solution {
+    public int minimumDeletions(String word, int k) {
+        int[] freq = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+        int minDeletions = Integer.MAX_VALUE;
+        int cumulativeDeletions = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int deletionsNeeded = cumulativeDeletions;
+
+            for (int j = 25; j > i; j--) {
+                if (freq[j] - freq[i] <= k) break;
+                deletionsNeeded += freq[j] - freq[i] - k;
+            }
+
+            minDeletions = Math.min(minDeletions, deletionsNeeded);
+            cumulativeDeletions += freq[i];
+        }
+
+        return minDeletions;
+    }
+}
