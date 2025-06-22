2d49f57257c0380c9859137ba618042b2e8731c0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-22 18:04:22 +0530

    Update Divide a String Into Groups of Size k.cpp

--- Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.cpp
+++ Divide a String Into Groups of Size k/Divide a String Into Groups of Size k.cpp
@@ -1,1 +1,27 @@
 // Solution for Divide a String Into Groups of Size k in CPP
+
+class Solution {
+public:
+    vector<string> divideString(string s, int k, char fill) {
+        vector<string> result;
+        int n = s.length();
+
+        // Traverse the string in steps of size k
+        for (int i = 0; i < n; i += k) {
+            // Calculate the actual substring size (might be less than k at the end)
+            int len = min(k, n - i);
+
+            // Extract substring of up to k characters starting from index i
+            string group = s.substr(i, len);
+
+            // If substring length is less than k, pad it with the fill character
+            if (len < k) {
+                group += string(k - len, fill);
+            }
+
+            result.push_back(group);
+        }
+
+        return result;
+    }
+};
