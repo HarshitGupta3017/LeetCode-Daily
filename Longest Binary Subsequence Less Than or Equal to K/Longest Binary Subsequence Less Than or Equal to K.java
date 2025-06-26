51eec51fc0fe3ac92cd56468aa4ec3647972c64b
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-26 18:24:07 +0530

    Update Longest Binary Subsequence Less Than or Equal to K.java

--- Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
+++ Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.java
@@ -1,1 +1,22 @@
 // Solution for Longest Binary Subsequence Less Than or Equal to K in JAVA
+
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        int n = s.length();
+        long powerOfTwo = 1;  
+        int maxLength = 0;       
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                maxLength++;
+            } else if (powerOfTwo <= k) {
+                k -= powerOfTwo;
+                maxLength++;
+            }
+            if (powerOfTwo <= k) {
+                powerOfTwo <<= 1; 
+            }
+        }
+        return maxLength;
+    }
+}
