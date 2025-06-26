f15a123395c74966a7ba844a39e98b179fc34b85
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-26 18:23:53 +0530

    Update Longest Binary Subsequence Less Than or Equal to K.py

--- Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.py
+++ Longest Binary Subsequence Less Than or Equal to K/Longest Binary Subsequence Less Than or Equal to K.py
@@ -1,1 +1,18 @@
 # Solution for Longest Binary Subsequence Less Than or Equal to K in PY
+
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        n = len(s)
+        powerOfTwo = 1
+        maxLength = 0
+
+        for i in range(n - 1, -1, -1):
+            if s[i] == '0':
+                maxLength += 1
+            elif powerOfTwo <= k:
+                k -= powerOfTwo
+                maxLength += 1
+            if powerOfTwo <= k:
+                powerOfTwo <<= 1
+
+        return maxLength
