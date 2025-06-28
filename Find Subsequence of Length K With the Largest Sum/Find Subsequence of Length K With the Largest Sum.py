cea0109f1f94751ec8bd16f2dfef6eb28f655fc7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-28 14:06:05 +0530

    Update Find Subsequence of Length K With the Largest Sum.py

--- Find Subsequence of Length K With the Largest Sum/Find Subsequence of Length K With the Largest Sum.py
+++ Find Subsequence of Length K With the Largest Sum/Find Subsequence of Length K With the Largest Sum.py
@@ -1,1 +1,23 @@
 # Solution for Find Subsequence of Length K With the Largest Sum in PY
+
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+
+        temp = sorted(nums, reverse=True)
+        kthLargest = temp[k - 1]
+        remainingKthCount = temp[:k].count(kthLargest)
+
+        result = []
+        for num in nums:
+            if num > kthLargest:
+                result.append(num)
+            elif num == kthLargest and remainingKthCount > 0:
+                result.append(num)
+                remainingKthCount -= 1
+
+            if len(result) == k:
+                break
+
+        return result
