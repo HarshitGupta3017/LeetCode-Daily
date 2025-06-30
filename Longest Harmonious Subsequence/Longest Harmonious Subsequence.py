b7636625a1dbb1cf64606e9540326456c86c9f9d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-30 11:57:36 +0530

    Update Longest Harmonious Subsequence.py

--- Longest Harmonious Subsequence/Longest Harmonious Subsequence.py
+++ Longest Harmonious Subsequence/Longest Harmonious Subsequence.py
@@ -1,1 +1,13 @@
 # Solution for Longest Harmonious Subsequence in PY
+
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        freq_map = Counter(nums)
+        max_length = 0
+
+        for num in freq_map:
+            if num + 1 in freq_map:
+                current_length = freq_map[num] + freq_map[num + 1]
+                max_length = max(max_length, current_length)
+
+        return max_length
