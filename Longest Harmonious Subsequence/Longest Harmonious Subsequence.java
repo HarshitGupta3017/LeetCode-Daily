6a024e1b370db5487508003b1194a5887a2bc773
Harshit Gupta <harshitgupta3017@gmail.com>
2025-06-30 11:57:25 +0530

    Update Longest Harmonious Subsequence.java

--- Longest Harmonious Subsequence/Longest Harmonious Subsequence.java
+++ Longest Harmonious Subsequence/Longest Harmonious Subsequence.java
@@ -1,1 +1,22 @@
 // Solution for Longest Harmonious Subsequence in JAVA
+
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxLength = 0;
+
+        for (int num : freqMap.keySet()) {
+            if (freqMap.containsKey(num + 1)) {
+                int currentLength = freqMap.get(num) + freqMap.get(num + 1);
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+
+        return maxLength;
+    }
+}
