f172a9958dca90c400aef547d61d7b21560f489f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-01 13:57:02 +0530

    Update Find the Original Typed String I.py

--- Find the Original Typed String I/Find the Original Typed String I.py
+++ Find the Original Typed String I/Find the Original Typed String I.py
@@ -1,1 +1,9 @@
 # Solution for Find the Original Typed String I in PY
+
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        repeat_count = 0
+        for i in range(1, len(word)):
+            if word[i] == word[i - 1]:
+                repeat_count += 1
+        return repeat_count + 1
