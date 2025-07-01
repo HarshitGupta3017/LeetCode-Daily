ed31ea568a3a3e88927934e8e913f879393ad907
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-01 13:57:14 +0530

    Update Find the Original Typed String I.java

--- Find the Original Typed String I/Find the Original Typed String I.java
+++ Find the Original Typed String I/Find the Original Typed String I.java
@@ -1,1 +1,13 @@
 // Solution for Find the Original Typed String I in JAVA
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int repeatCount = 0;
+        for (int i = 1; i < word.length(); i++) {
+            if (word.charAt(i) == word.charAt(i - 1)) {
+                repeatCount++;
+            }
+        }
+        return repeatCount + 1;
+    }
+}
