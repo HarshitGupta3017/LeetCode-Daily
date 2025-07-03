b809318ef0c6970575e594cf88175c76ce6aef3d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-03 13:39:07 +0530

    Update Find the K-th Character in String Game I.cpp

--- Find the K-th Character in String Game I/Find the K-th Character in String Game I.cpp
+++ Find the K-th Character in String Game I/Find the K-th Character in String Game I.cpp
@@ -1,1 +1,16 @@
 // Solution for Find the K-th Character in String Game I in CPP
+
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string word = "a";
+        while (word.length() < k) {
+            string temp;
+            for(char ch: word) {
+                temp += (ch == 'z') ? 'a' : ch + 1;
+            }
+            word += temp;
+        }
+        return word[k - 1];
+    }
+};
