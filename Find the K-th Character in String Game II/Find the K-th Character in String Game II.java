0dbeacadec8dcfedfd471986a417ad3ea263659d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-04 18:02:33 +0530

    Update Find the K-th Character in String Game II.java

--- Find the K-th Character in String Game II/Find the K-th Character in String Game II.java
+++ Find the K-th Character in String Game II/Find the K-th Character in String Game II.java
@@ -1,1 +1,28 @@
 // Solution for Find the K-th Character in String Game II in JAVA
+
+class Solution {
+    public char kthCharacter(long k, int[] operations) {
+        if (k == 1) return 'a';
+
+        long currentLength = 1;
+        long reducedK = -1;
+        int operationType = 0;
+
+        for (int i = 0; i < operations.length; i++) {
+            currentLength *= 2;
+            if (currentLength >= k) {
+                operationType = operations[i];
+                reducedK = k - currentLength / 2;
+                break;
+            }
+        }
+
+        char prevChar = kthCharacter(reducedK, operations);
+
+        if (operationType == 0) {
+            return prevChar;
+        } else {
+            return (prevChar == 'z') ? 'a' : (char)(prevChar + 1);
+        }
+    }
+}
