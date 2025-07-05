665bd8b2845ca9f88b66f26d3dabc36d4a290585
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-05 16:51:03 +0530

    Update Find Lucky Integer in an Array.py

--- Find Lucky Integer in an Array/Find Lucky Integer in an Array.py
+++ Find Lucky Integer in an Array/Find Lucky Integer in an Array.py
@@ -1,1 +1,15 @@
 # Solution for Find Lucky Integer in an Array in PY
+
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        n = len(arr)
+        for num in arr:
+            value = num & 65535
+            if value <= n:
+                arr[value - 1] += (1 << 16)
+
+        for val in range(n, 0, -1):
+            freq = arr[val - 1] >> 16
+            if freq == val:
+                return val
+        return -1
