de4ef052f67ab7cdc62842443ed54baa74cd2c13
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-05 16:51:14 +0530

    Update Find Lucky Integer in an Array.java

--- Find Lucky Integer in an Array/Find Lucky Integer in an Array.java
+++ Find Lucky Integer in an Array/Find Lucky Integer in an Array.java
@@ -1,1 +1,20 @@
 // Solution for Find Lucky Integer in an Array in JAVA
+
+class Solution {
+    public int findLucky(int[] arr) {
+        int n = arr.length;
+        for (int num : arr) {
+            int value = num & 65535;
+            if (value <= n) {
+                arr[value - 1] += (1 << 16);
+            }
+        }
+        for (int val = n; val >= 1; val--) {
+            int freq = arr[val - 1] >> 16;
+            if (freq == val) {
+                return val;
+            }
+        }
+        return -1;
+    }
+}
