c13278adf34e031fc98537109329dd150fa2b682
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-07 17:48:33 +0530

    Update Maximum Number of Events That Can Be Attended.java

--- Maximum Number of Events That Can Be Attended/Maximum Number of Events That Can Be Attended.java
+++ Maximum Number of Events That Can Be Attended/Maximum Number of Events That Can Be Attended.java
@@ -1,1 +1,35 @@
 // Solution for Maximum Number of Events That Can Be Attended in JAVA
+
+class Solution {
+    public int maxEvents(int[][] events) {
+        Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int day = events[0][0];
+        int totalAttended = 0;
+        int i = 0, n = events.length;
+
+        while (i < n || !minHeap.isEmpty()) {
+            if (minHeap.isEmpty()) {
+                day = events[i][0];
+            }
+
+            while (i < n && events[i][0] == day) {
+                minHeap.offer(events[i][1]);
+                i++;
+            }
+
+            if (!minHeap.isEmpty()) {
+                minHeap.poll();
+                totalAttended++;
+            }
+
+            day++;
+
+            while (!minHeap.isEmpty() && minHeap.peek() < day) {
+                minHeap.poll();
+            }
+        }
+
+        return totalAttended;
+    }
+}
