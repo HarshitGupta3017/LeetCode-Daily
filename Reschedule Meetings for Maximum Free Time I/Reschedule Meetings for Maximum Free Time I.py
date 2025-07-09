12da69983817b44fa3d5c76f24dc02a672a1fa4d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-09 18:21:57 +0530

    Update Reschedule Meetings for Maximum Free Time I.py

--- Reschedule Meetings for Maximum Free Time I/Reschedule Meetings for Maximum Free Time I.py
+++ Reschedule Meetings for Maximum Free Time I/Reschedule Meetings for Maximum Free Time I.py
@@ -1,1 +1,26 @@
 # Solution for Reschedule Meetings for Maximum Free Time I in PY
+
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: list[int], endTime: list[int]) -> int:
+        n = len(startTime)
+        freeGaps = []
+
+        freeGaps.append(startTime[0])
+        for i in range(1, n):
+            freeGaps.append(startTime[i] - endTime[i - 1])
+        freeGaps.append(eventTime - endTime[-1])
+
+        maxFree = 0
+        currSum = 0
+        left = 0
+
+        for right in range(len(freeGaps)):
+            currSum += freeGaps[right]
+
+            while right - left + 1 > k + 1:
+                currSum -= freeGaps[left]
+                left += 1
+
+            maxFree = max(maxFree, currSum)
+
+        return maxFree
