d8da1577be7facca24e4b99073167d9931208373
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-13 15:49:49 +0530

    Update Maximum Matching of Players With Trainers.java

--- Maximum Matching of Players With Trainers/Maximum Matching of Players With Trainers.java
+++ Maximum Matching of Players With Trainers/Maximum Matching of Players With Trainers.java
@@ -1,1 +1,22 @@
 // Solution for Maximum Matching of Players With Trainers in JAVA
+
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int playerIdx = 0;
+        int trainerIdx = 0;
+        int matched = 0;
+
+        while (playerIdx < players.length && trainerIdx < trainers.length) {
+            if (players[playerIdx] <= trainers[trainerIdx]) {
+                matched++;
+                playerIdx++;
+            }
+            trainerIdx++;
+        }
+
+        return matched;
+    }
+}
