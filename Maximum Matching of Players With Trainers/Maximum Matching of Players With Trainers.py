0b67cd509e1cc9ac38d9df20f78ab6e98fc26de6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-13 15:49:35 +0530

    Update Maximum Matching of Players With Trainers.py

--- Maximum Matching of Players With Trainers/Maximum Matching of Players With Trainers.py
+++ Maximum Matching of Players With Trainers/Maximum Matching of Players With Trainers.py
@@ -1,1 +1,16 @@
 # Solution for Maximum Matching of Players With Trainers in PY
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+
+        i = j = matched = 0
+
+        while i < len(players) and j < len(trainers):
+            if players[i] <= trainers[j]:
+                matched += 1
+                i += 1
+            j += 1
+
+        return matched
