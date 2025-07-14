02bae593cb1168b3fabcd808558faf10391bf76d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-14 13:08:02 +0530

    Update Convert Binary Number in a Linked List to Integer.cpp

--- Convert Binary Number in a Linked List to Integer/Convert Binary Number in a Linked List to Integer.cpp
+++ Convert Binary Number in a Linked List to Integer/Convert Binary Number in a Linked List to Integer.cpp
@@ -1,1 +1,21 @@
 // Solution for Convert Binary Number in a Linked List to Integer in CPP
+
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int decimalValue = 0;
+
+        // Traverse the linked list node by node
+        while (head) {
+            // Left shift the current result (equivalent to multiplying by 2)
+            // and add the current node's binary digit
+            decimalValue = (decimalValue << 1) | head->val;
+
+            // Move to the next node
+            head = head->next;
+        }
+
+        // Final result contains the decimal value of the binary number
+        return decimalValue;
+    }
+};
