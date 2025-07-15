792be14bba57ca1e407d222a1089df81d9a65239
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-15 17:21:44 +0530

    Update Valid Word.java

--- Valid Word/Valid Word.java
+++ Valid Word/Valid Word.java
@@ -1,1 +1,25 @@
 // Solution for Valid Word in JAVA
+
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            ch = Character.toLowerCase(ch);
+            if (Character.isLetter(ch)) {
+                if ("aeiou".indexOf(ch) >= 0) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
