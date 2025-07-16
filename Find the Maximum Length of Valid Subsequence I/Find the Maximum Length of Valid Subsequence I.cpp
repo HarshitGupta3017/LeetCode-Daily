d3066a6c6240ac92eb3aa731a43663295c6eadab
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-16 18:00:01 +0530

    Update Find the Maximum Length of Valid Subsequence I.cpp

--- Find the Maximum Length of Valid Subsequence I/Find the Maximum Length of Valid Subsequence I.cpp
+++ Find the Maximum Length of Valid Subsequence I/Find the Maximum Length of Valid Subsequence I.cpp
@@ -1,1 +1,35 @@
 // Solution for Find the Maximum Length of Valid Subsequence I in CPP
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        // Counters for number of even and odd elements in the array
+        int evenCount = 0;
+        int oddCount = 0;
+
+        // Tracks the maximum length of an alternating parity subsequence
+        int maxAlternatingLength = 1;
+
+        // First element's parity: 0 for even, 1 for odd
+        int previousParity = nums[0] % 2;
+
+        // Count the first element
+        (previousParity == 0) ? evenCount++ : oddCount++;
+
+        for (int i = 1; i < nums.size(); i++) {
+            int currentParity = nums[i] % 2;
+
+            // Update alternating subsequence length only when parity changes
+            if (currentParity != previousParity) {
+                maxAlternatingLength++;
+                previousParity = currentParity;
+            }
+
+            // Count even and odd numbers
+            (currentParity == 0) ? evenCount++ : oddCount++;
+        }
+
+        // Return the longest valid subsequence length
+        return max({evenCount, oddCount, maxAlternatingLength});
+    }
+};
