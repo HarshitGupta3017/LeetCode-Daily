18cdec34bf274e8dec22f566d8a97c6c067024c9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-17 22:50:37 +0530

    Update Find the Maximum Length of Valid Subsequence II.py

--- Find the Maximum Length of Valid Subsequence II/Find the Maximum Length of Valid Subsequence II.py
+++ Find the Maximum Length of Valid Subsequence II/Find the Maximum Length of Valid Subsequence II.py
@@ -1,1 +1,15 @@
 # Solution for Find the Maximum Length of Valid Subsequence II in PY
+
+class Solution:
+    def maximumLength(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        dp = [[1] * n for _ in range(k)]
+        maxLength = 1
+
+        for i in range(1, n):
+            for j in range(i):
+                mod = (nums[i] + nums[j]) % k
+                dp[mod][i] = max(dp[mod][i], 1 + dp[mod][j])
+                maxLength = max(maxLength, dp[mod][i])
+
+        return maxLength
