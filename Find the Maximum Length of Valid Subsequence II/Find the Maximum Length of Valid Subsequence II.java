a257609a384d7e77b16f7f8844545703e05bb9f9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-17 22:50:10 +0530

    Update Find the Maximum Length of Valid Subsequence II.java

--- Find the Maximum Length of Valid Subsequence II/Find the Maximum Length of Valid Subsequence II.java
+++ Find the Maximum Length of Valid Subsequence II/Find the Maximum Length of Valid Subsequence II.java
@@ -1,1 +1,23 @@
 // Solution for Find the Maximum Length of Valid Subsequence II in JAVA
+
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int n = nums.length;
+        int[][] dp = new int[k][n];
+
+        for (int i = 0; i < k; i++)
+            Arrays.fill(dp[i], 1);
+
+        int maxLength = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int mod = (nums[i] + nums[j]) % k;
+                dp[mod][i] = Math.max(dp[mod][i], 1 + dp[mod][j]);
+                maxLength = Math.max(maxLength, dp[mod][i]);
+            }
+        }
+
+        return maxLength;
+    }
+}
