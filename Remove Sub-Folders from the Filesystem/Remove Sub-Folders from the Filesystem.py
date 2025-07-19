6870a92a138fa368be65537f85c9fdf0e0548e88
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-19 22:33:12 +0530

    Update Remove Sub-Folders from the Filesystem.py

--- Remove Sub-Folders from the Filesystem/Remove Sub-Folders from the Filesystem.py
+++ Remove Sub-Folders from the Filesystem/Remove Sub-Folders from the Filesystem.py
@@ -1,1 +1,10 @@
 # Solution for Remove Sub-Folders from the Filesystem in PY
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        res = []
+        for f in folder:
+            if not res or not f.startswith(res[-1] + '/'):
+                res.append(f)
+        return res
