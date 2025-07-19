01e9db4dfb11ce71be03507c3ea80f1c389f070a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-19 22:32:44 +0530

    Update Remove Sub-Folders from the Filesystem.java

--- Remove Sub-Folders from the Filesystem/Remove Sub-Folders from the Filesystem.java
+++ Remove Sub-Folders from the Filesystem/Remove Sub-Folders from the Filesystem.java
@@ -1,1 +1,13 @@
 // Solution for Remove Sub-Folders from the Filesystem in JAVA
+
+class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> res = new ArrayList<>();
+        for (String f : folder) {
+            if (res.isEmpty() || !f.startsWith(res.get(res.size() - 1) + "/"))
+                res.add(f);
+        }
+        return res;
+    }
+}
