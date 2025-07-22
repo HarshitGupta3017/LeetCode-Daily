19b991d487c0db023dee9c2f3a042fa9bad694df
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-22 14:36:20 +0530

    Update Maximum Erasure Value.py

--- Maximum Erasure Value/Maximum Erasure Value.py
+++ Maximum Erasure Value/Maximum Erasure Value.py
@@ -1,1 +1,21 @@
 # Solution for Maximum Erasure Value in PY
+
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        cumSum = [0] * n
+        cumSum[0] = nums[0]
+        for i in range(1, n):
+            cumSum[i] = cumSum[i - 1] + nums[i]
+        
+        left = right = maxScore = 0
+        lastSeen = [-1] * 10001
+        
+        while right < n:
+            left = max(left, lastSeen[nums[right]] + 1)
+            currentSum = cumSum[right] - (cumSum[left - 1] if left > 0 else 0)
+            maxScore = max(maxScore, currentSum)
+            lastSeen[nums[right]] = right
+            right += 1
+        
+        return maxScore
