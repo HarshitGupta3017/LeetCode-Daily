93bc9aa5a9278ad8cd2647ba42dd884ce478d9b4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-22 14:35:53 +0530

    Update Maximum Erasure Value.java

--- Maximum Erasure Value/Maximum Erasure Value.java
+++ Maximum Erasure Value/Maximum Erasure Value.java
@@ -1,1 +1,24 @@
 // Solution for Maximum Erasure Value in JAVA
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int n = nums.length;
+        int[] cumSum = new int[n];
+        cumSum[0] = nums[0];
+        for (int i = 1; i < n; i++) cumSum[i] = cumSum[i - 1] + nums[i];
+        
+        int left = 0, right = 0, maxScore = 0;
+        int[] lastSeen = new int[10001];
+        Arrays.fill(lastSeen, -1);
+        
+        while (right < n) {
+            left = Math.max(left, lastSeen[nums[right]] + 1);
+            int currentSum = cumSum[right] - (left > 0 ? cumSum[left - 1] : 0);
+            maxScore = Math.max(maxScore, currentSum);
+            lastSeen[nums[right]] = right;
+            right++;
+        }
+        
+        return maxScore;
+    }
+}
