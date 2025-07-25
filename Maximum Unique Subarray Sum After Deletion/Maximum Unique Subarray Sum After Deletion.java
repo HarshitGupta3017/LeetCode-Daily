dbcdbd2aabb6ce9d46fe4289c73998b00205b159
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-25 14:46:25 +0530

    Update Maximum Unique Subarray Sum After Deletion.java

--- Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.java
+++ Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.java
@@ -1,1 +1,21 @@
 // Solution for Maximum Unique Subarray Sum After Deletion in JAVA
+
+class Solution {
+    public int maxSum(int[] nums) {
+        int[] seen = new int[101];
+        Arrays.fill(seen, -1);
+        int maxSum = 0;
+        int maxNegative = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num <= 0) {
+                maxNegative = Math.max(maxNegative, num);
+            } else if (seen[num] == -1) {
+                maxSum += num;
+                seen[num] = 1;
+            }
+        }
+
+        return maxSum == 0 ? maxNegative : maxSum;
+    }
+}
