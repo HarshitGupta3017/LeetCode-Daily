cf20282ac783eabe32d0f1adcb93fc0a5d3f37d4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-25 14:46:47 +0530

    Update Maximum Unique Subarray Sum After Deletion.cpp

--- Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.cpp
+++ Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.cpp
@@ -1,1 +1,24 @@
 // Solution for Maximum Unique Subarray Sum After Deletion in CPP
+
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        vector<int> seen(101, -1); // Track which positive numbers have been added
+        int maxSum = 0;            // To store sum of unique positive elements
+        int maxNegative = INT_MIN; // Track max negative in case all are <= 0
+
+        for (const auto& num : nums) {
+            if (num <= 0) {
+                // Keep track of highest negative number
+                maxNegative = max(maxNegative, num);
+            } else if (seen[num] == -1) {
+                // Add unique positive number to sum
+                maxSum += num;
+                seen[num] = 1;
+            }
+        }
+
+        // If no positive number was added, return the max negative
+        return maxSum == 0 ? maxNegative : maxSum;
+    }
+};
