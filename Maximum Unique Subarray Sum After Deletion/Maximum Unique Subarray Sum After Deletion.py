025e2f7ce5e879ad070b513a4dc95fed8ad4a7cc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-25 14:46:34 +0530

    Update Maximum Unique Subarray Sum After Deletion.py

--- Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.py
+++ Maximum Unique Subarray Sum After Deletion/Maximum Unique Subarray Sum After Deletion.py
@@ -1,1 +1,16 @@
 # Solution for Maximum Unique Subarray Sum After Deletion in PY
+
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        seen = [-1] * 101
+        maxSum = 0
+        maxNegative = float('-inf')
+
+        for num in nums:
+            if num <= 0:
+                maxNegative = max(maxNegative, num)
+            elif seen[num] == -1:
+                maxSum += num
+                seen[num] = 1
+
+        return maxNegative if maxSum == 0 else maxSum
