1e0adae6665e9b91c1afbf231f1f9b68c4421780
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-27 22:46:46 +0530

    Update Count Hills and Valleys in an Array.py

--- Count Hills and Valleys in an Array/Count Hills and Valleys in an Array.py
+++ Count Hills and Valleys in an Array/Count Hills and Valleys in an Array.py
@@ -1,1 +1,15 @@
 # Solution for Count Hills and Valleys in an Array in PY
+
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        n = len(nums)
+        i, j, count = 0, 1, 0
+        while j + 1 < n:
+            if ((nums[i] < nums[j] and nums[j] > nums[j + 1]) 
+                    or 
+                (nums[i] > nums[j] and nums[j] < nums[j + 1])
+            ):
+                count += 1
+                i = j
+            j += 1
+        return count
