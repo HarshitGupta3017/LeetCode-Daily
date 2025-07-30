42446157a257aeb6aee97029d9bbd303d537c492
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-30 13:00:41 +0530

    Update Longest Subarray With Maximum Bitwise AND.py

--- Longest Subarray With Maximum Bitwise AND/Longest Subarray With Maximum Bitwise AND.py
+++ Longest Subarray With Maximum Bitwise AND/Longest Subarray With Maximum Bitwise AND.py
@@ -1,1 +1,20 @@
 # Solution for Longest Subarray With Maximum Bitwise AND in PY
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        maxLength = currentLength = maxVal = 0
+        
+        for num in nums:
+            if num > maxVal:
+                maxVal = num       
+                currentLength = 1
+                maxLength = 1  
+    
+            elif num == maxVal:
+                currentLength += 1
+                maxLength = max(maxLength, currentLength)
+        
+            else:
+                currentLength = 0
+
+        return maxLength
