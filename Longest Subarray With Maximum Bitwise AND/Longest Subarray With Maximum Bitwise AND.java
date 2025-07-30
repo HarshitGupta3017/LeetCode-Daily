059a5114dda0e272e6e15fb2c8c79c949920d7c1
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-30 13:00:20 +0530

    Update Longest Subarray With Maximum Bitwise AND.java

--- Longest Subarray With Maximum Bitwise AND/Longest Subarray With Maximum Bitwise AND.java
+++ Longest Subarray With Maximum Bitwise AND/Longest Subarray With Maximum Bitwise AND.java
@@ -1,1 +1,28 @@
 // Solution for Longest Subarray With Maximum Bitwise AND in JAVA
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int maxLength = 0;          
+        int currentLength = 0;      
+        int maxVal = 0;       
+
+        for (int num : nums) {
+            if (num > maxVal) {
+                maxVal = num;       
+                currentLength = 1;  
+                maxLength = 1;     
+            } 
+    
+            else if (num == maxVal) {
+                currentLength++;
+                maxLength = Math.max(maxLength, currentLength);
+            } 
+        
+            else {
+                currentLength = 0;
+            }
+        }
+
+        return maxLength;
+    }
+}
