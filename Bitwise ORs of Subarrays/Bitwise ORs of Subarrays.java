50cb41e3121875886bcc59828690a0193d936b4f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-31 13:12:49 +0530

    Update Bitwise ORs of Subarrays.java

--- Bitwise ORs of Subarrays/Bitwise ORs of Subarrays.java
+++ Bitwise ORs of Subarrays/Bitwise ORs of Subarrays.java
@@ -1,1 +1,25 @@
 // Solution for Bitwise ORs of Subarrays in JAVA
+
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> prevOrs = new HashSet<>();
+        Set<Integer> currOrs = new HashSet<>();
+        Set<Integer> distinctOrs = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            currOrs.add(arr[i]);
+            distinctOrs.add(arr[i]);
+
+            for (int prevVal : prevOrs) {
+                int newOr = prevVal | arr[i];
+                currOrs.add(newOr);
+                distinctOrs.add(newOr);
+            }
+
+            prevOrs = new HashSet<>(currOrs);
+            currOrs.clear();
+        }
+
+        return distinctOrs.size();
+    }
+}
