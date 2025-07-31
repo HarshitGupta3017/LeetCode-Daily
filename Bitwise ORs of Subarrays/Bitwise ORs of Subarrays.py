97eb6fc8ab6bd8c6edf83bdddc0f4ba0a1d8d489
Harshit Gupta <harshitgupta3017@gmail.com>
2025-07-31 13:12:38 +0530

    Update Bitwise ORs of Subarrays.py

--- Bitwise ORs of Subarrays/Bitwise ORs of Subarrays.py
+++ Bitwise ORs of Subarrays/Bitwise ORs of Subarrays.py
@@ -1,1 +1,21 @@
 # Solution for Bitwise ORs of Subarrays in PY
+
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        prevOrs = set()
+        currOrs = set()
+        distinctOrs = set()
+
+        for i in range(len(arr)):
+            currOrs.add(arr[i])
+            distinctOrs.add(arr[i])
+
+            for prevVal in prevOrs:
+                newOr = prevVal | arr[i]
+                currOrs.add(newOr)
+                distinctOrs.add(newOr)
+
+            prevOrs = currOrs.copy()
+            currOrs.clear()
+
+        return len(distinctOrs)
