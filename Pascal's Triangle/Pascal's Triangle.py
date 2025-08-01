f041c236fc050017f3d83af8f0a9f3fb7c28e0ab
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-01 18:17:41 +0530

    Update Pascal's Triangle.py

--- Pascal's Triangle/Pascal's Triangle.py
+++ Pascal's Triangle/Pascal's Triangle.py
@@ -1,1 +1,11 @@
 # Solution for Pascal's Triangle in PY
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for i in range(numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = result[i - 1][j] + result[i - 1][j - 1]
+            result.append(row)
+        return result
