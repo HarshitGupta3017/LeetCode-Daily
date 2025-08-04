74338db3b1a97ee7bdb3e2fd49787d7c83e8044a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-04 19:03:25 +0530

    Update Fruit Into Baskets.py

--- Fruit Into Baskets/Fruit Into Baskets.py
+++ Fruit Into Baskets/Fruit Into Baskets.py
@@ -1,1 +1,22 @@
 # Solution for Fruit Into Baskets in PY
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        from collections import defaultdict
+
+        fruitCount = defaultdict(int)
+        windowStart = 0
+        maxFruitsCollected = 0
+
+        for windowEnd in range(len(fruits)):
+            fruitCount[fruits[windowEnd]] += 1
+
+            while len(fruitCount) > 2:
+                fruitCount[fruits[windowStart]] -= 1
+                if fruitCount[fruits[windowStart]] == 0:
+                    del fruitCount[fruits[windowStart]]
+                windowStart += 1
+
+            maxFruitsCollected = max(maxFruitsCollected, windowEnd - windowStart + 1)
+
+        return maxFruitsCollected
