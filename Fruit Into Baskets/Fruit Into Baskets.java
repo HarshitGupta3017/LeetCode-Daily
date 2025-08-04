9b35c6778f2d49468c69d762ae183afe68a260bf
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-04 19:03:54 +0530

    Update Fruit Into Baskets.java

--- Fruit Into Baskets/Fruit Into Baskets.java
+++ Fruit Into Baskets/Fruit Into Baskets.java
@@ -1,1 +1,25 @@
 // Solution for Fruit Into Baskets in JAVA
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> fruitCount = new HashMap<>();
+        int windowStart = 0;
+        int maxFruitsCollected = 0;
+
+        for (int windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+            fruitCount.put(fruits[windowEnd], fruitCount.getOrDefault(fruits[windowEnd], 0) + 1);
+
+            while (fruitCount.size() > 2) {
+                fruitCount.put(fruits[windowStart], fruitCount.get(fruits[windowStart]) - 1);
+                if (fruitCount.get(fruits[windowStart]) == 0) {
+                    fruitCount.remove(fruits[windowStart]);
+                }
+                windowStart++;
+            }
+
+            maxFruitsCollected = Math.max(maxFruitsCollected, windowEnd - windowStart + 1);
+        }
+
+        return maxFruitsCollected;
+    }
+}
