11510f9ac809091f8c46a10bd04efcb89da28061
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-05 19:35:54 +0530

    Update Fruits Into Baskets II.py

--- Fruits Into Baskets II/Fruits Into Baskets II.py
+++ Fruits Into Baskets II/Fruits Into Baskets II.py
@@ -1,1 +1,21 @@
 # Solution for Fruits Into Baskets II in PY
+
+class Solution:
+    def numOfUnplacedFruits(self, fruits, baskets):
+        totalFruits = len(fruits)
+        totalBaskets = len(baskets)
+
+        isBasketUsed = [False] * totalBaskets  # Track used baskets
+        unplacedFruitCount = 0
+
+        for i in range(totalFruits):
+            isPlaced = False
+            for j in range(totalBaskets):
+                if not isBasketUsed[j] and baskets[j] >= fruits[i]:
+                    isBasketUsed[j] = True
+                    isPlaced = True
+                    break
+            if not isPlaced:
+                unplacedFruitCount += 1
+
+        return unplacedFruitCount
