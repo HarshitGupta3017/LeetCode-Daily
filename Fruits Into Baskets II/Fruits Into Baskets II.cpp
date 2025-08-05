24de8139fd5762f6890939e291afe4552f907de6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-05 19:34:45 +0530

    Update Fruits Into Baskets II.cpp

--- Fruits Into Baskets II/Fruits Into Baskets II.cpp
+++ Fruits Into Baskets II/Fruits Into Baskets II.cpp
@@ -1,1 +1,34 @@
 // Solution for Fruits Into Baskets II in CPP
+
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int totalFruits = fruits.size();     // Total number of fruit types
+        int totalBaskets = baskets.size();   // Total number of baskets
+
+        vector<bool> isBasketUsed(totalBaskets, false);  // Track whether a basket is already used
+
+        int unplacedFruitCount = 0;          // Counter for fruits that could not be placed
+
+        // Try placing each fruit in the first available basket that can hold it
+        for (int i = 0; i < totalFruits; i++) {
+            bool isPlaced = false;
+
+            for (int j = 0; j < totalBaskets; j++) {
+                // If the basket is unused and can hold the current fruit quantity
+                if (!isBasketUsed[j] && baskets[j] >= fruits[i]) {
+                    isBasketUsed[j] = true;  // Mark the basket as used
+                    isPlaced = true;
+                    break;                   // Move to next fruit
+                }
+            }
+
+            // If no suitable basket found for the current fruit
+            if (!isPlaced) {
+                unplacedFruitCount++;
+            }
+        }
+
+        return unplacedFruitCount;
+    }
+};
