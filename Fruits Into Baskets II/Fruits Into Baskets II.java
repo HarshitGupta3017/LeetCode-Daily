49f3ef070104d5734ebd92bc6133605f920f0bec
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-05 19:35:15 +0530

    Update Fruits Into Baskets II.java

--- Fruits Into Baskets II/Fruits Into Baskets II.java
+++ Fruits Into Baskets II/Fruits Into Baskets II.java
@@ -1,1 +1,27 @@
 // Solution for Fruits Into Baskets II in JAVA
+
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int totalFruits = fruits.length;
+        int totalBaskets = baskets.length;
+
+        boolean[] isBasketUsed = new boolean[totalBaskets]; // Track used baskets
+        int unplacedFruitCount = 0;
+
+        for (int i = 0; i < totalFruits; i++) {
+            boolean isPlaced = false;
+            for (int j = 0; j < totalBaskets; j++) {
+                if (!isBasketUsed[j] && baskets[j] >= fruits[i]) {
+                    isBasketUsed[j] = true;
+                    isPlaced = true;
+                    break;
+                }
+            }
+            if (!isPlaced) {
+                unplacedFruitCount++;
+            }
+        }
+
+        return unplacedFruitCount;
+    }
+}
