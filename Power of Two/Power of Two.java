0117a05f0bf3588b2dd80b0f11f3de1a3b1dab34
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-09 12:38:07 +0530

    Update Power of Two.java

--- Power of Two/Power of Two.java
+++ Power of Two/Power of Two.java
@@ -1,1 +1,8 @@
 // Solution for Power of Two in JAVA
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return Integer.bitCount(n) == 1;
+    }
+}
