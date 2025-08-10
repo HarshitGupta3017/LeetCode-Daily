493142b40cd51e6894757b2b5aed9de1f772c796
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-10 18:04:46 +0530

    Update Reordered Power of 2.py

--- Reordered Power of 2/Reordered Power of 2.py
+++ Reordered Power of 2/Reordered Power of 2.py
@@ -1,1 +1,13 @@
 # Solution for Reordered Power of 2 in PY
+
+class Solution:
+    def getDigitSignature(self, num):
+        sig = 0
+        while num:
+            sig += 10 ** (num % 10)
+            num //= 10
+        return sig
+
+    def reorderedPowerOf2(self, n: int) -> bool:
+        sig = self.getDigitSignature(n)
+        return any(sig == self.getDigitSignature(1 << p) for p in range(30))
