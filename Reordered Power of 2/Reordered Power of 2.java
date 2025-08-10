6b3fb1744364a4114cad08dd0f0969365c1190c0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-10 18:04:57 +0530

    Update Reordered Power of 2.java

--- Reordered Power of 2/Reordered Power of 2.java
+++ Reordered Power of 2/Reordered Power of 2.java
@@ -1,1 +1,20 @@
 // Solution for Reordered Power of 2 in JAVA
+
+class Solution {
+    private int getDigitSignature(int num) {
+        int sig = 0;
+        while (num > 0) {
+            sig += Math.pow(10, num % 10);
+            num /= 10;
+        }
+        return sig;
+    }
+
+    public boolean reorderedPowerOf2(int n) {
+        int sig = getDigitSignature(n);
+        for (int p = 0; p < 30; p++) {
+            if (sig == getDigitSignature(1 << p)) return true;
+        }
+        return false;
+    }
+}
