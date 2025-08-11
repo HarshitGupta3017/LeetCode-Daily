b4ba4c24c14d13eea0381faa4e124fb02a3577ca
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-11 17:59:03 +0530

    Update Range Product Queries of Powers.py

--- Range Product Queries of Powers/Range Product Queries of Powers.py
+++ Range Product Queries of Powers/Range Product Queries of Powers.py
@@ -1,1 +1,16 @@
 # Solution for Range Product Queries of Powers in PY
+
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        MOD = 10**9+7
+
+        powers_of_two = [1 << bit_position for bit_position in range(32) if n & (1 << bit_position)]
+
+        queryResults = []
+
+        for leftIdx, rightIdx in queries: 
+            product = prod(powers_of_two[idx] for idx in range(leftIdx, rightIdx + 1)) % MOD
+            
+            queryResults.append(product) 
+
+        return queryResults
