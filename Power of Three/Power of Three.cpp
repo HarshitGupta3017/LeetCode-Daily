60e74b4de6d91253f7124c2f75a5467210de1716
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-13 19:30:08 +0530

    Update Power of Three.cpp

--- Power of Three/Power of Three.cpp
+++ Power of Three/Power of Three.cpp
@@ -1,1 +1,9 @@
 // Solution for Power of Three in CPP
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        // 3^19 (max valid integer range) = 1162261467
+        return (n > 0) && (1162261467 % n == 0);
+    }
+};
