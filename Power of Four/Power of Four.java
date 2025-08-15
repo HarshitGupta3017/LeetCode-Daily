cebec5168aca15e02bfb28ae7246a28b68f4a549
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-15 12:46:23 +0530

    Update Power of Four.java

--- Power of Four/Power of Four.java
+++ Power of Four/Power of Four.java
@@ -1,1 +1,9 @@
 // Solution for Power of Four in JAVA
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        double x = Math.log10(n) / Math.log10(4);
+        return x == (int) x;
+    }
+}
