d0dc09abbe5502e409d773aa45743c9bcec067bc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-15 12:46:34 +0530

    Update Power of Four.py

--- Power of Four/Power of Four.py
+++ Power of Four/Power of Four.py
@@ -1,1 +1,8 @@
 # Solution for Power of Four in PY
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        x = math.log10(n) / math.log10(4)
+        return x == int(x)
