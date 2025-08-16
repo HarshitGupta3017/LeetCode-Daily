8bf83a1c736319ae31f6b12b1ed6ac6715a439fc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-16 17:24:59 +0530

    Update Maximum 69 Number.py

--- Maximum 69 Number/Maximum 69 Number.py
+++ Maximum 69 Number/Maximum 69 Number.py
@@ -1,1 +1,16 @@
 # Solution for Maximum 69 Number in PY
+
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        tempNum = num
+        digitPosition = 0
+        leftmostSixPosition = -1
+
+        while tempNum > 0:
+            currentDigit = tempNum % 10
+            if currentDigit == 6:
+                leftmostSixPosition = digitPosition
+            tempNum //= 10
+            digitPosition += 1
+
+        return num if leftmostSixPosition == -1 else num + 3 * (10 ** leftmostSixPosition)
