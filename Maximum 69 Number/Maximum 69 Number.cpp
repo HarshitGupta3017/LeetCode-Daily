47cb2634688d4ace9cff73b1218e79dd3623f545
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-16 17:23:29 +0530

    Update Maximum 69 Number.cpp

--- Maximum 69 Number/Maximum 69 Number.cpp
+++ Maximum 69 Number/Maximum 69 Number.cpp
@@ -1,1 +1,29 @@
 // Solution for Maximum 69 Number in CPP
+
+class Solution {
+public:
+    int maximum69Number(int num) {
+        int tempNum = num;                 // Copy of the number for processing
+        int digitPosition = 0;             // Tracks current digit position (units, tens, etc.)
+        int leftmostSixPosition = -1;      // Stores the position of the *leftmost* 6 found
+        
+        // Traverse through all digits of the number
+        while (tempNum > 0) {
+            int currentDigit = tempNum % 10;   // Extract last digit
+            
+            // If digit is 6, update its position
+            if (currentDigit == 6) {
+                leftmostSixPosition = digitPosition;
+            }
+            
+            tempNum /= 10;   // Remove last digit
+            digitPosition++; // Move to next digit position
+        }
+        
+        // If no '6' was found, return original number
+        // Otherwise, change the *leftmost 6* into a 9
+        return (leftmostSixPosition == -1) 
+            ? num 
+            : num + 3 * pow(10, leftmostSixPosition);
+    }
+};
