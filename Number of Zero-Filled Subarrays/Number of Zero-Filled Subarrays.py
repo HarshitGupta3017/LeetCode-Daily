c073211d9bc6c00022e757419bedeca87b3b40f6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-19 14:39:44 +0530

    Update Number of Zero-Filled Subarrays.py

--- Number of Zero-Filled Subarrays/Number of Zero-Filled Subarrays.py
+++ Number of Zero-Filled Subarrays/Number of Zero-Filled Subarrays.py
@@ -1,1 +1,9 @@
 # Solution for Number of Zero-Filled Subarrays in PY
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        result = count = 0
+        for num in nums:
+            count = 0 if num != 0 else count + 1
+            result += count
+        return result
