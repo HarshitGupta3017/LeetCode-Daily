b5fa56d2a7ec7e99a0d769ec9a1407fae1eede28
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-19 14:39:59 +0530

    Update Number of Zero-Filled Subarrays.java

--- Number of Zero-Filled Subarrays/Number of Zero-Filled Subarrays.java
+++ Number of Zero-Filled Subarrays/Number of Zero-Filled Subarrays.java
@@ -1,1 +1,13 @@
 // Solution for Number of Zero-Filled Subarrays in JAVA
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long result = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = (nums[i] == 0) ? count + 1 : 0;
+            result += count;
+        }   
+        return result;
+    }
+}
