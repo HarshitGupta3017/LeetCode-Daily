00fa5515b59c487db0766f87c5795bbb56466f05
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-21 18:20:04 +0530

    Update Count Submatrices With All Ones.java

--- Count Submatrices With All Ones/Count Submatrices With All Ones.java
+++ Count Submatrices With All Ones/Count Submatrices With All Ones.java
@@ -1,1 +1,28 @@
 // Solution for Count Submatrices With All Ones in JAVA
+
+class Solution {
+    private int countSubarraysWithOnes(int[] arr) {
+        int ones = 0, total = 0;
+        for (int val : arr) {
+            if (val == 0) ones = 0;
+            else ones++;
+            total += ones;
+        }
+        return total;
+    }
+
+    public int numSubmat(int[][] mat) {
+        int rows = mat.length, cols = mat[0].length, total = 0;
+        int[] mask = new int[cols];
+        for (int i = 0; i < rows; i++) {
+            Arrays.fill(mask, 1);
+            for (int j = i; j < rows; j++) {
+                for (int k = 0; k < cols; k++) {
+                    mask[k] &= mat[j][k];
+                }
+                total += countSubarraysWithOnes(mask);
+            }
+        }
+        return total;
+    }
+}
