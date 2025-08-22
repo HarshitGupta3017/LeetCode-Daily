f1ac0300e1b16cc3aa41e58aaea19d27e3d4c1d5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-22 19:58:54 +0530

    Update Find the Minimum Area to Cover All Ones I.py

--- Find the Minimum Area to Cover All Ones I/Find the Minimum Area to Cover All Ones I.py
+++ Find the Minimum Area to Cover All Ones I/Find the Minimum Area to Cover All Ones I.py
@@ -1,1 +1,14 @@
 # Solution for Find the Minimum Area to Cover All Ones I in PY
+
+class Solution:
+    def minimumArea(self, grid: list[list[int]]) -> int:
+        totalRows, totalCols = len(grid), len(grid[0])
+        minRow, maxRow, minCol, maxCol = totalRows, -1, totalCols, -1
+        for row in range(totalRows):
+            for col in range(totalCols):
+                if grid[row][col] == 1:
+                    minRow = min(minRow, row)
+                    maxRow = max(maxRow, row)
+                    minCol = min(minCol, col)
+                    maxCol = max(maxCol, col)
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1)
