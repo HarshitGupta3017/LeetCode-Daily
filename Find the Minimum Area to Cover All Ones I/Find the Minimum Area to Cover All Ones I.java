c0ec8fac4b8db90666c2165e95a1e77995ca61d8
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-22 19:58:40 +0530

    Update Find the Minimum Area to Cover All Ones I.java

--- Find the Minimum Area to Cover All Ones I/Find the Minimum Area to Cover All Ones I.java
+++ Find the Minimum Area to Cover All Ones I/Find the Minimum Area to Cover All Ones I.java
@@ -1,1 +1,20 @@
 // Solution for Find the Minimum Area to Cover All Ones I in JAVA
+
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int totalRows = grid.length;
+        int totalCols = grid[0].length;
+        int minRow = totalRows, maxRow = -1, minCol = totalCols, maxCol = -1;
+        for (int row = 0; row < totalRows; row++) {
+            for (int col = 0; col < totalCols; col++) {
+                if (grid[row][col] == 1) {
+                    minRow = Math.min(minRow, row);
+                    maxRow = Math.max(maxRow, row);
+                    minCol = Math.min(minCol, col);
+                    maxCol = Math.max(maxCol, col);
+                }
+            }
+        }
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1);
+    }
+}
