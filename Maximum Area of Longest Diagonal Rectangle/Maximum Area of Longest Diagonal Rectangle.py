344a071a8fd604e6402db536ee50850ba692243c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-26 14:53:51 +0530

    Update Maximum Area of Longest Diagonal Rectangle.py

--- Maximum Area of Longest Diagonal Rectangle/Maximum Area of Longest Diagonal Rectangle.py
+++ Maximum Area of Longest Diagonal Rectangle/Maximum Area of Longest Diagonal Rectangle.py
@@ -1,1 +1,18 @@
 # Solution for Maximum Area of Longest Diagonal Rectangle in PY
+
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        maxDiag, maxArea = 0, 0
+        n = len(dimensions)
+        
+        for l, w in dimensions:
+            diag = l*l + w*w
+            area = l * w
+            if diag > maxDiag:
+                maxDiag = diag
+                maxArea = area
+            
+            elif diag == maxDiag:
+                maxArea = max(maxArea, area)
+            
+        return maxArea
