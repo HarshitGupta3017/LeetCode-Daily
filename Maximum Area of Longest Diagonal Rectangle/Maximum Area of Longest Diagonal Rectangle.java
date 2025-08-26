7b92da2a589acbc290e8988a78d907c9eedb1770
Harshit Gupta <harshitgupta3017@gmail.com>
2025-08-26 14:54:01 +0530

    Update Maximum Area of Longest Diagonal Rectangle.java

--- Maximum Area of Longest Diagonal Rectangle/Maximum Area of Longest Diagonal Rectangle.java
+++ Maximum Area of Longest Diagonal Rectangle/Maximum Area of Longest Diagonal Rectangle.java
@@ -1,1 +1,21 @@
 // Solution for Maximum Area of Longest Diagonal Rectangle in JAVA
+
+class Solution {
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        int maxDiag = 0, maxArea = 0;
+        int n = dimensions.length;
+        for (int i = 0; i < n; i++) {
+            int l = dimensions[i][0];
+            int w = dimensions[i][1];
+            int diag = l*l + w*w;
+            int area = l * w;
+            if (diag > maxDiag) {
+                maxDiag = diag;
+                maxArea = area;
+            } else if (diag == maxDiag) {
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+}
