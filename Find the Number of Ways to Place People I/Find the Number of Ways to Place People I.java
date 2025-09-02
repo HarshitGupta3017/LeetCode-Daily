e7dc85ce98877a14c2a45e55b3f6d2a2012aac45
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-02 20:14:32 +0530

    Update Find the Number of Ways to Place People I.java

--- Find the Number of Ways to Place People I/Find the Number of Ways to Place People I.java
+++ Find the Number of Ways to Place People I/Find the Number of Ways to Place People I.java
@@ -1,1 +1,38 @@
 // Solution for Find the Number of Ways to Place People I in JAVA
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        int result = 0;
+
+        // Sort by x ascending, if x same then y descending
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+
+        for (int i = 0; i < n; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            int maxY = Integer.MIN_VALUE; // track highest y seen so far for valid B
+
+            for (int j = i + 1; j < n; j++) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                // Condition 1: A must be above B
+                if (y2 > y1) continue;
+
+                // Condition 2: ensure no blocking point in between
+                if (y2 > maxY) {
+                    result++;
+                    maxY = y2;
+                }
+            }
+        }
+        return result;
+    }
+}
