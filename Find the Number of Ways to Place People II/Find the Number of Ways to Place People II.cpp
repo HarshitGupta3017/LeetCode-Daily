536d5cd262a889bfe98ed2bf279664cc61ff9a5e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-03 18:52:29 +0530

    Update Find the Number of Ways to Place People II.cpp

--- Find the Number of Ways to Place People II/Find the Number of Ways to Place People II.cpp
+++ Find the Number of Ways to Place People II/Find the Number of Ways to Place People II.cpp
@@ -1,1 +1,45 @@
 // Solution for Find the Number of Ways to Place People II in CPP
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        int result = 0;
+
+        // Sorting points:
+        //   - By increasing x
+        //   - If same x, higher y first
+        auto lambda = [](auto& p1, auto& p2){
+            if (p1[0] == p2[0]) {
+                return p1[1] > p2[1];
+            }
+            return p1[0] < p2[0];
+        };
+        sort(points.begin(), points.end(), lambda);
+
+        for (int i = 0; i < n; i++) {
+            // Consider current point as Alice
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            
+            int maxY = INT_MIN;  // Keeps track of "highest y so far" among valid B points
+
+            for (int j = i+1; j < n; j++) {
+                // Candidate Bob
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                // Condition 1: A must be above B
+                if (y2 > y1) continue; // B is above A → not valid
+
+                // Condition 2: No other point in between
+                // We ensure this by checking `maxY`
+                if (y2 > maxY) {
+                    result++;    // Found a valid (A, B)
+                    maxY = y2;   // Update maxY boundary
+                }
+            }
+        }
+        return result;
+    }
+};
