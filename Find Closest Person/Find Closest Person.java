1eab52c4d9d58a4575418909d6da3252033dddac
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-04 14:48:14 +0530

    Update Find Closest Person.java

--- Find Closest Person/Find Closest Person.java
+++ Find Closest Person/Find Closest Person.java
@@ -1,1 +1,14 @@
 // Solution for Find Closest Person in JAVA
+
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int person1diff = Math.abs(z - x);
+        int person2diff = Math.abs(z - y);
+        if (person1diff < person2diff) {
+            return 1;
+        } else if (person1diff > person2diff) {
+            return 2;
+        } 
+        return 0;
+    }
+}
