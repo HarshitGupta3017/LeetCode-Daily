ca90c2514de871f23ca43aef07840bcd6496ccdc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-04 14:48:25 +0530

    Update Find Closest Person.cpp

--- Find Closest Person/Find Closest Person.cpp
+++ Find Closest Person/Find Closest Person.cpp
@@ -1,1 +1,15 @@
 // Solution for Find Closest Person in CPP
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int person1diff = abs(z - x);
+        int person2diff = abs(z - y);
+        if (person1diff < person2diff) {
+            return 1;
+        } else if (person1diff > person2diff) {
+            return 2;
+        } 
+        return 0;
+    }
+};
