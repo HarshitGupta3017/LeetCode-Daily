828a89f1f966d24bba51ed06d6c8527eba747f3d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-04 14:47:47 +0530

    Update Find Closest Person.py

--- Find Closest Person/Find Closest Person.py
+++ Find Closest Person/Find Closest Person.py
@@ -1,1 +1,10 @@
 # Solution for Find Closest Person in PY
+
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        person1diff, person2diff = abs(z - x), abs(z - y)
+        if person1diff < person2diff:
+            return 1
+        elif person1diff > person2diff:
+            return 2
+        return 0
