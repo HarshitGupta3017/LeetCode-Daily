0d988accca812076c2b32bb587e78d70b3835fe5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-07 12:07:49 +0530

    Update Find N Unique Integers Sum up to Zero.java

--- Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.java
+++ Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.java
@@ -1,1 +1,15 @@
 // Solution for Find N Unique Integers Sum up to Zero in JAVA
+
+class Solution {
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+        int idx = 0;
+        
+        for (int num = -(n / 2); num <= (n / 2); num++) {
+            if (num == 0 && n % 2 == 0) continue;
+            result[idx++] = num;
+        }
+        
+        return result;
+    }
+}
