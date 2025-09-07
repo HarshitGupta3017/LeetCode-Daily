1ce2062ce2ba5f80d9fd163ea6f382db4c8655fa
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-07 12:07:58 +0530

    Update Find N Unique Integers Sum up to Zero.cpp

--- Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.cpp
+++ Find N Unique Integers Sum up to Zero/Find N Unique Integers Sum up to Zero.cpp
@@ -1,1 +1,22 @@
 // Solution for Find N Unique Integers Sum up to Zero in CPP
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> result;
+        
+        // We want n unique integers that sum to 0.
+        // Idea:
+        //   - Pair numbers symmetrically: (-x, +x)
+        //   - If n is odd, also include 0 in the middle
+        
+        for (int num = -(n / 2); num <= (n / 2); num++) {
+            // If n is even, skip 0 (otherwise sum won't stay zero-balanced)
+            if (num == 0 && n % 2 == 0) continue;
+            
+            result.push_back(num);
+        }
+        
+        return result;
+    }
+};
