876822b6352a40289650db941589d148ad2e11f0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-08 12:23:10 +0530

    Update Convert Integer to the Sum of Two No-Zero Integers.cpp

--- Convert Integer to the Sum of Two No-Zero Integers/Convert Integer to the Sum of Two No-Zero Integers.cpp
+++ Convert Integer to the Sum of Two No-Zero Integers/Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -1,1 +1,14 @@
 // Solution for Convert Integer to the Sum of Two No-Zero Integers in CPP
+
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if ((to_string(a) + to_string(b)).find('0') == string::npos) {
+                return {a, b};
+            }
+        }
+        return {};
+    }
+};
