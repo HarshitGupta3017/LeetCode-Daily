ad57490c5bc908e90712beed811fc5a3402e54cc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-08 12:23:36 +0530

    Update Convert Integer to the Sum of Two No-Zero Integers.py

--- Convert Integer to the Sum of Two No-Zero Integers/Convert Integer to the Sum of Two No-Zero Integers.py
+++ Convert Integer to the Sum of Two No-Zero Integers/Convert Integer to the Sum of Two No-Zero Integers.py
@@ -1,1 +1,5 @@
 # Solution for Convert Integer to the Sum of Two No-Zero Integers in PY
+
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        return next([a, n-a] for a in range(1, n) if "0" not in str(a) and "0" not in str(n-a))
