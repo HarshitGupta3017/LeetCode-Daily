2954034881c49f3e8fbe910754e1f03e680c5143
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-10 19:28:05 +0530

    Update Minimum Number of People to Teach.py

--- Minimum Number of People to Teach/Minimum Number of People to Teach.py
+++ Minimum Number of People to Teach/Minimum Number of People to Teach.py
@@ -1,1 +1,12 @@
 # Solution for Minimum Number of People to Teach in PY
+
+class Solution:
+    def minimumTeachings(self, n: int, userLanguages: list[list[int]], friendships: list[list[int]]) -> int:
+        sad = set()
+        for a,b in friendships:
+            a-=1; b-=1
+            if not set(userLanguages[a]) & set(userLanguages[b]):
+                sad.add(a); sad.add(b)
+        from collections import Counter
+        freq = Counter(l for u in sad for l in userLanguages[u])
+        return len(sad) - (max(freq.values()) if freq else 0)
