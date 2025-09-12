f16c9862d5cbf6371ac800fa6cc0f88978981946
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-12 16:58:00 +0530

    Update Vowels Game in a String.java

--- Vowels Game in a String/Vowels Game in a String.java
+++ Vowels Game in a String/Vowels Game in a String.java
@@ -1,1 +1,7 @@
 // Solution for Vowels Game in a String in JAVA
+
+class Solution {
+    public boolean doesAliceWin(String s) {
+        return s.chars().anyMatch(ch -> "aeiou".indexOf(ch) >= 0);
+    }
+}
