790f29e284d2fd8a6c7b03406184ae30a7313571
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-13 12:35:04 +0530

    Update Find Most Frequent Vowel and Consonant.cpp

--- Find Most Frequent Vowel and Consonant/Find Most Frequent Vowel and Consonant.cpp
+++ Find Most Frequent Vowel and Consonant/Find Most Frequent Vowel and Consonant.cpp
@@ -1,1 +1,26 @@
 // Solution for Find Most Frequent Vowel and Consonant in CPP
+
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        int maxConsonantFreq = 0; // highest frequency of consonants
+        int maxVowelFreq = 0;     // highest frequency of vowels
+        int frequency[26] = {0};  // frequency of each character 'a' to 'z'
+
+        for (char ch : s) {
+            frequency[ch - 'a']++; // update count of this character
+
+            // Check if it's a vowel
+            if (string("aeiou").find(ch) != string::npos) {
+                maxVowelFreq = max(maxVowelFreq, frequency[ch - 'a']);
+            } 
+            // Otherwise, it's a consonant
+            else {
+                maxConsonantFreq = max(maxConsonantFreq, frequency[ch - 'a']);
+            }
+        }
+
+        // Return sum of max vowel freq + max consonant freq
+        return maxConsonantFreq + maxVowelFreq;
+    }
+};
