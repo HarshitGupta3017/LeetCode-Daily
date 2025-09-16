269b70ebfa266d106940e70aa810ce141c7f1b5f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-16 15:53:58 +0530

    Update Replace Non-Coprime Numbers in Array.java

--- Replace Non-Coprime Numbers in Array/Replace Non-Coprime Numbers in Array.java
+++ Replace Non-Coprime Numbers in Array/Replace Non-Coprime Numbers in Array.java
@@ -1,1 +1,33 @@
 // Solution for Replace Non-Coprime Numbers in Array in JAVA
+
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        List<Integer> finalArray = new ArrayList<>();
+
+        for (int num : nums) {
+            // Merge until coprime
+            while (!finalArray.isEmpty()) {
+                int lastNum = finalArray.get(finalArray.size() - 1);
+                int currentGCD = gcd(lastNum, num);
+
+                if (currentGCD == 1) break;
+
+                finalArray.remove(finalArray.size() - 1);
+                long lcmValue = 1L * lastNum / currentGCD * num;
+                num = (int) lcmValue;
+            }
+            finalArray.add(num);
+        }
+
+        return finalArray;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
