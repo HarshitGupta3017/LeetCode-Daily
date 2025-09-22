da5b7f9b784786b7dc6ae55c99b836fd8458c89f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-22 14:47:00 +0530

    Update Count Elements With Maximum Frequency.java

--- Count Elements With Maximum Frequency/Count Elements With Maximum Frequency.java
+++ Count Elements With Maximum Frequency/Count Elements With Maximum Frequency.java
@@ -1,1 +1,19 @@
 // Solution for Count Elements With Maximum Frequency in JAVA
+
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int[] freq = new int[101];
+        int maxFreq = 0, total = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+            if (freq[num] > maxFreq) {
+                maxFreq = freq[num];
+                total = maxFreq;
+            } else if (freq[num] == maxFreq) {
+                total += maxFreq;
+            }
+        }
+        return total;
+    }
+}
