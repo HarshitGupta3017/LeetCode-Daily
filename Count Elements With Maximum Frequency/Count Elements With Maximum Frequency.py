9eb50452d93a2c622ef707afff584356de9a9daa
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-22 14:46:50 +0530

    Update Count Elements With Maximum Frequency.py

--- Count Elements With Maximum Frequency/Count Elements With Maximum Frequency.py
+++ Count Elements With Maximum Frequency/Count Elements With Maximum Frequency.py
@@ -1,1 +1,15 @@
 # Solution for Count Elements With Maximum Frequency in PY
+
+class Solution:
+    def maxFrequencyElements(self, nums: list[int]) -> int:
+        freq = [0] * 101
+        maxFreq = total = 0
+
+        for num in nums:
+            freq[num] += 1
+            if freq[num] > maxFreq:
+                maxFreq = freq[num]
+                total = maxFreq
+            elif freq[num] == maxFreq:
+                total += maxFreq
+        return total
