212e32b774a75bf9be815b0d5d9853a13afb1dca
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-25 17:37:24 +0530

    Update Triangle.py

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -1,1 +1,11 @@
 # Solution for Triangle in PY
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1] # last row
+        
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(row + 1):
+                dp[col] = triangle[row][col] + min(dp[col], dp[col + 1])
+    
+        return dp[0]
