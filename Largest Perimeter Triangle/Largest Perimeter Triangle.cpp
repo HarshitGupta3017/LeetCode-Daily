581bf0ddba60334af2b2807a91a7ccbc8bc9d8b6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-28 10:56:28 +0530

    Update Largest Perimeter Triangle.cpp

--- Largest Perimeter Triangle/Largest Perimeter Triangle.cpp
+++ Largest Perimeter Triangle/Largest Perimeter Triangle.cpp
@@ -1,1 +1,17 @@
 // Solution for Largest Perimeter Triangle in CPP
+
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        for (int i = nums.size() - 1; i >= 2; i--) {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            if (a + b > c) {
+                return a + b + c;
+            }
+        }
+        return 0;
+    }
+};
