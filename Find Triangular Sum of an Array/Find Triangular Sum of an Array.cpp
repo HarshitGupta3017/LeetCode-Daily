59533888d83e7db6a9e4b84c50ced013ded7dd1c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-30 12:11:09 +0530

    Update Find Triangular Sum of an Array.cpp

--- Find Triangular Sum of an Array/Find Triangular Sum of an Array.cpp
+++ Find Triangular Sum of an Array/Find Triangular Sum of an Array.cpp
@@ -1,1 +1,24 @@
 // Solution for Find Triangular Sum of an Array in CPP
+
+class Solution {
+public:
+    // Function to compute the triangular sum of nums
+    int triangularSum(vector<int>& nums) {
+        // Keep reducing nums until only one element remains
+        while (nums.size() > 1) {
+            // Create a temporary vector of reduced size
+            vector<int> reduced(nums.size() - 1);
+            
+            // Compute each element in the reduced array
+            for (int i = 0; i < nums.size() - 1; i++) {
+                reduced[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            
+            // Replace nums with reduced version
+            nums = reduced;
+        }
+        
+        // Final remaining element is the triangular sum
+        return nums[0];
+    }
+};
