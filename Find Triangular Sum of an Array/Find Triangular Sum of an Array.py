a76c337bc7fd90c84b85ace0c799a23d347d3de2
Harshit Gupta <harshitgupta3017@gmail.com>
2025-09-30 12:10:44 +0530

    Update Find Triangular Sum of an Array.py

--- Find Triangular Sum of an Array/Find Triangular Sum of an Array.py
+++ Find Triangular Sum of an Array/Find Triangular Sum of an Array.py
@@ -1,1 +1,10 @@
 # Solution for Find Triangular Sum of an Array in PY
+
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        while n > 1:
+            for i in range(n - 1):
+                nums[i] = (nums[i] + nums[i + 1]) % 10
+            n -= 1
+        return nums[0]
