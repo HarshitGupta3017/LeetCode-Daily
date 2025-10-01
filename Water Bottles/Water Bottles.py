dcae330ca0dd0730e4e8944c0a2ade23b6e23418
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-01 17:40:25 +0530

    Update Water Bottles.py

--- Water Bottles/Water Bottles.py
+++ Water Bottles/Water Bottles.py
@@ -1,1 +1,5 @@
 # Solution for Water Bottles in PY
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        return numBottles + (numBottles - 1) // (numExchange - 1)
