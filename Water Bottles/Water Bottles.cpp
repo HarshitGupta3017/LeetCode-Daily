4946d35cb2da4507506b3cedc556b601f1fd922e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-01 17:39:49 +0530

    Update Water Bottles.cpp

--- Water Bottles/Water Bottles.cpp
+++ Water Bottles/Water Bottles.cpp
@@ -1,1 +1,8 @@
 // Solution for Water Bottles in CPP
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles + (numBottles - 1) / (numExchange - 1);
+    }
+};
