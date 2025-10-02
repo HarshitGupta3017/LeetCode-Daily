322192f731bafa7b433d2459258323f746cf121e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-02 11:36:55 +0530

    Update Water Bottles II.py

--- Water Bottles II/Water Bottles II.py
+++ Water Bottles II/Water Bottles II.py
@@ -1,1 +1,17 @@
 # Solution for Water Bottles II in PY
+
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        totalDrank = 0
+
+        while (numBottles >= numExchange) :
+            
+            totalDrank += numExchange
+
+            numBottles -= numExchange
+
+            numBottles += 1
+
+            numExchange += 1
+
+        return totalDrank + numBottles
