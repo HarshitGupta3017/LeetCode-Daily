6224b1a029edfbac5c1f51302abede1560c89765
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-02 11:37:07 +0530

    Update Water Bottles II.java

--- Water Bottles II/Water Bottles II.java
+++ Water Bottles II/Water Bottles II.java
@@ -1,1 +1,26 @@
 // Solution for Water Bottles II in JAVA
+
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int totalDrank = 0;  // Total number of bottles drunk
+
+        // While we still have enough bottles to perform an exchange
+        while (numBottles >= numExchange) {
+            
+            // Drink exactly 'numExchange' bottles
+            totalDrank += numExchange;
+
+            // After drinking, those bottles become empty, so subtract from full
+            numBottles -= numExchange;
+
+            // Exchange the empty bottles for 1 new full bottle
+            numBottles += 1;
+
+            // Increase the exchange cost by 1 (it becomes harder each time)
+            numExchange++;
+        }
+
+        // Drink the remaining bottles that can’t be exchanged anymore
+        return totalDrank + numBottles;
+    }
+}
