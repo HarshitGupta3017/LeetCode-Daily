93c76c6009be42e62c5971f0b7528bcc5697f8f1
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-04 13:55:19 +0530

    Update Container With Most Water.py

--- Container With Most Water/Container With Most Water.py
+++ Container With Most Water/Container With Most Water.py
@@ -1,1 +1,10 @@
 # Solution for Container With Most Water in PY
+
+class Solution:
+    def maxArea(self, h: List[int]) -> int:
+        l, r, res = 0, len(h)-1, 0
+        while l < r:
+            res = max(res, (r-l) * min(h[l], h[r]))
+            if h[l] < h[r]: l += 1
+            else: r -= 1
+        return res
