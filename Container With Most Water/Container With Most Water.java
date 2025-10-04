d339161a2eb7360bb4524fbc8e469086e8b06e39
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-04 13:55:28 +0530

    Update Container With Most Water.java

--- Container With Most Water/Container With Most Water.java
+++ Container With Most Water/Container With Most Water.java
@@ -1,1 +1,13 @@
 // Solution for Container With Most Water in JAVA
+
+class Solution {
+    public int maxArea(int[] h) {
+        int l = 0, r = h.length - 1, res = 0;
+        while (l < r) {
+            res = Math.max(res, (r - l) * Math.min(h[l], h[r]));
+            if (h[l] < h[r]) l++;
+            else r--;
+        }
+        return res;
+    }
+}
