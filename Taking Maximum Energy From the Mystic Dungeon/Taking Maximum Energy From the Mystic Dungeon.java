a9ae56d61e076fb2b86d4eb9516975bd974cb9d0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-10 12:43:13 +0530

    Update Taking Maximum Energy From the Mystic Dungeon.java

--- Taking Maximum Energy From the Mystic Dungeon/Taking Maximum Energy From the Mystic Dungeon.java
+++ Taking Maximum Energy From the Mystic Dungeon/Taking Maximum Energy From the Mystic Dungeon.java
@@ -1,1 +1,26 @@
 // Solution for Taking Maximum Energy From the Mystic Dungeon in JAVA
+
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        int totalMagicians = energy.length;
+        int[] dp = new int[totalMagicians];
+        Arrays.fill(dp, 0);
+        int maxEnergyCollected = Integer.MIN_VALUE;
+
+        // Process from right to left because dp[i] depends on dp[i + k]
+        for (int index = totalMagicians - 1; index >= 0; index--) {
+            int nextIndex = index + k;
+
+            // If jumping goes out of bounds, only take current energy
+            if (nextIndex >= totalMagicians)
+                dp[index] = energy[index];
+            else
+                dp[index] = energy[index] + dp[nextIndex];
+
+            // Track the best possible energy
+            maxEnergyCollected = Math.max(maxEnergyCollected, dp[index]);
+        }
+
+        return maxEnergyCollected;
+    }
+}
