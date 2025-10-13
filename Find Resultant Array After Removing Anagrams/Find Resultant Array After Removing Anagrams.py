e5e01a2e4159d83e57472b456c65d1862a5d6937
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-13 12:28:40 +0530

    Update Find Resultant Array After Removing Anagrams.py

--- Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.py
+++ Find Resultant Array After Removing Anagrams/Find Resultant Array After Removing Anagrams.py
@@ -1,1 +1,22 @@
 # Solution for Find Resultant Array After Removing Anagrams in PY
+
+class Solution:
+    def checkAnagram(self, s1: str, s2: str) -> bool:
+        arr = [0] * 26
+        for ch in s1:
+            arr[ord(ch) - ord('a')] += 1
+        
+        for ch in s2:
+            arr[ord(ch) - ord('a')] -= 1
+        
+        for i in range(26):
+            if arr[i] != 0:
+                return False
+        return True
+
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        result = [words[0]]
+        for i in range(1, len(words)):
+            if self.checkAnagram(result[-1], words[i]) == False:
+                result.append(words[i])
+        return result
