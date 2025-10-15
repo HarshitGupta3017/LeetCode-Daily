2bbaa5f375c88579ec6e0bd79acc4b91d1d7b715
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-15 14:55:10 +0530

    Update Adjacent Increasing Subarrays Detection II.py

--- Adjacent Increasing Subarrays Detection II/Adjacent Increasing Subarrays Detection II.py
+++ Adjacent Increasing Subarrays Detection II/Adjacent Increasing Subarrays Detection II.py
@@ -1,1 +1,14 @@
 # Solution for Adjacent Increasing Subarrays Detection II in PY
+
+class Solution:
+    def maxIncreasingSubarrays(self, nums: list[int]) -> int:
+        prev = 0
+        curr = 1
+        max_k = 0
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                curr += 1
+            else:
+                prev, curr = curr, 1
+            max_k = max(max_k, curr // 2, min(prev, curr))
+        return max_k
