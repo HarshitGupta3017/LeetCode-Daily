29b48866c9752de9ac5f8380492142d73b06fc83
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-15 14:55:22 +0530

    Update Adjacent Increasing Subarrays Detection II.java

--- Adjacent Increasing Subarrays Detection II/Adjacent Increasing Subarrays Detection II.java
+++ Adjacent Increasing Subarrays Detection II/Adjacent Increasing Subarrays Detection II.java
@@ -1,1 +1,13 @@
 // Solution for Adjacent Increasing Subarrays Detection II in JAVA
+
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int prev = 0, curr = 1, maxK = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i - 1)) curr++;
+            else { prev = curr; curr = 1; }
+            maxK = Math.max(maxK, Math.max(curr / 2, Math.min(prev, curr)));
+        }
+        return maxK;
+    }
+}
