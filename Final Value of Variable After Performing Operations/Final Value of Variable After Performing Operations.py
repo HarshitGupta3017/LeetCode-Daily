071f184b0315bd972a0da00659bc86fe43386b86
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-20 11:54:44 +0530

    Update Final Value of Variable After Performing Operations.py

--- Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.py
+++ Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.py
@@ -1,1 +1,5 @@
 # Solution for Final Value of Variable After Performing Operations in PY
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        return sum(1 if o[1] == '+' else -1 for o in operations)
