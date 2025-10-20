1b5b77b94a733da51f86feb2ace03c8806851f73
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-20 11:54:56 +0530

    Update Final Value of Variable After Performing Operations.java

--- Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.java
+++ Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.java
@@ -1,1 +1,9 @@
 // Solution for Final Value of Variable After Performing Operations in JAVA
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for (String o: operations) x = (o.charAt(1) == '-') ? x - 1 : x + 1;
+        return x;
+    }
+}
