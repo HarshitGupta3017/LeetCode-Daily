58cd10b3e95ddb48fb176c7a79cb6a0f39c32e4a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-20 11:55:15 +0530

    Update Final Value of Variable After Performing Operations.cpp

--- Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.cpp
+++ Final Value of Variable After Performing Operations/Final Value of Variable After Performing Operations.cpp
@@ -1,1 +1,10 @@
 // Solution for Final Value of Variable After Performing Operations in CPP
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (const auto& o: operations) x = (o[1] == '-') ? x - 1 : x + 1;
+        return x;
+    }
+};
