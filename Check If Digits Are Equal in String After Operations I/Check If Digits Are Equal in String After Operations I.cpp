fd37e2a12071b34700bf28430749869e5acfe3f4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-23 11:59:21 +0530

    Update Check If Digits Are Equal in String After Operations I.cpp

--- Check If Digits Are Equal in String After Operations I/Check If Digits Are Equal in String After Operations I.cpp
+++ Check If Digits Are Equal in String After Operations I/Check If Digits Are Equal in String After Operations I.cpp
@@ -1,1 +1,15 @@
 // Solution for Check If Digits Are Equal in String After Operations I in CPP
+
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int n = s.length();
+        while (n > 2) {
+            for (int i = 0; i < n - 1; i++) {
+                s[i] = (((s[i] - '0') + (s[i + 1] - '0')) % 10) + '0';
+            }
+            n--;
+        }
+        return s[0] == s[1];
+    }
+};
