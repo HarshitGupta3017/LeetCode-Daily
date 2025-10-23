026a39230d405ed31fb7633d4da123f193c19bfc
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-23 11:59:44 +0530

    Update Check If Digits Are Equal in String After Operations I.py

--- Check If Digits Are Equal in String After Operations I/Check If Digits Are Equal in String After Operations I.py
+++ Check If Digits Are Equal in String After Operations I/Check If Digits Are Equal in String After Operations I.py
@@ -1,1 +1,13 @@
 # Solution for Check If Digits Are Equal in String After Operations I in PY
+
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        n = len(s)
+        s = list(s)
+
+        while n > 2:
+            for i in range(n - 1):
+                s[i] = str((int(s[i]) + int(s[i + 1])) % 10)
+            n -= 1
+
+        return s[0] == s[1]
