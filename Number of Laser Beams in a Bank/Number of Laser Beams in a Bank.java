485907979262bd61c91716ddc81e9fddb75712a0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-27 15:58:42 +0530

    Update Number of Laser Beams in a Bank.java

--- Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.java
+++ Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.java
@@ -1,1 +1,16 @@
 // Solution for Number of Laser Beams in a Bank in JAVA
+
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int beams = 0, prev = 0;
+        for (String row : bank) {
+            int curr = 0;
+            for (char c : row.toCharArray()) if (c == '1') curr++;
+            if (curr > 0) {
+                beams += prev * curr;
+                prev = curr;
+            }
+        }
+        return beams;
+    }
+}
