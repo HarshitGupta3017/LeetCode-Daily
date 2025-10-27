06c55528aa7be9e7e2a1fb30e13bd3771cf2b05e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-27 15:58:25 +0530

    Update Number of Laser Beams in a Bank.py

--- Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.py
+++ Number of Laser Beams in a Bank/Number of Laser Beams in a Bank.py
@@ -1,1 +1,11 @@
 # Solution for Number of Laser Beams in a Bank in PY
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        res, prev = 0, 0
+        for row in bank:
+            curr = row.count('1')
+            if curr:
+                res += prev * curr
+                prev = curr
+        return res
