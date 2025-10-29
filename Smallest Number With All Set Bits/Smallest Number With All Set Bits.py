b0df1a927885b2bed6e00edc8497a12dcb6ddb27
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-29 15:17:20 +0530

    Update Smallest Number With All Set Bits.py

--- Smallest Number With All Set Bits/Smallest Number With All Set Bits.py
+++ Smallest Number With All Set Bits/Smallest Number With All Set Bits.py
@@ -1,1 +1,8 @@
 # Solution for Smallest Number With All Set Bits in PY
+
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        x = 1
+        while x < n: 
+            x = x * 2 + 1
+        return x
