a91a413e4d0adc2fd982da9724b63dc051c223c7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-29 15:17:31 +0530

    Update Smallest Number With All Set Bits.java

--- Smallest Number With All Set Bits/Smallest Number With All Set Bits.java
+++ Smallest Number With All Set Bits/Smallest Number With All Set Bits.java
@@ -1,1 +1,9 @@
 // Solution for Smallest Number With All Set Bits in JAVA
+
+class Solution {
+    public int smallestNumber(int n) {
+        int x = 1;
+        while (x < n) x = x * 2 + 1;
+        return x;
+    }
+}
