579b451eca2a69b7786a545159a6a4931e184b1f
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-29 15:17:50 +0530

    Update Smallest Number With All Set Bits.cpp

--- Smallest Number With All Set Bits/Smallest Number With All Set Bits.cpp
+++ Smallest Number With All Set Bits/Smallest Number With All Set Bits.cpp
@@ -1,1 +1,11 @@
 // Solution for Smallest Number With All Set Bits in CPP
+
+class Solution {
+public:
+    int smallestNumber(int n) {
+        // Follow the pattern - 1,3,7,15,31
+        int x = 1;
+        while (x < n) x = x * 2 + 1;
+        return x;
+    }
+};
