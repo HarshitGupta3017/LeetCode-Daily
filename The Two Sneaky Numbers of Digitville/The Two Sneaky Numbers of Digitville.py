00ea9aac2364a6a114f255962d1bac95dca57e2c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-31 13:43:11 +0530

    Update The Two Sneaky Numbers of Digitville.py

--- The Two Sneaky Numbers of Digitville/The Two Sneaky Numbers of Digitville.py
+++ The Two Sneaky Numbers of Digitville/The Two Sneaky Numbers of Digitville.py
@@ -1,1 +1,31 @@
 # Solution for The Two Sneaky Numbers of Digitville in PY
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        original_size = len(nums) - 2
+
+        xor_all = 0
+        for val in nums:
+            xor_all ^= val
+
+        for num in range(original_size):
+            xor_all ^= num
+
+        # Find rightmost set bit
+        bit_mask = xor_all & -xor_all
+
+        group1, group2 = 0, 0
+
+        for val in nums:
+            if val & bit_mask:
+                group1 ^= val
+            else:
+                group2 ^= val
+
+        for num in range(original_size):
+            if num & bit_mask:
+                group1 ^= num
+            else:
+                group2 ^= num
+
+        return [group1, group2]
