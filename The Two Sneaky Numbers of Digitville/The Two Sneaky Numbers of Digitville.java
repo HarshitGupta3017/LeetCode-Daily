17dd4b999baafe1fe19584d31ced46e6bb48a055
Harshit Gupta <harshitgupta3017@gmail.com>
2025-10-31 13:42:37 +0530

    Update The Two Sneaky Numbers of Digitville.java

--- The Two Sneaky Numbers of Digitville/The Two Sneaky Numbers of Digitville.java
+++ The Two Sneaky Numbers of Digitville/The Two Sneaky Numbers of Digitville.java
@@ -1,1 +1,31 @@
 // Solution for The Two Sneaky Numbers of Digitville in JAVA
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int originalSize = nums.length - 2;
+
+        int xorAllElements = 0;
+        for (int val : nums)
+            xorAllElements ^= val;
+
+        for (int num = 0; num < originalSize; num++)
+            xorAllElements ^= num;
+
+        // Find rightmost set bit
+        int bitMask = xorAllElements & -xorAllElements;
+
+        int group1Xor = 0, group2Xor = 0;
+
+        for (int val : nums) {
+            if ((val & bitMask) != 0) group1Xor ^= val;
+            else group2Xor ^= val;
+        }
+
+        for (int num = 0; num < originalSize; num++) {
+            if ((num & bitMask) != 0) group1Xor ^= num;
+            else group2Xor ^= num;
+        }
+
+        return new int[]{group1Xor, group2Xor};
+    }
+}
