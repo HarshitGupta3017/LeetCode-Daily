3dfaa79bd2e80720651507390ab5bdab6ff206e5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-03 11:31:42 +0530

    Update Minimum Time to Make Rope Colorful.java

--- Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.java
+++ Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.java
@@ -1,1 +1,20 @@
 // Solution for Minimum Time to Make Rope Colorful in JAVA
+
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = neededTime.length;
+        int time = 0;
+        int prevMax = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1))
+                prevMax = 0;
+
+            int curr = neededTime[i];
+            time += Math.min(prevMax, curr);
+            prevMax = Math.max(prevMax, curr);
+        }
+
+        return time;
+    }
+}
