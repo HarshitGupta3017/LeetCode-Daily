88add4e8357152eeb1d94119730171b7835f5cf2
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-03 11:31:30 +0530

    Update Minimum Time to Make Rope Colorful.py

--- Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.py
+++ Minimum Time to Make Rope Colorful/Minimum Time to Make Rope Colorful.py
@@ -1,1 +1,17 @@
 # Solution for Minimum Time to Make Rope Colorful in PY
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(neededTime)
+        time = 0
+        prev_max = 0
+
+        for i in range(n):
+            if i > 0 and colors[i] != colors[i - 1]:
+                prev_max = 0
+
+            curr = neededTime[i]
+            time += min(prev_max, curr)
+            prev_max = max(prev_max, curr)
+
+        return time
