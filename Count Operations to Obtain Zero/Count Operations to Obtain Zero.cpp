cfe4ed3e3de19ea01c228fd337a7ead23b1eec7d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-09 12:32:35 +0530

    Update Count Operations to Obtain Zero.cpp

--- Count Operations to Obtain Zero/Count Operations to Obtain Zero.cpp
+++ Count Operations to Obtain Zero/Count Operations to Obtain Zero.cpp
@@ -1,1 +1,14 @@
 // Solution for Count Operations to Obtain Zero in CPP
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while (num1 > 0 && num2 > 0) {
+            count += num1 / num2;
+            num1 %= num2;
+            swap(num1, num2);
+        }
+        return count;
+    }
+};
