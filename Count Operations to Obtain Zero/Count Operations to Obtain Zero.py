50cdaa07ba1b02ed8da5b9b07225fed92f344ec6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-09 12:32:47 +0530

    Update Count Operations to Obtain Zero.py

--- Count Operations to Obtain Zero/Count Operations to Obtain Zero.py
+++ Count Operations to Obtain Zero/Count Operations to Obtain Zero.py
@@ -1,1 +1,10 @@
 # Solution for Count Operations to Obtain Zero in PY
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        while num1 > 0 and num2 > 0:
+            count += num1 // num2
+            num1 %= num2
+            num1, num2 = num2, num1  # swap
+        return count
