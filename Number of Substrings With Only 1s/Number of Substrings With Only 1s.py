15dd55b3b32e5e4744f725b5518c9587360d53ee
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-16 16:18:47 +0530

    Update Number of Substrings With Only 1s.py

--- Number of Substrings With Only 1s/Number of Substrings With Only 1s.py
+++ Number of Substrings With Only 1s/Number of Substrings With Only 1s.py
@@ -1,1 +1,17 @@
 # Solution for Number of Substrings With Only 1s in PY
+
+class Solution:
+    def numSub(self, s: str) -> int:
+        
+        MOD = 10**9 + 7
+        total = 0
+        streak = 0
+
+        for ch in s:
+            if ch == '1':
+                streak += 1
+                total = (total + streak) % MOD
+            else:
+                streak = 0
+
+        return total
