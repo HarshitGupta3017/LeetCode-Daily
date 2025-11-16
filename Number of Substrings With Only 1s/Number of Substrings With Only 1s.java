c55a5caa6420b2497d83222939c9d6a8f606cee6
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-16 16:19:05 +0530

    Update Number of Substrings With Only 1s.java

--- Number of Substrings With Only 1s/Number of Substrings With Only 1s.java
+++ Number of Substrings With Only 1s/Number of Substrings With Only 1s.java
@@ -1,1 +1,24 @@
 // Solution for Number of Substrings With Only 1s in JAVA
+
+class Solution {
+
+    private static final int MOD = 1000000007;
+
+    public int numSub(String s) {
+
+        long totalSubstrings = 0;
+        long consecutiveOnes = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '1') {
+                consecutiveOnes++;
+                totalSubstrings = (totalSubstrings + consecutiveOnes) % MOD;
+            } else {
+                consecutiveOnes = 0;
+            }
+        }
+
+        return (int) totalSubstrings;
+    }
+}
