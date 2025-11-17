869e58afb4c39a189fbff614ddf7af34bfab8017
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-17 11:37:42 +0530

    Update Check If All 1's Are at Least Length K Places Away.cpp

--- Check If All 1's Are at Least Length K Places Away/Check If All 1's Are at Least Length K Places Away.cpp
+++ Check If All 1's Are at Least Length K Places Away/Check If All 1's Are at Least Length K Places Away.cpp
@@ -1,1 +1,16 @@
 // Solution for Check If All 1's Are at Least Length K Places Away in CPP
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int prev = -1;
+        for (int curr = 0; curr < nums.size(); curr++) {
+            if (nums[curr] == 0) continue;
+            if (prev != -1 && curr - prev - 1 < k) {
+                return false;
+            }
+            prev = curr;
+        }
+        return true;
+    }
+};
