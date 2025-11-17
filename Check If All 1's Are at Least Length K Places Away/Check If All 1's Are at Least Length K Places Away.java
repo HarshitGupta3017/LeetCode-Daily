8eaca2b7c809ce9c0313dc3d87c187f175e9f078
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-17 11:37:18 +0530

    Update Check If All 1's Are at Least Length K Places Away.java

--- Check If All 1's Are at Least Length K Places Away/Check If All 1's Are at Least Length K Places Away.java
+++ Check If All 1's Are at Least Length K Places Away/Check If All 1's Are at Least Length K Places Away.java
@@ -1,1 +1,15 @@
 // Solution for Check If All 1's Are at Least Length K Places Away in JAVA
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int prev = -1;
+        for (int curr = 0; curr < nums.length; curr++) {
+            if (nums[curr] == 0) continue;
+            if (prev != -1 && curr - prev - 1 < k) {
+                return false;
+            }
+            prev = curr;
+        }
+        return true;
+    }
+}
