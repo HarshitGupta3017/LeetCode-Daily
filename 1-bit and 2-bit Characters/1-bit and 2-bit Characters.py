4ffa86b410e61c3153bb94b9c8cbce05d5d8c810
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-18 14:15:54 +0530

    Update 1-bit and 2-bit Characters.py

--- 1-bit and 2-bit Characters/1-bit and 2-bit Characters.py
+++ 1-bit and 2-bit Characters/1-bit and 2-bit Characters.py
@@ -1,1 +1,10 @@
 # Solution for 1-bit and 2-bit Characters in PY
+
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n, trailingOnes = len(bits), 0
+        i = n - 2
+        while i >= 0 and bits[i] == 1:
+            trailingOnes += 1
+            i -= 1
+        return (trailingOnes % 2 == 0)
