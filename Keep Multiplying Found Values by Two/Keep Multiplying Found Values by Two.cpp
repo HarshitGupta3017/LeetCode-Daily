fb9c472a1c5411cf99d27a0913f15c13e167ba70
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-19 16:14:51 +0530

    Update Keep Multiplying Found Values by Two.cpp

--- Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.cpp
+++ Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.cpp
@@ -1,1 +1,13 @@
 // Solution for Keep Multiplying Found Values by Two in CPP
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(), nums.end());
+        for (const int& num: nums) {
+            if (num == original)
+                original *= 2;
+        }
+        return original;
+    }
+};
