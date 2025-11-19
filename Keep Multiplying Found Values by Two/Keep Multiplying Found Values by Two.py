762e9cbca136d1e99d37d84e70da6a30d43823e9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-19 16:13:42 +0530

    Update Keep Multiplying Found Values by Two.py

--- Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.py
+++ Keep Multiplying Found Values by Two/Keep Multiplying Found Values by Two.py
@@ -1,1 +1,9 @@
 # Solution for Keep Multiplying Found Values by Two in PY
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums.sort()
+        for num in nums:
+            if num == original:
+                original *= 2
+        return original
