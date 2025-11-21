50b7d08111b7f3535f1cd7c09fdb18ab0b476508
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-21 13:03:43 +0530

    Update Unique Length-3 Palindromic Subsequences.py

--- Unique Length-3 Palindromic Subsequences/Unique Length-3 Palindromic Subsequences.py
+++ Unique Length-3 Palindromic Subsequences/Unique Length-3 Palindromic Subsequences.py
@@ -1,1 +1,31 @@
 # Solution for Unique Length-3 Palindromic Subsequences in PY
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        n = len(s)
+
+        # (first, last) for each char
+        charBounds = [[-1, -1] for _ in range(26)]
+
+        # record first & last appearance
+        for i, ch in enumerate(s):
+            idx = ord(ch) - ord('a')
+            if charBounds[idx][0] == -1:
+                charBounds[idx][0] = i
+            charBounds[idx][1] = i
+
+        result = 0
+
+        # count distinct middle chars
+        for c in range(26):
+            leftIndex, rightIndex = charBounds[c]
+            if leftIndex == -1:
+                continue
+
+            middleChars = set()
+            for mid in range(leftIndex + 1, rightIndex):
+                middleChars.add(s[mid])
+
+            result += len(middleChars)
+
+        return result
