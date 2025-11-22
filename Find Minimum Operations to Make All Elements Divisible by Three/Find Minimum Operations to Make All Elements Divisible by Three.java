1a7bcea6a7623e3307f47e18f4709410c5fcb92a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-22 14:41:59 +0530

    Update Find Minimum Operations to Make All Elements Divisible by Three.java

--- Find Minimum Operations to Make All Elements Divisible by Three/Find Minimum Operations to Make All Elements Divisible by Three.java
+++ Find Minimum Operations to Make All Elements Divisible by Three/Find Minimum Operations to Make All Elements Divisible by Three.java
@@ -1,1 +1,11 @@
 // Solution for Find Minimum Operations to Make All Elements Divisible by Three in JAVA
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int ops = 0;
+        for (int num: nums) {
+            ops += (num % 3 != 0) ? 1 : 0;
+        }
+        return ops;
+    }
+}
