77af7de00b47ba3c45774b34839515196d2aa7d0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-24 13:44:18 +0530

    Update Binary Prefix Divisible By 5.py

--- Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.py
+++ Binary Prefix Divisible By 5/Binary Prefix Divisible By 5.py
@@ -1,1 +1,12 @@
 # Solution for Binary Prefix Divisible By 5 in PY
+
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        result = []
+        prefixMod = 0
+
+        for bit in nums:
+            prefixMod = ((prefixMod << 1) + bit) % 5
+            result.append(prefixMod == 0)
+
+        return result
