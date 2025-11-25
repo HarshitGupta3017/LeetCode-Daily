b63a1e758e67b2e1cceec8d8eec049866cca0e2a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-25 17:39:32 +0530

    Update Smallest Integer Divisible by K.java

--- Smallest Integer Divisible by K/Smallest Integer Divisible by K.java
+++ Smallest Integer Divisible by K/Smallest Integer Divisible by K.java
@@ -1,1 +1,13 @@
 // Solution for Smallest Integer Divisible by K in JAVA
+
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        int num = 0;
+        for (int len = 1; len <= k; len++) {
+            num = (num * 10 + 1) % k;
+            if (num == 0)
+                return len;
+        }
+        return -1;
+    }
+}
