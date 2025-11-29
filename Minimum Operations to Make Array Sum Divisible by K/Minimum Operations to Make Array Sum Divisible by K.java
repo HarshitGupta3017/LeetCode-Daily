06b301ff0d30ce1bad58365ac0cee51d608c00a5
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-29 12:33:42 +0530

    Update Minimum Operations to Make Array Sum Divisible by K.java

--- Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.java
+++ Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.java
@@ -1,1 +1,7 @@
 // Solution for Minimum Operations to Make Array Sum Divisible by K in JAVA
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        return Arrays.stream(nums).sum() % k;
+    }
+}
