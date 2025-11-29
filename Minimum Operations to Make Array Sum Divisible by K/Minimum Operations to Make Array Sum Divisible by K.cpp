e9efc31d4628ba0d92d94182283f20be48d24ffb
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-29 12:33:52 +0530

    Update Minimum Operations to Make Array Sum Divisible by K.cpp

--- Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.cpp
+++ Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.cpp
@@ -1,1 +1,8 @@
 // Solution for Minimum Operations to Make Array Sum Divisible by K in CPP
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        return accumulate(nums.begin(), nums.end(), 0) % k;
+    }
+};
