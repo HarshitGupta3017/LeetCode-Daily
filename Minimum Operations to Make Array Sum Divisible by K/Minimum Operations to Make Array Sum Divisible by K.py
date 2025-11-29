bd0c162b440012c357b451e67442e5c9dacfe604
Harshit Gupta <harshitgupta3017@gmail.com>
2025-11-29 12:33:28 +0530

    Update Minimum Operations to Make Array Sum Divisible by K.py

--- Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.py
+++ Minimum Operations to Make Array Sum Divisible by K/Minimum Operations to Make Array Sum Divisible by K.py
@@ -1,1 +1,5 @@
 # Solution for Minimum Operations to Make Array Sum Divisible by K in PY
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        return sum(nums) % k
