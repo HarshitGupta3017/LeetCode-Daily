e0865e7e878cdbbf50590bb906ea955d030a158a
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-07 10:29:18 +0530

    Update Count Odd Numbers in an Interval Range.cpp

--- Count Odd Numbers in an Interval Range/Count Odd Numbers in an Interval Range.cpp
+++ Count Odd Numbers in an Interval Range/Count Odd Numbers in an Interval Range.cpp
@@ -1,1 +1,8 @@
 // Solution for Count Odd Numbers in an Interval Range in CPP
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+};
