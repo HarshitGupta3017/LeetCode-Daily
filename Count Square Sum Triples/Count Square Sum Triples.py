0195e17092438ccfd5ec7929cfc9b5247081997e
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-08 12:31:05 +0530

    Update Count Square Sum Triples.py

--- Count Square Sum Triples/Count Square Sum Triples.py
+++ Count Square Sum Triples/Count Square Sum Triples.py
@@ -1,1 +1,11 @@
 # Solution for Count Square Sum Triples in PY
+
+class Solution:
+    def countTriples(self, n: int) -> int:
+        result = 0
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+                c = int(math.sqrt(a * a + b * b))
+                if (c <= n and c * c == a * a + b * b):
+                    result += 1
+        return result
