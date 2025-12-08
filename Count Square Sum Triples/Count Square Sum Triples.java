57a74d6b417a7f8ba1d7c1081620dc90a298626d
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-08 12:31:20 +0530

    Update Count Square Sum Triples.java

--- Count Square Sum Triples/Count Square Sum Triples.java
+++ Count Square Sum Triples/Count Square Sum Triples.java
@@ -1,1 +1,16 @@
 // Solution for Count Square Sum Triples in JAVA
+
+class Solution {
+    public int countTriples(int n) {
+        int result = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c = (int)(Math.sqrt(a * a + b * b));
+                if (c <= n && c * c == a * a + b * b) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
