b66c9b469e06b4318ecb6328b95c58cfeb9b3ab0
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-08 12:31:32 +0530

    Update Count Square Sum Triples.cpp

--- Count Square Sum Triples/Count Square Sum Triples.cpp
+++ Count Square Sum Triples/Count Square Sum Triples.cpp
@@ -1,1 +1,17 @@
 // Solution for Count Square Sum Triples in CPP
+
+class Solution {
+public:
+    int countTriples(int n) {
+        int result = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c = (int)(sqrt(a * a + b * b));
+                if (c <= n && c * c == a * a + b * b) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+};
