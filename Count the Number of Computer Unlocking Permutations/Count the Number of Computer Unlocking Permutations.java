b8fe064b28b7a3bc83d4df2679f29f463d18bb04
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-10 18:45:16 +0530

    Update Count the Number of Computer Unlocking Permutations.java

--- Count the Number of Computer Unlocking Permutations/Count the Number of Computer Unlocking Permutations.java
+++ Count the Number of Computer Unlocking Permutations/Count the Number of Computer Unlocking Permutations.java
@@ -1,1 +1,23 @@
 // Solution for Count the Number of Computer Unlocking Permutations in JAVA
+
+class Solution {
+    public int countPermutations(int[] complexity) {
+        final int MOD = 1_000_000_007;
+
+        long totalSequences = 1;
+
+        // Iterate over all computers except root (index 0)
+        for (int i = 1; i < complexity.length; i++) {
+
+            // If complexity is <= root's complexity, no valid permutation
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+
+            // Multiply choices for this computer
+            totalSequences = (totalSequences * i) % MOD;
+        }
+
+        return (int) totalSequences;
+    }
+}
