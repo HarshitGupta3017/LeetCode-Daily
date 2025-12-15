8c8917ab531e5fc25a514aeec56bc05bdfd8d0d9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-15 13:25:22 +0530

    Update Number of Smooth Descent Periods of a Stock.py

--- Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.py
+++ Number of Smooth Descent Periods of a Stock/Number of Smooth Descent Periods of a Stock.py
@@ -1,1 +1,19 @@
 # Solution for Number of Smooth Descent Periods of a Stock in PY
+
+class Solution:
+    def getDescentPeriods(self, prices: List[int]) -> int:
+
+        total_descent_periods = 0
+        current_streak = 1  # single day is valid
+
+        for i in range(1, len(prices)):
+            if prices[i - 1] - prices[i] == 1:
+                current_streak += 1
+            else:
+                total_descent_periods += current_streak * (current_streak + 1) // 2
+                current_streak = 1
+
+        # Add last streak
+        total_descent_periods += current_streak * (current_streak + 1) // 2
+
+        return total_descent_periods
