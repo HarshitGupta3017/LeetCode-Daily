dff8e3f5effc3651633e3576e061ca7f93cb9bd9
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-20 16:06:06 +0530

    Update Delete Columns to Make Sorted.java

--- Delete Columns to Make Sorted/Delete Columns to Make Sorted.java
+++ Delete Columns to Make Sorted/Delete Columns to Make Sorted.java
@@ -1,1 +1,22 @@
 // Solution for Delete Columns to Make Sorted in JAVA
+
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int numRows = strs.length;
+        int numCols = strs[0].length();
+        int columnsToDelete = 0;
+
+        // Iterate over each column
+        for (int col = 0; col < numCols; col++) {
+            // Check if this column is sorted
+            for (int row = 1; row < numRows; row++) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    columnsToDelete++; // column is unsorted
+                    break;
+                }
+            }
+        }
+
+        return columnsToDelete;
+    }
+}
