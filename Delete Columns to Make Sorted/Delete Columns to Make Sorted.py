f553be4e52c55db0750f1da7bcf7a824276fb50c
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-20 16:05:51 +0530

    Update Delete Columns to Make Sorted.py

--- Delete Columns to Make Sorted/Delete Columns to Make Sorted.py
+++ Delete Columns to Make Sorted/Delete Columns to Make Sorted.py
@@ -1,1 +1,15 @@
 # Solution for Delete Columns to Make Sorted in PY
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        numRows = len(strs)
+        numCols = len(strs[0])
+        columnsToDelete = 0
+
+        for col in range(numCols):
+            for row in range(1, numRows):
+                if strs[row][col] < strs[row - 1][col]:
+                    columnsToDelete += 1
+                    break
+
+        return columnsToDelete
