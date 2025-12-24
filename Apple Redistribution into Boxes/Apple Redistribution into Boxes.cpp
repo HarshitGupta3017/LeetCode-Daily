1fc68d5ec4b6fc02dd5f4ca6271a13fb12e262e7
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-24 13:27:57 +0530

    Update Apple Redistribution into Boxes.cpp

--- Apple Redistribution into Boxes/Apple Redistribution into Boxes.cpp
+++ Apple Redistribution into Boxes/Apple Redistribution into Boxes.cpp
@@ -1,1 +1,15 @@
 // Solution for Apple Redistribution into Boxes in CPP
+
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        int totalApples = accumulate(apple.begin(), apple.end(), 0);
+        int count = 0;
+        for (int i = 0; i < capacity.size() && totalApples > 0; i++) {
+            totalApples -= capacity[i];
+            count++;
+        }
+        return count;
+    }
+};
