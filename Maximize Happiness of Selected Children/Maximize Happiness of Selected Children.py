9d868970c592b901c193f8cd03f759f8c7e9a597
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-25 12:16:25 +0530

    Update Maximize Happiness of Selected Children.py

--- Maximize Happiness of Selected Children/Maximize Happiness of Selected Children.py
+++ Maximize Happiness of Selected Children/Maximize Happiness of Selected Children.py
@@ -1,1 +1,17 @@
 # Solution for Maximize Happiness of Selected Children in PY
+
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+
+        # Sort happiness in descending order
+        happiness.sort(reverse=True)
+
+        total_happiness = 0
+
+        # Select k children
+        for turn in range(k):
+            # Happiness decreases by 1 for each previously selected child
+            current_happiness = max(happiness[turn] - turn, 0)
+            total_happiness += current_happiness
+
+        return total_happiness
