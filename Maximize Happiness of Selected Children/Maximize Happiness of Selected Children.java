e40d1e6d177c4d042beb0e46dad69cf3777296b4
Harshit Gupta <harshitgupta3017@gmail.com>
2025-12-25 12:16:46 +0530

    Update Maximize Happiness of Selected Children.java

--- Maximize Happiness of Selected Children/Maximize Happiness of Selected Children.java
+++ Maximize Happiness of Selected Children/Maximize Happiness of Selected Children.java
@@ -1,1 +1,22 @@
 // Solution for Maximize Happiness of Selected Children in JAVA
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        int n = happiness.length;
+
+        // Sort happiness in descending order
+        Integer[] happinessDesc = Arrays.stream(happiness).boxed().toArray(Integer[]::new);
+        Arrays.sort(happinessDesc, Collections.reverseOrder());
+
+        long totalHappiness = 0;
+
+        // Select k children
+        for (int turn = 0; turn < k; turn++) {
+            // Happiness decreases by 1 for each previously selected child
+            long currentHappiness = Math.max(happinessDesc[turn] - turn, 0);
+            totalHappiness += currentHappiness;
+        }
+
+        return totalHappiness;
+    }
+}
