9e085a6f9693095b09acf7cc91a9166456739f2c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-01-02 12:49:47 +0530

    Update N-Repeated Element in Size 2N Array.java

--- N-Repeated Element in Size 2N Array/N-Repeated Element in Size 2N Array.java
+++ N-Repeated Element in Size 2N Array/N-Repeated Element in Size 2N Array.java
@@ -1,1 +1,14 @@
 // Solution for N-Repeated Element in Size 2N Array in JAVA
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num;
+            }
+            seen.add(num);
+        }
+        return 0; 
+    }
+}
