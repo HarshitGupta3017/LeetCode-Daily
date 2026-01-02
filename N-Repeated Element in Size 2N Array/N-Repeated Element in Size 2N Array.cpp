5055a46df24d633fccd1c0c4e9e82558c3155dd7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-01-02 12:49:58 +0530

    Update N-Repeated Element in Size 2N Array.cpp

--- N-Repeated Element in Size 2N Array/N-Repeated Element in Size 2N Array.cpp
+++ N-Repeated Element in Size 2N Array/N-Repeated Element in Size 2N Array.cpp
@@ -1,1 +1,13 @@
 // Solution for N-Repeated Element in Size 2N Array in CPP
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> st;
+        for (const auto& num: nums) {
+            if (st.count(num)) return num;
+            st.insert(num);
+        }
+        return 0;
+    }
+};
