9bd445dbd607c3d80d94654f092525d8aa695764
Harshit Gupta <harshitgupta3017@gmail.com>
2026-01-22 16:07:30 +0530

    Update Minimum Pair Removal to Sort Array I.py

--- Minimum Pair Removal to Sort Array I/Minimum Pair Removal to Sort Array I.py
+++ Minimum Pair Removal to Sort Array I/Minimum Pair Removal to Sort Array I.py
@@ -1,1 +1,24 @@
 # Solution for Minimum Pair Removal to Sort Array I in PY
+
+class Solution:
+    def minimumPairRemoval(self, nums: List[int]) -> int:
+
+        def minIdx(arr):
+            min_sum = float('inf')
+            min_index = -1
+            for i in range(len(arr) - 1):
+                s = arr[i] + arr[i + 1]
+                if s < min_sum:
+                    min_sum = s
+                    min_index = i
+            return min_index
+
+        operations = 0
+
+        while nums != sorted(nums):
+            idx = minIdx(nums)
+            nums[idx] = nums[idx] + nums[idx + 1]
+            nums.pop(idx + 1)
+            operations += 1
+
+        return operations
