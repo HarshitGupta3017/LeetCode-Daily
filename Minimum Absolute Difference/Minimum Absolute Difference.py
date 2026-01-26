aa0e6394088fd1bc7e55bb54732b9259af548a6d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-01-26 17:37:50 +0530

    Update Minimum Absolute Difference.py

--- Minimum Absolute Difference/Minimum Absolute Difference.py
+++ Minimum Absolute Difference/Minimum Absolute Difference.py
@@ -1,1 +1,19 @@
 # Solution for Minimum Absolute Difference in PY
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        
+        n = len(arr)
+        minDiff = float('inf')
+
+        for i in range(1, n):
+            minDiff = min(minDiff, arr[i] - arr[i - 1])
+
+        result = []
+        for i in range(1, n):
+            diff = arr[i] - arr[i - 1]
+            if diff == minDiff:
+                result.append([arr[i - 1], arr[i]])
+        
+        return result
