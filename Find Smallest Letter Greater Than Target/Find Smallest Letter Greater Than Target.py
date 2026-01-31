c4974592cc18a3c2045a35e29b58c9f17a0e7a20
Harshit Gupta <harshitgupta3017@gmail.com>
2026-01-31 22:20:04 +0530

    Update Find Smallest Letter Greater Than Target.py

--- Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.py
+++ Find Smallest Letter Greater Than Target/Find Smallest Letter Greater Than Target.py
@@ -1,1 +1,6 @@
 # Solution for Find Smallest Letter Greater Than Target in PY
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        idx = bisect_right(letters, target)
+        return letters[0] if idx == len(letters) else letters[idx]
