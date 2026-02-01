daf5967484ba4e0e4eefd86df17dd36cab5ab3b2
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-01 16:34:46 +0530

    Update Divide an Array Into Subarrays With Minimum Cost I.py

--- Divide an Array Into Subarrays With Minimum Cost I/Divide an Array Into Subarrays With Minimum Cost I.py
+++ Divide an Array Into Subarrays With Minimum Cost I/Divide an Array Into Subarrays With Minimum Cost I.py
@@ -1,1 +1,20 @@
 # Solution for Divide an Array Into Subarrays With Minimum Cost I in PY
+
+class Solution:
+    def minimumCost(self, nums: List[int]) -> int:
+
+        # Cost of the first subarray (always nums[0])
+        total_cost = nums[0]
+
+        smallest = float('inf')
+        second_smallest = float('inf')
+
+        # Find two smallest values in nums[1:]
+        for num in nums[1:]:
+            if num < smallest:
+                second_smallest = smallest
+                smallest = num
+            elif num < second_smallest:
+                second_smallest = num
+
+        return total_cost + smallest + second_smallest
