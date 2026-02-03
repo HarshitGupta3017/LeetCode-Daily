0313a069ce755d0a4ec67c042a871c148b58f2a7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-03 15:39:35 +0530

    Update Trionic Array I.py

--- Trionic Array I/Trionic Array I.py
+++ Trionic Array I/Trionic Array I.py
@@ -1,1 +1,29 @@
 # Solution for Trionic Array I in PY
+
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+
+        n = len(nums)
+        index = 0
+
+        # Phase 1: Strictly Increasing
+        while index + 1 < n and nums[index] < nums[index + 1]:
+            index += 1
+
+        # No increasing part OR reached the end
+        if index == 0 or index == n - 1:
+            return False
+
+        # Phase 2: Strictly Decreasing
+        while index + 1 < n and nums[index] > nums[index + 1]:
+            index += 1
+
+        # No final increasing phase
+        if index == n - 1:
+            return False
+
+        # Phase 3: Strictly Increasing again
+        while index + 1 < n and nums[index] < nums[index + 1]:
+            index += 1
+
+        return index == n - 1
