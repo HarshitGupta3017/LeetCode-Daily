bbe0537fa5bb4ae921b2e696ac454c9d4243ca7a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-05 15:07:50 +0530

    Update Transformed Array.py

--- Transformed Array/Transformed Array.py
+++ Transformed Array/Transformed Array.py
@@ -1,1 +1,14 @@
 # Solution for Transformed Array in PY
+
+class Solution:
+    def constructTransformedArray(self, nums):
+        n = len(nums)
+        transformed_result = [0] * n
+
+        for index in range(n):
+            steps = nums[index] % n
+            target_index = (index + steps) % n  # Python already handles negatives
+
+            transformed_result[index] = nums[target_index]
+
+        return transformed_result
