761b67120331285f1f1e8c7e703492f2fcb2dca8
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-05 15:08:04 +0530

    Update Transformed Array.java

--- Transformed Array/Transformed Array.java
+++ Transformed Array/Transformed Array.java
@@ -1,1 +1,22 @@
 // Solution for Transformed Array in JAVA
+
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] transformedResult = new int[n];
+
+        for (int index = 0; index < n; index++) {
+            int steps = nums[index] % n;
+            int targetIndex = (index + steps) % n;
+
+            // Normalize negative index
+            if (targetIndex < 0) {
+                targetIndex += n;
+            }
+
+            transformedResult[index] = nums[targetIndex];
+        }
+
+        return transformedResult;
+    }
+}
