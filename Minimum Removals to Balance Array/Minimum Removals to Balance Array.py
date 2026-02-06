968d9e0ccbb52925ede8d986849583de7e390244
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-06 14:58:43 +0530

    Update Minimum Removals to Balance Array.py

--- Minimum Removals to Balance Array/Minimum Removals to Balance Array.py
+++ Minimum Removals to Balance Array/Minimum Removals to Balance Array.py
@@ -1,1 +1,14 @@
 # Solution for Minimum Removals to Balance Array in PY
+
+class Solution:
+    def minRemoval(self, nums, k):
+        nums.sort()
+        left = 0
+        best = 1
+
+        for right in range(len(nums)):
+            while nums[right] > k * nums[left]:
+                left += 1
+            best = max(best, right - left + 1)
+
+        return len(nums) - best
