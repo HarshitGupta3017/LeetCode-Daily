17d7f7efa10bbb95ed2319984964e02af3a41ba3
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-15 12:49:17 +0530

    Update Add Binary.py

--- Add Binary/Add Binary.py
+++ Add Binary/Add Binary.py
@@ -1,1 +1,31 @@
 # Solution for Add Binary in PY
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        indexA = len(a) - 1
+        indexB = len(b) - 1
+
+        result = []
+        carry = 0
+
+        while indexA >= 0 or indexB >= 0:
+
+            current_sum = carry
+
+            if indexA >= 0:
+                current_sum += int(a[indexA])
+                indexA -= 1
+
+            if indexB >= 0:
+                current_sum += int(b[indexB])
+                indexB -= 1
+
+            result.append(str(current_sum % 2))
+            carry = current_sum // 2
+
+        if carry > 0:
+            result.append('1')
+
+        # Reverse because built from LSB → MSB
+        return ''.join(reversed(result))
