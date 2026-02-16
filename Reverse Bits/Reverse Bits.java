84d50470983962aedfbaa066e668dc08071a34c4
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-16 15:34:42 +0530

    Update Reverse Bits.java

--- Reverse Bits/Reverse Bits.java
+++ Reverse Bits/Reverse Bits.java
@@ -1,1 +1,37 @@
 // Solution for Reverse Bits in JAVA
+
+class Solution {
+    public int reverseBits(int n) {
+        
+        int reversed = 0;
+
+        /*
+            We process exactly 32 bits.
+        */
+        for (int i = 0; i < 32; i++) {
+
+            /*
+                Step 1:
+                Shift result left to make space
+                for the next bit.
+            */
+            reversed <<= 1;
+
+            /*
+                Step 2:
+                Extract the last bit of number
+                using (number & 1)
+            */
+            reversed |= (n & 1);
+
+            /*
+                Step 3:
+                Shift number right
+                to process next bit.
+            */
+            n >>= 1;
+        }
+
+        return reversed;
+    }
+}
