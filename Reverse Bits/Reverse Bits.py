be15af4cd3a1f0382cd582efd345cdd2a3847961
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-16 15:34:29 +0530

    Update Reverse Bits.py

--- Reverse Bits/Reverse Bits.py
+++ Reverse Bits/Reverse Bits.py
@@ -1,1 +1,13 @@
 # Solution for Reverse Bits in PY
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        
+        reversed = 0
+        
+        for i in range(32):
+            reversed <<= 1
+            reversed |= (n & 1)
+            n >>= 1
+
+        return reversed
