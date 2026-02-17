7a0d3383cc0377f830dfe48cb2f130116324447f
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-17 13:13:51 +0530

    Update Binary Watch.java

--- Binary Watch/Binary Watch.java
+++ Binary Watch/Binary Watch.java
@@ -1,1 +1,29 @@
 // Solution for Binary Watch in JAVA
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+
+        List<String> possibleTimes = new ArrayList<>();
+
+        for (int hour = 0; hour <= 11; hour++) {
+            for (int minute = 0; minute <= 59; minute++) {
+
+                // Count total bits ON
+                int totalBitsOn = Integer.bitCount(hour) + Integer.bitCount(minute);
+
+                if (totalBitsOn == turnedOn) {
+
+                    // Hour: no leading zero needed
+                    String hourStr = String.valueOf(hour);
+
+                    // Minute: must be 2 digits
+                    String minuteStr = (minute < 10 ? "0" : "") + minute;
+
+                    possibleTimes.add(hourStr + ":" + minuteStr);
+                }
+            }
+        }
+
+        return possibleTimes;
+    }
+}
