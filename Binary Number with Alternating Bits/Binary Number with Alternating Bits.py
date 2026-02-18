3fdb83471ca8d967f5d4e7a06ba8d34dda6d1f1f
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-18 17:34:25 +0530

    Update Binary Number with Alternating Bits.py

--- Binary Number with Alternating Bits/Binary Number with Alternating Bits.py
+++ Binary Number with Alternating Bits/Binary Number with Alternating Bits.py
@@ -1,1 +1,6 @@
 # Solution for Binary Number with Alternating Bits in PY
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        xor_result = n ^ (n >> 1)
+        return (xor_result & (xor_result + 1)) == 0
