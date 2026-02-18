81fb14a05729251a936071afc7a1bf650427b2ea
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-18 17:34:36 +0530

    Update Binary Number with Alternating Bits.java

--- Binary Number with Alternating Bits/Binary Number with Alternating Bits.java
+++ Binary Number with Alternating Bits/Binary Number with Alternating Bits.java
@@ -1,1 +1,8 @@
 // Solution for Binary Number with Alternating Bits in JAVA
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int xorResult = n ^ (n >> 1);
+        return (xorResult & (xorResult + 1)) == 0;
+    }
+}
