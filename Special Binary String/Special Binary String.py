cbf096bbf4a580ad746cca48251b57d7efaae990
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-20 16:15:14 +0530

    Update Special Binary String.py

--- Special Binary String/Special Binary String.py
+++ Special Binary String/Special Binary String.py
@@ -1,1 +1,16 @@
 # Solution for Special Binary String in PY
+
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        blocks = []
+        balance = start = 0
+
+        for i, ch in enumerate(s):
+            balance += 1 if ch == '1' else -1
+            if balance == 0:
+                inner = s[start + 1:i]
+                blocks.append("1" + self.makeLargestSpecial(inner) + "0")
+                start = i + 1
+
+        blocks.sort(reverse=True)
+        return "".join(blocks)
