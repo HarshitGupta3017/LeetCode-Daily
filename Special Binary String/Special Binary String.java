faa2d2bca4495a1bce4344e9e5f8cfdc616b4949
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-20 16:15:29 +0530

    Update Special Binary String.java

--- Special Binary String/Special Binary String.java
+++ Special Binary String/Special Binary String.java
@@ -1,1 +1,21 @@
 // Solution for Special Binary String in JAVA
+
+class Solution {
+    public String makeLargestSpecial(String s) {
+        List<String> blocks = new ArrayList<>();
+        int balance = 0, start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            balance += s.charAt(i) == '1' ? 1 : -1;
+
+            if (balance == 0) {
+                String inner = s.substring(start + 1, i);
+                blocks.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(blocks, Collections.reverseOrder());
+        return String.join("", blocks);
+    }
+}
