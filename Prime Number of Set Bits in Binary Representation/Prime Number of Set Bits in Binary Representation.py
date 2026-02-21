95a6227a0c2829cd5e50ac221cd970c4aba4f2ad
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-21 18:45:59 +0530

    Update Prime Number of Set Bits in Binary Representation.py

--- Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.py
+++ Prime Number of Set Bits in Binary Representation/Prime Number of Set Bits in Binary Representation.py
@@ -1,1 +1,6 @@
 # Solution for Prime Number of Set Bits in Binary Representation in PY
+
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        prime_set_bits = {2, 3, 5, 7, 11, 13, 17, 19}
+        return sum(1 for num in range(left, right + 1) if num.bit_count() in prime_set_bits)
