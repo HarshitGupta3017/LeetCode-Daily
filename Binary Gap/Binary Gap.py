5ad6203fce596509a8ab0958d05f6d34322bd978
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-22 18:30:43 +0530

    Update Binary Gap.py

--- Binary Gap/Binary Gap.py
+++ Binary Gap/Binary Gap.py
@@ -1,1 +1,21 @@
 # Solution for Binary Gap in PY
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+
+        prev_position = -1
+        max_distance = 0
+
+        # A 32-bit integer has at most 32 bits
+        for current_position in range(32):
+
+            # Check if current bit is 1
+            if (n >> current_position) & 1:
+
+                if prev_position != -1:
+                    distance = current_position - prev_position
+                    max_distance = max(max_distance, distance)
+
+                prev_position = current_position
+
+        return max_distance
