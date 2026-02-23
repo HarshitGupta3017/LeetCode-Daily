8383193ed627f57d13eb913d08a22d3ddcb5e01c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-23 11:15:26 +0530

    Update Check If a String Contains All Binary Codes of Size K.py

--- Check If a String Contains All Binary Codes of Size K/Check If a String Contains All Binary Codes of Size K.py
+++ Check If a String Contains All Binary Codes of Size K/Check If a String Contains All Binary Codes of Size K.py
@@ -1,1 +1,20 @@
 # Solution for Check If a String Contains All Binary Codes of Size K in PY
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+
+        total_codes_needed = 1 << k
+        seen = set()
+
+        for i in range(k, len(s) + 1):
+
+            sub = s[i - k:i]
+
+            if sub not in seen:
+                seen.add(sub)
+                total_codes_needed -= 1
+
+            if total_codes_needed == 0:
+                return True
+
+        return False
