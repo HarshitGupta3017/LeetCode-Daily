0648a5f64a57fd70d4669d2e412792277fd44247
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-24 15:45:42 +0530

    Update Sum of Root To Leaf Binary Numbers.py

--- Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.py
+++ Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.py
@@ -1,1 +1,19 @@
 # Solution for Sum of Root To Leaf Binary Numbers in PY
+
+class Solution:
+    def sumRootToLeaf(self, root) -> int:
+
+        def solve(node, current_value):
+            if not node:
+                return 0
+
+            # Shift left and add current bit
+            current_value = (current_value << 1) + node.val
+
+            # If leaf node
+            if not node.left and not node.right:
+                return current_value
+
+            return solve(node.left, current_value) + solve(node.right, current_value)
+
+        return solve(root, 0)
