c3f1cf46f4581176fa5e60bd253b22d5c2f7e994
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-24 15:46:03 +0530

    Update Sum of Root To Leaf Binary Numbers.java

--- Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.java
+++ Sum of Root To Leaf Binary Numbers/Sum of Root To Leaf Binary Numbers.java
@@ -1,1 +1,24 @@
 // Solution for Sum of Root To Leaf Binary Numbers in JAVA
+
+class Solution {
+    private int solve(TreeNode root, int currentValue) {
+
+        if (root == null)
+            return 0;
+
+        // Shift left and add current bit
+        currentValue = (currentValue << 1) + root.val;
+
+        // If leaf node
+        if (root.left == null && root.right == null)
+            return currentValue;
+
+        // Recurse left and right
+        return solve(root.left, currentValue)
+             + solve(root.right, currentValue);
+    }
+
+    public int sumRootToLeaf(TreeNode root) {
+        return solve(root, 0);
+    }
+}
