8a12cef1194e9cf7f952a3ba7c14a89949e1057c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-25 15:13:27 +0530

    Update Sort Integers by The Number of 1 Bits.cpp

--- Sort Integers by The Number of 1 Bits/Sort Integers by The Number of 1 Bits.cpp
+++ Sort Integers by The Number of 1 Bits/Sort Integers by The Number of 1 Bits.cpp
@@ -1,1 +1,15 @@
 // Solution for Sort Integers by The Number of 1 Bits in CPP
+
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        auto lambda = [](const auto& a, const auto& b) {
+            int cA = __builtin_popcount(a);
+            int cB = __builtin_popcount(b);
+            if (cA == cB) return a < b;
+            return cA < cB;
+        };
+        sort(begin(arr), end(arr), lambda);
+        return arr;
+    }
+};
