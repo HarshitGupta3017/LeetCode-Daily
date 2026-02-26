420abedb133db457e03f90819887528a508df063
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-26 15:50:49 +0530

    Update Number of Steps to Reduce a Number in Binary Representation to One.py

--- Number of Steps to Reduce a Number in Binary Representation to One/Number of Steps to Reduce a Number in Binary Representation to One.py
+++ Number of Steps to Reduce a Number in Binary Representation to One/Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -1,1 +1,28 @@
 # Solution for Number of Steps to Reduce a Number in Binary Representation to One in PY
+
+class Solution:
+    def numSteps(self, s: str) -> int:
+        
+        length = len(s)               # Length of the binary string
+        total_operations = 0          # Counts total steps required
+        carry_from_addition = 0       # Simulates carry when we add 1
+        
+        # Traverse from right to left
+        for index in range(length - 1, 0, -1):
+            
+            # Convert current character to integer
+            current_bit = int(s[index])
+            
+            # Add carry
+            effective_bit = current_bit + carry_from_addition
+            
+            # If even → divide by 2
+            if effective_bit % 2 == 0:
+                total_operations += 1
+            # If odd → add 1 then divide by 2
+            else:
+                total_operations += 2
+                carry_from_addition = 1
+        
+        # Add remaining carry if exists
+        return total_operations + carry_from_addition
