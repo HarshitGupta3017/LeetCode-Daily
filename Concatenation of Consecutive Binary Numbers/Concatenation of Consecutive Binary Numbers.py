5040bef0780d2ae8b4ea8df3cd7993029f6b9054
Harshit Gupta <harshitgupta3017@gmail.com>
2026-02-28 16:33:44 +0530

    Update Concatenation of Consecutive Binary Numbers.py

--- Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
+++ Concatenation of Consecutive Binary Numbers/Concatenation of Consecutive Binary Numbers.py
@@ -1,1 +1,13 @@
 # Solution for Concatenation of Consecutive Binary Numbers in PY
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+
+        finalNumber = 0
+        MOD = 10**9 + 7
+
+        for currentNumber in range(1, n + 1):
+            numberOfBits = currentNumber.bit_length()
+            finalNumber = ((finalNumber << numberOfBits) % MOD + currentNumber) % MOD
+
+        return finalNumber
