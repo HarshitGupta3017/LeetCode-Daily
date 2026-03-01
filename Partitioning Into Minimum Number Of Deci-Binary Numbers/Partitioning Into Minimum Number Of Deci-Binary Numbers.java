f239eec8c17112a9177061629443605a84203ad8
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-01 18:08:45 +0530

    Update Partitioning Into Minimum Number Of Deci-Binary Numbers.java

--- Partitioning Into Minimum Number Of Deci-Binary Numbers/Partitioning Into Minimum Number Of Deci-Binary Numbers.java
+++ Partitioning Into Minimum Number Of Deci-Binary Numbers/Partitioning Into Minimum Number Of Deci-Binary Numbers.java
@@ -1,1 +1,13 @@
 // Solution for Partitioning Into Minimum Number Of Deci-Binary Numbers in JAVA
+
+class Solution {
+    public int minPartitions(String n) {
+        char maxDigit = '0';
+        for (char c : n.toCharArray()) {
+            if (c > maxDigit) {
+                maxDigit = c;
+            }
+        }
+        return maxDigit - '0';
+    }
+}
