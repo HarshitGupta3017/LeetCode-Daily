681b1b9c0090dbda557899cb9b105e1a3ae8f515
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-08 17:26:15 +0530

    Update Find Unique Binary String.java

--- Find Unique Binary String/Find Unique Binary String.java
+++ Find Unique Binary String/Find Unique Binary String.java
@@ -1,1 +1,28 @@
 // Solution for Find Unique Binary String in JAVA
+
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+
+        /*
+         We construct a binary string guaranteed
+         to differ from every string in nums.
+
+         Cantor's Diagonal Argument:
+         Flip the i-th bit of nums[i].
+        */
+
+        int n = nums.length;
+        StringBuilder differentString = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+
+            // Look at diagonal bit nums[i][i] and flip it
+            if (nums[i].charAt(i) == '0')
+                differentString.append('1');
+            else
+                differentString.append('0');
+        }
+
+        return differentString.toString();
+    }
+}
