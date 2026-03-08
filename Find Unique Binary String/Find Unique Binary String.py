564e07776808c81a6730376342d4019d028f12b0
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-08 17:26:01 +0530

    Update Find Unique Binary String.py

--- Find Unique Binary String/Find Unique Binary String.py
+++ Find Unique Binary String/Find Unique Binary String.py
@@ -1,1 +1,20 @@
 # Solution for Find Unique Binary String in PY
+
+class Solution:
+    def findDifferentBinaryString(self, nums):
+        """
+        Construct a binary string that differs from
+        every string in nums using Cantor's Diagonal Argument.
+        """
+
+        n = len(nums)
+        different_string = []
+
+        for i in range(n):
+            # Flip the diagonal bit
+            if nums[i][i] == '0':
+                different_string.append('1')
+            else:
+                different_string.append('0')
+
+        return ''.join(different_string)
