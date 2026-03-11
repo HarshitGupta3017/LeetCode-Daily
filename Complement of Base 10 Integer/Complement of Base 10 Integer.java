826971cf5a05a68aaacbdd51bd8b2f9621c9c6d7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-11 14:21:06 +0530

    Update Complement of Base 10 Integer.java

--- Complement of Base 10 Integer/Complement of Base 10 Integer.java
+++ Complement of Base 10 Integer/Complement of Base 10 Integer.java
@@ -1,1 +1,7 @@
 // Solution for Complement of Base 10 Integer in JAVA
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        return n == 0 ? 1 : n ^ ((1 << ((int)(Math.log(n) / Math.log(2)) + 1)) - 1);
+    }
+}
