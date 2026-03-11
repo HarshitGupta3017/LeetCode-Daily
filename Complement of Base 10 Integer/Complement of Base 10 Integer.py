1ce74ab1456449e4287215bf66a2dad59f631776
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-11 14:20:43 +0530

    Update Complement of Base 10 Integer.py

--- Complement of Base 10 Integer/Complement of Base 10 Integer.py
+++ Complement of Base 10 Integer/Complement of Base 10 Integer.py
@@ -1,1 +1,5 @@
 # Solution for Complement of Base 10 Integer in PY
+
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        return 1 if n == 0 else n ^ ((1 << (int(log2(n)) + 1)) - 1)
