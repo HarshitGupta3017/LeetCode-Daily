0e8a20dc1101ff7abe4152872477d441841d59ff
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-18 15:51:49 +0530

    Update Count Submatrices with Top-Left Element and Sum Less Than k.py

--- Count Submatrices with Top-Left Element and Sum Less Than k/Count Submatrices with Top-Left Element and Sum Less Than k.py
+++ Count Submatrices with Top-Left Element and Sum Less Than k/Count Submatrices with Top-Left Element and Sum Less Than k.py
@@ -1,1 +1,32 @@
 # Solution for Count Submatrices with Top-Left Element and Sum Less Than k in PY
+
+class Solution:
+    def countSubmatrices(self, grid, k):
+        totalRows = len(grid)
+        totalCols = len(grid[0])
+
+        validSubmatrixCount = 0
+
+        for row in range(totalRows):
+            for col in range(totalCols):
+
+                # Add top
+                if row > 0:
+                    grid[row][col] += grid[row - 1][col]
+
+                # Add left
+                if col > 0:
+                    grid[row][col] += grid[row][col - 1]
+
+                # Subtract overlap
+                if row > 0 and col > 0:
+                    grid[row][col] -= grid[row - 1][col - 1]
+
+                # Check condition
+                if grid[row][col] <= k:
+                    validSubmatrixCount += 1
+                else:
+                    # Optimization
+                    break
+
+        return validSubmatrixCount
