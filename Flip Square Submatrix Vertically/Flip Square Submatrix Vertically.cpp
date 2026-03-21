8242e4a8bce92d1bf5edca839f65bd1623d91757
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-21 18:09:56 +0530

    Update Flip Square Submatrix Vertically.cpp

--- Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.cpp
+++ Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.cpp
@@ -1,1 +1,37 @@
 // Solution for Flip Square Submatrix Vertically in CPP
+
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+
+        /*
+            Define boundaries of the k x k submatrix
+        */
+        int topRow = x;
+        int bottomRow = x + k - 1;
+        int leftCol = y;
+        int rightCol = y + k - 1;
+
+        /*
+            We only need to process half rows
+            because each swap fixes two rows
+        */
+        while (topRow < bottomRow) {
+
+            /*
+                Swap entire rows within column range [y, y+k-1]
+            */
+            for (int col = leftCol; col <= rightCol; col++) {
+                swap(grid[topRow][col], grid[bottomRow][col]);
+            }
+
+            /*
+                Move pointers inward
+            */
+            topRow++;
+            bottomRow--;
+        }
+
+        return grid;
+    }
+};
