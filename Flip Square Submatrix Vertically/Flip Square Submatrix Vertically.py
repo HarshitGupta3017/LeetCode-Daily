820b3097bb1f98fc46cab333eef04e68d632e416
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-21 18:09:34 +0530

    Update Flip Square Submatrix Vertically.py

--- Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.py
+++ Flip Square Submatrix Vertically/Flip Square Submatrix Vertically.py
@@ -1,1 +1,22 @@
 # Solution for Flip Square Submatrix Vertically in PY
+
+class Solution:
+    def reverseSubmatrix(self, grid, x, y, k):
+        # Define boundaries
+        topRow = x
+        bottomRow = x + k - 1
+        leftCol = y
+        rightCol = y + k - 1
+
+        # Swap rows
+        while topRow < bottomRow:
+            for col in range(leftCol, rightCol + 1):
+                grid[topRow][col], grid[bottomRow][col] = (
+                    grid[bottomRow][col],
+                    grid[topRow][col],
+                )
+
+            topRow += 1
+            bottomRow -= 1
+
+        return grid
