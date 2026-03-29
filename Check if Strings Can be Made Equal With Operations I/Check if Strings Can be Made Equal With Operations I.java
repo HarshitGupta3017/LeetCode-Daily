30482466125bc9b4b3a24987ab7d963e429b8126
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-29 19:10:38 +0530

    Update Check if Strings Can be Made Equal With Operations I.java

--- Check if Strings Can be Made Equal With Operations I/Check if Strings Can be Made Equal With Operations I.java
+++ Check if Strings Can be Made Equal With Operations I/Check if Strings Can be Made Equal With Operations I.java
@@ -1,1 +1,15 @@
 // Solution for Check if Strings Can be Made Equal With Operations I in JAVA
+
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        boolean condition1 = 
+            (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+            (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0));
+
+        boolean condition2 = 
+            (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+            (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1));
+
+        return condition1 && condition2;
+    }
+}
