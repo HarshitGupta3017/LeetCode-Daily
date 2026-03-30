7b79c5703b03b90ef9a67d1381b84b600296520a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-03-30 10:29:25 +0530

    Update Check if Strings Can be Made Equal With Operations II.py

--- Check if Strings Can be Made Equal With Operations II/Check if Strings Can be Made Equal With Operations II.py
+++ Check if Strings Can be Made Equal With Operations II/Check if Strings Can be Made Equal With Operations II.py
@@ -1,1 +1,16 @@
 # Solution for Check if Strings Can be Made Equal With Operations II in PY
+
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even = [0]*26
+        odd = [0]*26
+
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                even[ord(s1[i]) - 97] += 1
+                even[ord(s2[i]) - 97] -= 1
+            else:
+                odd[ord(s1[i]) - 97] += 1
+                odd[ord(s2[i]) - 97] -= 1
+
+        return all(x == 0 for x in even) and all(x == 0 for x in odd)
