5a78525e284d281a3acbcf62d8ffdf7b7e1216c2
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-01 17:31:56 +0530

    Update Robot Collisions.py

--- Robot Collisions/Robot Collisions.py
+++ Robot Collisions/Robot Collisions.py
@@ -1,1 +1,24 @@
 # Solution for Robot Collisions in PY
+
+class Solution:
+    def survivedRobotsHealths(self, pos, health, dir):
+        idx = sorted(range(len(pos)), key=lambda i: pos[i])
+        stack = []
+
+        for i in idx:
+            if dir[i] == 'R':
+                stack.append(i)
+            else:
+                while stack and health[i] > 0:
+                    j = stack.pop()
+                    if health[j] > health[i]:
+                        health[j] -= 1
+                        health[i] = 0
+                        stack.append(j)
+                    elif health[j] < health[i]:
+                        health[i] -= 1
+                        health[j] = 0
+                    else:
+                        health[i] = health[j] = 0
+
+        return [h for h in health if h > 0]
