93d08494518fdf832829c94a946e9bc6b43bcf63
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-05 22:29:29 +0530

    Update Robot Return to Origin.java

--- Robot Return to Origin/Robot Return to Origin.java
+++ Robot Return to Origin/Robot Return to Origin.java
@@ -1,1 +1,14 @@
 // Solution for Robot Return to Origin in JAVA
+
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char ch: moves.toCharArray()) {
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'L') x--;
+            else x++;
+        }
+        return (x == 0 && y == 0);
+    }
+}
