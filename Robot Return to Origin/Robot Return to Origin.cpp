0ccc4a1132e4cdaa2449c57233ebf9446692ab02
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-05 22:29:39 +0530

    Update Robot Return to Origin.cpp

--- Robot Return to Origin/Robot Return to Origin.cpp
+++ Robot Return to Origin/Robot Return to Origin.cpp
@@ -1,1 +1,15 @@
 // Solution for Robot Return to Origin in CPP
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (const auto& ch: moves) {
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'L') x--;
+            else x++;
+        }
+        return (x == 0 && y == 0);
+    }
+};
