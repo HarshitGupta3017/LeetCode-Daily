645d3f30db2adea0eb7a75134f69f37b657f8d9d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-06 16:48:38 +0530

    Update Walking Robot Simulation.java

--- Walking Robot Simulation/Walking Robot Simulation.java
+++ Walking Robot Simulation/Walking Robot Simulation.java
@@ -1,1 +1,60 @@
 // Solution for Walking Robot Simulation in JAVA
+
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+
+        // STEP 1: Store obstacles
+        Set<String> obstacleSet = new HashSet<>();
+        for (int[] obs : obstacles) {
+            String key = obs[0] + "_" + obs[1];
+            obstacleSet.add(key);
+        }
+
+        // STEP 2: Initialize
+        int x = 0, y = 0;
+        int maxDistanceSquared = 0;
+
+        // Facing NORTH
+        int dx = 0, dy = 1;
+
+        // STEP 3: Process commands
+        for (int command : commands) {
+
+            // Turn Left
+            if (command == -2) {
+                int temp = dx;
+                dx = -dy;
+                dy = temp;
+            }
+
+            // Turn Right
+            else if (command == -1) {
+                int temp = dx;
+                dx = dy;
+                dy = -temp;
+            }
+
+            // Move Forward
+            else {
+                while (command-- > 0) {
+
+                    int nextX = x + dx;
+                    int nextY = y + dy;
+
+                    String key = nextX + "_" + nextY;
+
+                    // Stop if obstacle
+                    if (obstacleSet.contains(key)) break;
+
+                    x = nextX;
+                    y = nextY;
+                }
+            }
+
+            // Update max distance
+            maxDistanceSquared = Math.max(maxDistanceSquared, x * x + y * y);
+        }
+
+        return maxDistanceSquared;
+    }
+}
