53e0f46a6112c96c9db087e7ee93b46a72428dcc
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-11 15:11:49 +0530

    Update Minimum Distance Between Three Equal Elements II.py

--- Minimum Distance Between Three Equal Elements II/Minimum Distance Between Three Equal Elements II.py
+++ Minimum Distance Between Three Equal Elements II/Minimum Distance Between Three Equal Elements II.py
@@ -1,1 +1,19 @@
 # Solution for Minimum Distance Between Three Equal Elements II in PY
+
+class Solution:
+    def minimumDistance(self, nums):
+        mp = {}
+        ans = float('inf')
+
+        for i, val in enumerate(nums):
+            if val in mp:
+                prev2, prev1 = mp[val]
+
+                if prev2 != -1:
+                    ans = min(ans, 2 * (i - prev2))
+
+                mp[val] = (prev1, i)
+            else:
+                mp[val] = (-1, i)
+
+        return -1 if ans == float('inf') else ans
