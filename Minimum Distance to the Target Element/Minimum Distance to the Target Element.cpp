418f00a45c6f01cea473a44ffe4f728990240230
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-13 13:17:52 +0530

    Update Minimum Distance to the Target Element.cpp

--- Minimum Distance to the Target Element/Minimum Distance to the Target Element.cpp
+++ Minimum Distance to the Target Element/Minimum Distance to the Target Element.cpp
@@ -1,1 +1,14 @@
 // Solution for Minimum Distance to the Target Element in CPP
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int result = INT_MAX;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                result = min(result, abs(i - start));
+            }
+        }
+        return result;
+    }
+};
