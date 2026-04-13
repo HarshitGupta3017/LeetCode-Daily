c38fc53840690af110054231315370f4112136e0
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-13 13:17:40 +0530

    Update Minimum Distance to the Target Element.java

--- Minimum Distance to the Target Element/Minimum Distance to the Target Element.java
+++ Minimum Distance to the Target Element/Minimum Distance to the Target Element.java
@@ -1,1 +1,13 @@
 // Solution for Minimum Distance to the Target Element in JAVA
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                result = Math.min(result, Math.abs(i - start));
+            }
+        }
+        return result;
+    }
+}
