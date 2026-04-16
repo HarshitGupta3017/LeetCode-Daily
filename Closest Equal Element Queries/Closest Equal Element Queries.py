2b41c1574a74dee15d2b7f2e995b8a365e1371bd
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-16 13:53:02 +0530

    Update Closest Equal Element Queries.py

--- Closest Equal Element Queries/Closest Equal Element Queries.py
+++ Closest Equal Element Queries/Closest Equal Element Queries.py
@@ -1,1 +1,33 @@
 # Solution for Closest Equal Element Queries in PY
+
+from bisect import bisect_left
+from collections import defaultdict
+
+class Solution:
+    def solveQueries(self, nums, queries):
+        n = len(nums)
+
+        mp = defaultdict(list)
+        for i, v in enumerate(nums):
+            mp[v].append(i)
+
+        ans = []
+        for q in queries:
+            lst = mp[nums[q]]
+
+            if len(lst) == 1:
+                ans.append(-1)
+                continue
+
+            pos = bisect_left(lst, q)
+            size = len(lst)
+
+            prev = lst[(pos - 1) % size]
+            nxt = lst[(pos + 1) % size]
+
+            d1 = abs(prev - q)
+            d2 = abs(nxt - q)
+
+            ans.append(min(d1, d2, n - d1, n - d2))
+
+        return ans
