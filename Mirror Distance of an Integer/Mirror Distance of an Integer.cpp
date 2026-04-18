671493395c0ce2dd11121f5e29cd3781ddcc3e5e
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-18 22:19:55 +0530

    Update Mirror Distance of an Integer.cpp

--- Mirror Distance of an Integer/Mirror Distance of an Integer.cpp
+++ Mirror Distance of an Integer/Mirror Distance of an Integer.cpp
@@ -1,1 +1,17 @@
 // Solution for Mirror Distance of an Integer in CPP
+
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        auto getReverse = [](int x){
+            int rev = 0;
+            while (x > 0) {
+                rev = (rev * 10) + (x % 10);
+                x /= 10;
+            };
+            return rev;
+        };
+        
+        return abs(n - getReverse(n));
+    }
+};
