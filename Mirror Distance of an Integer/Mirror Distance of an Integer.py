9fda445779e5bebd595e11285cadbfdf781613b3
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-18 22:19:28 +0530

    Update Mirror Distance of an Integer.py

--- Mirror Distance of an Integer/Mirror Distance of an Integer.py
+++ Mirror Distance of an Integer/Mirror Distance of an Integer.py
@@ -1,1 +1,12 @@
 # Solution for Mirror Distance of an Integer in PY
+
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+        return abs(n - self.getReverse(n))
+        
+    def getReverse(self, x: int) -> int:
+        rev = 0
+        while (x > 0):
+            rev = (rev * 10) + (x % 10)
+            x //= 10
+        return rev
