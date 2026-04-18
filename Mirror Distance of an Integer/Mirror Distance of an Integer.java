ad7f7d0424013ff140bed7bbb001c218ce76ce66
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-18 22:19:43 +0530

    Update Mirror Distance of an Integer.java

--- Mirror Distance of an Integer/Mirror Distance of an Integer.java
+++ Mirror Distance of an Integer/Mirror Distance of an Integer.java
@@ -1,1 +1,16 @@
 // Solution for Mirror Distance of an Integer in JAVA
+
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n - getReverse(n));
+    }
+    
+    private int getReverse(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        };
+        return rev;
+    };
+}
