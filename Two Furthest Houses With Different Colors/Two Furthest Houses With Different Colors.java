0e320842af28564d91a2aca78562d02cb6acf37b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-20 11:58:48 +0530

    Update Two Furthest Houses With Different Colors.java

--- Two Furthest Houses With Different Colors/Two Furthest Houses With Different Colors.java
+++ Two Furthest Houses With Different Colors/Two Furthest Houses With Different Colors.java
@@ -1,1 +1,23 @@
 // Solution for Two Furthest Houses With Different Colors in JAVA
+
+class Solution {
+    public int maxDistance(int[] colors) {
+        int totalHouses = colors.length;
+        int maximumDistance = 0;
+
+        for (int currentIndex = 0; currentIndex < totalHouses; currentIndex++) {
+
+            // Case 1: Compare with first house
+            if (colors[currentIndex] != colors[0]) {
+                maximumDistance = Math.max(maximumDistance, currentIndex);
+            }
+
+            // Case 2: Compare with last house
+            if (colors[currentIndex] != colors[totalHouses - 1]) {
+                maximumDistance = Math.max(maximumDistance, totalHouses - 1 - currentIndex);
+            }
+        }
+
+        return maximumDistance;
+    }
+}
