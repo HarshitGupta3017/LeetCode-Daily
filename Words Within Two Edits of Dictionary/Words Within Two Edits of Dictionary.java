80bcbf54725b3ede57068550d41e53b102b0d5bb
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-22 15:55:46 +0530

    Update Words Within Two Edits of Dictionary.java

--- Words Within Two Edits of Dictionary/Words Within Two Edits of Dictionary.java
+++ Words Within Two Edits of Dictionary/Words Within Two Edits of Dictionary.java
@@ -1,1 +1,30 @@
 // Solution for Words Within Two Edits of Dictionary in JAVA
+
+class Solution {
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> matchingWords = new ArrayList<>();
+
+        for (String queryWord : queries) {
+            for (String dictWord : dictionary) {
+                int mismatchCount = 0;
+
+                for (int i = 0; i < queryWord.length(); i++) {
+                    if (queryWord.charAt(i) != dictWord.charAt(i)) {
+                        mismatchCount++;
+                    }
+
+                    if (mismatchCount > 2) {
+                        break;
+                    }
+                }
+
+                if (mismatchCount <= 2) {
+                    matchingWords.add(queryWord);
+                    break;
+                }
+            }
+        }
+
+        return matchingWords;
+    }
+}
