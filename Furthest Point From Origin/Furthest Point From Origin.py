c022b2588fee78e0cd1c11d8d3db928644600720
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-24 12:26:46 +0530

    Update Furthest Point From Origin.py

--- Furthest Point From Origin/Furthest Point From Origin.py
+++ Furthest Point From Origin/Furthest Point From Origin.py
@@ -1,1 +1,16 @@
 # Solution for Furthest Point From Origin in PY
+
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        
+        L = R = blank = 0
+        
+        for ch in moves:
+            if (ch == 'L'):
+                L += 1
+            elif (ch == 'R'):
+                R += 1
+            else:
+                blank += 1
+
+        return abs(L - R) + blank
