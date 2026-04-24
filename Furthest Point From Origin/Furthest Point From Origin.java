12de1d8ac988926f169793da015132141a2a5cf3
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-24 12:27:02 +0530

    Update Furthest Point From Origin.java

--- Furthest Point From Origin/Furthest Point From Origin.java
+++ Furthest Point From Origin/Furthest Point From Origin.java
@@ -1,1 +1,13 @@
 // Solution for Furthest Point From Origin in JAVA
+
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int L = 0, R = 0, blank = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'L') L++;
+            else if (ch == 'R') R++;
+            else blank++;
+        }
+        return Math.abs(L - R) + blank;
+    }
+}
