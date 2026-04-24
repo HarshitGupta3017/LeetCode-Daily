0e151fa097251509a6491097ab9335c9fbb2d126
Harshit Gupta <harshitgupta3017@gmail.com>
2026-04-24 12:27:15 +0530

    Update Furthest Point From Origin.cpp

--- Furthest Point From Origin/Furthest Point From Origin.cpp
+++ Furthest Point From Origin/Furthest Point From Origin.cpp
@@ -1,1 +1,14 @@
 // Solution for Furthest Point From Origin in CPP
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int L = 0, R = 0, blank = 0;
+        for (char ch : moves) {
+            if (ch == 'L') L++;
+            else if (ch == 'R') R++;
+            else blank++;
+        }
+        return abs(L - R) + blank;
+    }
+};
