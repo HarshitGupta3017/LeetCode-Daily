5bde17cc60ef6ca7eb760c91b6af1ee962cd88d7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-01 17:25:58 +0530

    Update Rotate Function.cpp

--- Rotate Function/Rotate Function.cpp
+++ Rotate Function/Rotate Function.cpp
@@ -1,1 +1,34 @@
 // Solution for Rotate Function in CPP
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+
+        int totalElements = nums.size();
+
+        int totalSum = 0;   // Sum of all elements
+        int currentValue = 0; // F(0)
+
+        // Step 1: Compute total sum and F(0)
+        for (int index = 0; index < totalElements; index++) {
+            totalSum += nums[index];
+            currentValue += index * nums[index];
+        }
+
+        int maximumValue = currentValue;
+
+        // Step 2: Use recurrence relation to compute F(k)
+        for (int rotation = 0; rotation < totalElements; rotation++) {
+
+            // Transition:
+            // F(k+1) = F(k) + totalSum - n * nums[n - 1 - k]
+            currentValue = currentValue + totalSum
+                           - totalElements * nums[totalElements - 1 - rotation];
+
+            // Update maximum
+            maximumValue = max(maximumValue, currentValue);
+        }
+
+        return maximumValue;
+    }
+};
