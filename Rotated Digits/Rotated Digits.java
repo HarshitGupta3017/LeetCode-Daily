c41d46a9920e5f5fc176080fef8cebaa9fab3193
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-02 21:47:07 +0530

    Update Rotated Digits.java

--- Rotated Digits/Rotated Digits.java
+++ Rotated Digits/Rotated Digits.java
@@ -1,1 +1,37 @@
 // Solution for Rotated Digits in JAVA
+
+class Solution {
+
+    // Check if number is "good"
+    private boolean isGoodNumber(int number) {
+
+        boolean hasValidChange = false;
+
+        while (number > 0) {
+            int digit = number % 10;
+
+            // Invalid digits
+            if (digit == 3 || digit == 4 || digit == 7)
+                return false;
+
+            // Digits that change after rotation
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9)
+                hasValidChange = true;
+
+            number /= 10;
+        }
+
+        return hasValidChange;
+    }
+
+    public int rotatedDigits(int n) {
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGoodNumber(i)) count++;
+        }
+
+        return count;
+    }
+}
