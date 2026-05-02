4deeb0c0d97831455c25f1ba2d9690a24c63aba6
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-02 21:46:50 +0530

    Update Rotated Digits.py

--- Rotated Digits/Rotated Digits.py
+++ Rotated Digits/Rotated Digits.py
@@ -1,1 +1,27 @@
 # Solution for Rotated Digits in PY
+
+class Solution:
+    def rotatedDigits(self, n):
+
+        def is_good(num):
+            has_change = False
+
+            while num > 0:
+                digit = num % 10
+
+                if digit in (3, 4, 7):
+                    return False
+
+                if digit in (2, 5, 6, 9):
+                    has_change = True
+
+                num //= 10
+
+            return has_change
+
+        count = 0
+        for i in range(1, n + 1):
+            if is_good(i):
+                count += 1
+
+        return count
