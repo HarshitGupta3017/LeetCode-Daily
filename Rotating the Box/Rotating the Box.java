586a4e6e7886133eef62a36f30375c4803df6a0c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-06 16:36:39 +0530

    Update Rotating the Box.java

--- Rotating the Box/Rotating the Box.java
+++ Rotating the Box/Rotating the Box.java
@@ -1,1 +1,39 @@
 // Solution for Rotating the Box in JAVA
+
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+
+        int originalRows = boxGrid.length;
+        int originalCols = boxGrid[0].length;
+
+        // Create rotated matrix
+        char[][] rotatedBox = new char[originalCols][originalRows];
+
+        // STEP 1: Rotate the matrix
+        for (int row = 0; row < originalRows; row++) {
+            for (int col = 0; col < originalCols; col++) {
+                rotatedBox[col][originalRows - row - 1] = boxGrid[row][col];
+            }
+        }
+
+        // STEP 2: Apply gravity
+        for (int col = 0; col < originalRows; col++) {
+
+            int emptyRowPointer = originalCols - 1;
+
+            for (int row = originalCols - 1; row >= 0; row--) {
+
+                if (rotatedBox[row][col] == '*') {
+                    emptyRowPointer = row - 1;
+                } 
+                else if (rotatedBox[row][col] == '#') {
+                    rotatedBox[row][col] = '.';
+                    rotatedBox[emptyRowPointer][col] = '#';
+                    emptyRowPointer--;
+                }
+            }
+        }
+
+        return rotatedBox;
+    }
+}
