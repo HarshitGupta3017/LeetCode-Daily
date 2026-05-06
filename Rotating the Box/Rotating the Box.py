6fdf72e057f9fee9717f9cd34c3a13e982f1e819
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-06 16:36:22 +0530

    Update Rotating the Box.py

--- Rotating the Box/Rotating the Box.py
+++ Rotating the Box/Rotating the Box.py
@@ -1,1 +1,31 @@
 # Solution for Rotating the Box in PY
+
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        
+        originalRows = len(boxGrid)
+        originalCols = len(boxGrid[0])
+
+        # Create rotated matrix
+        rotatedBox = [[None] * originalRows for _ in range(originalCols)]
+
+        # STEP 1: Rotate the matrix
+        for row in range(originalRows):
+            for col in range(originalCols):
+                rotatedBox[col][originalRows - row - 1] = boxGrid[row][col]
+
+        # STEP 2: Apply gravity
+        for col in range(originalRows):
+
+            emptyRowPointer = originalCols - 1
+
+            for row in range(originalCols - 1, -1, -1):
+
+                if rotatedBox[row][col] == '*':
+                    emptyRowPointer = row - 1
+                elif rotatedBox[row][col] == '#':
+                    rotatedBox[row][col] = '.'
+                    rotatedBox[emptyRowPointer][col] = '#'
+                    emptyRowPointer -= 1
+
+        return rotatedBox
