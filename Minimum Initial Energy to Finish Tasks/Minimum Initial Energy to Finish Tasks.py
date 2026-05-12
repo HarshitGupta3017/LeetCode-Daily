68d5bb8fe493f4e2abc1f4df4dbe0cb1d9338c11
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-12 14:32:27 +0530

    Update Minimum Initial Energy to Finish Tasks.py

--- Minimum Initial Energy to Finish Tasks/Minimum Initial Energy to Finish Tasks.py
+++ Minimum Initial Energy to Finish Tasks/Minimum Initial Energy to Finish Tasks.py
@@ -1,1 +1,21 @@
 # Solution for Minimum Initial Energy to Finish Tasks in PY
+
+class Solution:
+    def minimumEffort(self, tasks):
+
+        # Sort by (minimum - actual) descending
+        tasks.sort(key=lambda t: t[1] - t[0], reverse=True)
+
+        min_start_energy = 0
+        energy_consumed = 0
+
+        for actual, minimum in tasks:
+
+            min_start_energy = max(
+                min_start_energy,
+                energy_consumed + minimum
+            )
+
+            energy_consumed += actual
+
+        return min_start_energy
