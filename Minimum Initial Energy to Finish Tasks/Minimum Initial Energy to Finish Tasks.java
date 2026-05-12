c9daee86b9c604a992539d7b8d6eb79007277fcb
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-12 14:32:50 +0530

    Update Minimum Initial Energy to Finish Tasks.java

--- Minimum Initial Energy to Finish Tasks/Minimum Initial Energy to Finish Tasks.java
+++ Minimum Initial Energy to Finish Tasks/Minimum Initial Energy to Finish Tasks.java
@@ -1,1 +1,28 @@
 // Solution for Minimum Initial Energy to Finish Tasks in JAVA
+
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+
+        // Sort by (minimum - actual) in descending order
+        Arrays.sort(tasks, (a, b) ->
+                (b[1] - b[0]) - (a[1] - a[0]));
+
+        int minStartingEnergy = 0;
+        int energyConsumed = 0;
+
+        for (int[] task : tasks) {
+
+            int actualCost = task[0];
+            int minimumToStart = task[1];
+
+            minStartingEnergy = Math.max(
+                    minStartingEnergy,
+                    energyConsumed + minimumToStart
+            );
+
+            energyConsumed += actualCost;
+        }
+
+        return minStartingEnergy;
+    }
+}
