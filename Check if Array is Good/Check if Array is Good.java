cd9ed52dc9d02e019a76fbeaefe3db2a23d0c1b0
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-14 18:44:14 +0530

    Update Check if Array is Good.java

--- Check if Array is Good/Check if Array is Good.java
+++ Check if Array is Good/Check if Array is Good.java
@@ -1,1 +1,20 @@
 // Solution for Check if Array is Good in JAVA
+
+class Solution {
+    public boolean isGood(int[] nums) {
+
+        // Sort array
+        Arrays.sort(nums);
+
+        int expectedMax = nums.length - 1;
+
+        // Check sequence: 1,2,3,...,n-1
+        for (int i = 0; i < expectedMax; i++) {
+            if (nums[i] != i + 1)
+                return false;
+        }
+
+        // Last element must be n
+        return nums[expectedMax] == expectedMax;
+    }
+}
