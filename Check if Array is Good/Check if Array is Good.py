1d51892fa8b0c30b05550751ea5d00b68a5c787d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-14 18:44:01 +0530

    Update Check if Array is Good.py

--- Check if Array is Good/Check if Array is Good.py
+++ Check if Array is Good/Check if Array is Good.py
@@ -1,1 +1,16 @@
 # Solution for Check if Array is Good in PY
+
+class Solution:
+    def isGood(self, nums):
+
+        nums.sort()
+
+        expected_max = len(nums) - 1
+
+        # Check sequence: 1,2,3,...,n-1
+        for i in range(expected_max):
+            if nums[i] != i + 1:
+                return False
+
+        # Last element must be n
+        return nums[expected_max] == expected_max
