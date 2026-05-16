8daf363511da0d5bba05728b8c9b041710627d07
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-16 13:03:53 +0530

    Update Find Minimum in Rotated Sorted Array II.cpp

--- Find Minimum in Rotated Sorted Array II/Find Minimum in Rotated Sorted Array II.cpp
+++ Find Minimum in Rotated Sorted Array II/Find Minimum in Rotated Sorted Array II.cpp
@@ -1,1 +1,50 @@
 // Solution for Find Minimum in Rotated Sorted Array II in CPP
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        // Store index of smallest element found so far
+        int minimumIndex = 0;
+
+        while (left <= right) {
+
+            // Remove duplicate values from left boundary
+            // to reduce unnecessary comparisons
+            while (left < right && nums[left] == nums[left + 1]) {
+                left++;
+            }
+
+            // Remove duplicate values from right boundary
+            while (right > left && nums[right] == nums[right - 1]) {
+                right--;
+            }
+
+            int middle = left + (right - left) / 2;
+
+            // Update minimum element index if a smaller value is found
+            if (nums[middle] < nums[minimumIndex]) {
+                
+                minimumIndex = middle;
+            }
+
+            // If middle element is greater than rightmost element,
+            // rotation point must exist in the right half
+            if (nums[middle] > nums[right]) {
+
+                left = middle + 1;
+            }
+
+            // Otherwise, minimum lies in left half
+            else {
+
+                right = middle - 1;
+            }
+        }
+
+        return nums[minimumIndex];
+    }
+};
