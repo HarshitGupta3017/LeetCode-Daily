02a132b6b045863315218278ee8231ac92104b1f
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-22 15:21:17 +0530

    Update Search in Rotated Sorted Array.py

--- Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
@@ -1,1 +1,40 @@
 # Solution for Search in Rotated Sorted Array in PY
+
+class Solution:
+    def search(self, nums, target):
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+
+            middle = left + (right - left) // 2
+
+            # Target element found
+            if nums[middle] == target:
+                return middle
+
+            # Check if left half is sorted
+            if nums[left] <= nums[middle]:
+
+                # Target exists inside sorted left half
+                if nums[left] <= target < nums[middle]:
+                    right = middle - 1
+
+                # Otherwise search in right half
+                else:
+                    left = middle + 1
+
+            # Right half is sorted
+            else:
+
+                # Target exists inside sorted right half
+                if nums[middle] < target <= nums[right]:
+                    left = middle + 1
+
+                # Otherwise search in left half
+                else:
+                    right = middle - 1
+
+        # Target not present
+        return -1
