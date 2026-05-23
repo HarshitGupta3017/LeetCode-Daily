a574e9e99808d6192868934a60396a57d3de7a8a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-23 13:21:55 +0530

    Update Check if Array Is Sorted and Rotated.java

--- Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.java
+++ Check if Array Is Sorted and Rotated/Check if Array Is Sorted and Rotated.java
@@ -1,1 +1,18 @@
 // Solution for Check if Array Is Sorted and Rotated in JAVA
+
+class Solution {
+    public boolean check(int[] nums) {
+
+        int n = nums.length;
+        int breaksCount = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] > nums[(i + 1) % n]) {
+                breaksCount++;
+            }
+        }
+
+        return breaksCount <= 1;
+    }
+}
