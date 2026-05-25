3d29efe73f9dc38e9613c0733531e6feb3bb7b5a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-25 18:17:36 +0530

    Update Jump Game VII.py

--- Jump Game VII/Jump Game VII.py
+++ Jump Game VII/Jump Game VII.py
@@ -1,1 +1,19 @@
 # Solution for Jump Game VII in PY
+
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        dp = [0] * n
+        dp[0] = 1
+        window = 0
+
+        for i in range(1, n):
+            if i - minJump >= 0:
+                window += dp[i - minJump]
+            if i - maxJump - 1 >= 0:
+                window -= dp[i - maxJump - 1]
+
+            if window > 0 and s[i] == '0':
+                dp[i] = 1
+
+        return dp[-1] == 1
