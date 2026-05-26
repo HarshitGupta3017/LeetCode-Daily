e192d3be8095bcd2cc6f5c86904f0133b6f801b4
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-26 11:19:05 +0530

    Update Count the Number of Special Characters I.py

--- Count the Number of Special Characters I/Count the Number of Special Characters I.py
+++ Count the Number of Special Characters I/Count the Number of Special Characters I.py
@@ -1,1 +1,7 @@
 # Solution for Count the Number of Special Characters I in PY
+
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        lower = set(filter(str.islower, word))
+        upper = set(filter(str.isupper, word))
+        return sum(c.upper() in upper for c in lower)
