e538d79e3c472397e46c961231ab7d5b162d96e9
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-29 18:28:54 +0530

    Update Minimum Element After Replacement With Digit Sum.py

--- Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.py
+++ Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.py
@@ -1,1 +1,5 @@
 # Solution for Minimum Element After Replacement With Digit Sum in PY
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        return min(sum(map(int, str(number))) for number in nums)
