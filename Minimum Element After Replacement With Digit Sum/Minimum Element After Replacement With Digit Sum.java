f51c9f3bb34ab52f939502a4fe6a669b699f816d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-29 18:29:08 +0530

    Update Minimum Element After Replacement With Digit Sum.java

--- Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.java
+++ Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.java
@@ -1,1 +1,22 @@
 // Solution for Minimum Element After Replacement With Digit Sum in JAVA
+
+class Solution {
+    public int minElement(int[] nums) {
+
+        int minimumValue = 37;
+
+        for (int number : nums) {
+
+            int digitSum = 0;
+
+            while (number > 0) {
+                digitSum += number % 10;
+                number /= 10;
+            }
+
+            minimumValue = Math.min(minimumValue, digitSum);
+        }
+
+        return minimumValue;
+    }
+}
