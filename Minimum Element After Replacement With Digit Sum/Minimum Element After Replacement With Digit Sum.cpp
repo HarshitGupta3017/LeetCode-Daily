fd59a1dd56e7d717faa63840e53778389cf52f17
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-29 18:29:23 +0530

    Update Minimum Element After Replacement With Digit Sum.cpp

--- Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.cpp
+++ Minimum Element After Replacement With Digit Sum/Minimum Element After Replacement With Digit Sum.cpp
@@ -1,1 +1,31 @@
 // Solution for Minimum Element After Replacement With Digit Sum in CPP
+
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+
+        // Maximum possible digit sum:
+        // 9999 -> 9 + 9 + 9 + 9 = 36
+        int minimumValue = 37;
+
+        for (int number : nums) {
+
+            int digitSum = 0;
+
+            // Calculate sum of digits
+            // for current number
+            while (number > 0) {
+
+                digitSum += number % 10;
+
+                number /= 10;
+            }
+
+            // Maintain minimum digit sum
+            // among all transformed values
+            minimumValue = min(minimumValue, digitSum);
+        }
+
+        return minimumValue;
+    }
+};
