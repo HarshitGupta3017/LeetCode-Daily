1c6049fb178b0983ad1c402580a2e3a734659b55
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-31 13:56:24 +0530

    Update Destroying Asteroids.java

--- Destroying Asteroids/Destroying Asteroids.java
+++ Destroying Asteroids/Destroying Asteroids.java
@@ -1,1 +1,26 @@
 // Solution for Destroying Asteroids in JAVA
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+
+        // Always process asteroids from smallest to largest.
+        Arrays.sort(asteroids);
+
+        // Mass can grow beyond the range of int.
+        long currentMass = mass;
+
+        for (int asteroidMass : asteroids) {
+
+            // If the planet cannot absorb this asteroid,
+            // it cannot absorb any larger one either.
+            if (currentMass < asteroidMass) {
+                return false;
+            }
+
+            // Absorb the asteroid and gain its mass.
+            currentMass += asteroidMass;
+        }
+
+        return true;
+    }
+}
