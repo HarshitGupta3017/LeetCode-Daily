80475814788daaaca5c301916565f5704e698138
Harshit Gupta <harshitgupta3017@gmail.com>
2026-05-31 13:56:14 +0530

    Update Destroying Asteroids.py

--- Destroying Asteroids/Destroying Asteroids.py
+++ Destroying Asteroids/Destroying Asteroids.py
@@ -1,1 +1,9 @@
 # Solution for Destroying Asteroids in PY
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        for a in sorted(asteroids):
+            if mass < a:
+                return False
+            mass += a
+        return True
