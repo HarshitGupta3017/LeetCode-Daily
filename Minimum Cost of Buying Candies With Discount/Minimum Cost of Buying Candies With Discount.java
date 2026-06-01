534dd73bbcc5f3b3c480e37b0e472deb09464d0a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-01 14:23:33 +0530

    Update Minimum Cost of Buying Candies With Discount.java

--- Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.java
+++ Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.java
@@ -1,1 +1,14 @@
 // Solution for Minimum Cost of Buying Candies With Discount in JAVA
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int totalCost = 0;
+
+        for (int i = cost.length - 1, count = 0; i >= 0; i--, count++) {
+            if (count % 3 != 2) totalCost += cost[i];
+        }
+
+        return totalCost;
+    }
+}
