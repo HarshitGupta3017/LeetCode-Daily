1f9774ef77288a7e6e61fd6ce03708f195c44c2f
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-01 14:23:22 +0530

    Update Minimum Cost of Buying Candies With Discount.py

--- Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.py
+++ Minimum Cost of Buying Candies With Discount/Minimum Cost of Buying Candies With Discount.py
@@ -1,1 +1,5 @@
 # Solution for Minimum Cost of Buying Candies With Discount in PY
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        return sum(c for i, c in enumerate(sorted(cost, reverse=True)) if i % 3 != 2)
