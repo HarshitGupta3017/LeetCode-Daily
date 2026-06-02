ad581ae3f45f75915428ea5afce9844c1d86fb74
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-02 12:29:56 +0530

    Update Earliest Finish Time for Land and Water Rides I.py

--- Earliest Finish Time for Land and Water Rides I/Earliest Finish Time for Land and Water Rides I.py
+++ Earliest Finish Time for Land and Water Rides I/Earliest Finish Time for Land and Water Rides I.py
@@ -1,1 +1,20 @@
 # Solution for Earliest Finish Time for Land and Water Rides I in PY
+
+class Solution:
+    def earliestFinishTime(self, landStartTime, landDuration,
+                           waterStartTime, waterDuration):
+        ans = float('inf')
+
+        for i in range(len(landStartTime)):
+            land_end = landStartTime[i] + landDuration[i]
+
+            for j in range(len(waterStartTime)):
+                water_end = waterStartTime[j] + waterDuration[j]
+
+                ans = min(
+                    ans,
+                    max(land_end, waterStartTime[j]) + waterDuration[j],
+                    max(water_end, landStartTime[i]) + landDuration[i]
+                )
+
+        return ans
