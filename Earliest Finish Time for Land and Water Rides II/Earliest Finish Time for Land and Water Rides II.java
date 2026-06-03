3f14a0277b83265def00866308c880d2061280a5
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-03 12:16:26 +0530

    Update Earliest Finish Time for Land and Water Rides II.java

--- Earliest Finish Time for Land and Water Rides II/Earliest Finish Time for Land and Water Rides II.java
+++ Earliest Finish Time for Land and Water Rides II/Earliest Finish Time for Land and Water Rides II.java
@@ -1,1 +1,23 @@
 // Solution for Earliest Finish Time for Land and Water Rides II in JAVA
+
+class Solution {
+    private void solve(int[] s1, int[] d1, int[] s2, int[] d2, int[] ans) {
+        int firstFinish = Integer.MAX_VALUE;
+
+        for (int i = 0; i < s1.length; i++)
+            firstFinish = Math.min(firstFinish, s1[i] + d1[i]);
+
+        for (int j = 0; j < s2.length; j++)
+            ans[0] = Math.min(ans[0], Math.max(firstFinish, s2[j]) + d2[j]);
+    }
+
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+        int[] ans = {Integer.MAX_VALUE};
+
+        solve(landStartTime, landDuration, waterStartTime, waterDuration, ans);
+        solve(waterStartTime, waterDuration, landStartTime, landDuration, ans);
+
+        return ans[0];
+    }
+}
