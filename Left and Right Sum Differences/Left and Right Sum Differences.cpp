18e8b6897f40869336ec92e0869374ef6be311dd
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-06 12:39:08 +0530

    Update Left and Right Sum Differences.cpp

--- Left and Right Sum Differences/Left and Right Sum Differences.cpp
+++ Left and Right Sum Differences/Left and Right Sum Differences.cpp
@@ -1,1 +1,20 @@
 // Solution for Left and Right Sum Differences in CPP
+
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        int leftSum = 0, rightSum = totalSum;
+        vector<int> result;
+
+        for (const auto& num: nums) {
+            rightSum -= num;
+            result.push_back(abs(leftSum - rightSum));
+            leftSum += num;
+        }
+
+        return result;
+    }
+};
