d1b0165c78ecd8180d8ba5a41f488fd514e215f4
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-06 12:38:59 +0530

    Update Left and Right Sum Differences.java

--- Left and Right Sum Differences/Left and Right Sum Differences.java
+++ Left and Right Sum Differences/Left and Right Sum Differences.java
@@ -1,1 +1,22 @@
 // Solution for Left and Right Sum Differences in JAVA
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        int rightSum = totalSum;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            result[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
+        }
+
+        return result;
+    }
+}
