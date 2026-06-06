77c74b86e18c84e5249306ea216979b29f450082
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-06 12:38:48 +0530

    Update Left and Right Sum Differences.py

--- Left and Right Sum Differences/Left and Right Sum Differences.py
+++ Left and Right Sum Differences/Left and Right Sum Differences.py
@@ -1,1 +1,16 @@
 # Solution for Left and Right Sum Differences in PY
+
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        total_sum = sum(nums)
+
+        left_sum = 0
+        right_sum = total_sum
+        result = []
+
+        for num in nums:
+            right_sum -= num
+            result.append(abs(left_sum - right_sum))
+            left_sum += num
+
+        return result
