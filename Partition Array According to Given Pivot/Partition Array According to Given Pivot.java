a6b9fcf11e2bc711aa50988dfa758f3f1dc5424b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-08 12:29:12 +0530

    Update Partition Array According to Given Pivot.java

--- Partition Array According to Given Pivot/Partition Array According to Given Pivot.java
+++ Partition Array According to Given Pivot/Partition Array According to Given Pivot.java
@@ -1,1 +1,30 @@
 // Solution for Partition Array According to Given Pivot in JAVA
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0, right = n - 1;
+        int i = 0, j = n - 1;
+
+        while (i < n && j >= 0) {
+            if (nums[i] < pivot) {
+                result[left++] = nums[i];
+            }
+
+            if (nums[j] > pivot) {
+                result[right--] = nums[j];
+            }
+
+            i++;
+            j--;
+        }
+
+        while (left <= right) {
+            result[left++] = pivot;
+        }
+
+        return result;
+    }
+}
