300a491b40c6138ca81e6538f459ab60d15b87c9
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-09 12:03:36 +0530

    Update Maximum Total Subarray Value I.java

--- Maximum Total Subarray Value I/Maximum Total Subarray Value I.java
+++ Maximum Total Subarray Value I/Maximum Total Subarray Value I.java
@@ -1,1 +1,16 @@
 // Solution for Maximum Total Subarray Value I in JAVA
+
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+
+        int globalMax = Integer.MIN_VALUE;
+        int globalMin = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            globalMax = Math.max(globalMax, num);
+            globalMin = Math.min(globalMin, num);
+        }
+
+        return (long) (globalMax - globalMin) * k;
+    }
+}
