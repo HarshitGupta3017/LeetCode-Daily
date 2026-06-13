d147ccc0f88fd859ac07bc48359926ec8e7bb8a1
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-13 22:15:15 +0530

    Update Weighted Word Mapping.cpp

--- Weighted Word Mapping/Weighted Word Mapping.cpp
+++ Weighted Word Mapping/Weighted Word Mapping.cpp
@@ -1,1 +1,16 @@
 // Solution for Weighted Word Mapping in CPP
+
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        for (string word : words) {
+            int s = 0;
+            for (char c : word) {
+                s += weights[c - 'a'];
+            }
+            ans += 'z' - s % 26;
+        }
+        return ans;
+    }
+};
