ee543236bfd0e3f5b707a3c1fe8d07630312d258
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-13 22:15:01 +0530

    Update Weighted Word Mapping.java

--- Weighted Word Mapping/Weighted Word Mapping.java
+++ Weighted Word Mapping/Weighted Word Mapping.java
@@ -1,1 +1,21 @@
 // Solution for Weighted Word Mapping in JAVA
+
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+
+            int sum = 0;
+
+            for (char c : word.toCharArray()) {
+                sum += weights[c - 'a'];
+            }
+
+            ans.append((char) ('z' - (sum % 26)));
+        }
+
+        return ans.toString();
+    }
+}
