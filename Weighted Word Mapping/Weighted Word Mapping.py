559e953fcdb063feecc23b52a19c620446bfd9dc
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-13 22:14:49 +0530

    Update Weighted Word Mapping.py

--- Weighted Word Mapping/Weighted Word Mapping.py
+++ Weighted Word Mapping/Weighted Word Mapping.py
@@ -1,1 +1,5 @@
 # Solution for Weighted Word Mapping in PY
+
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        return ''.join(chr(ord('z') - sum(weights[ord(c) - ord('a')] for c in word) % 26) for word in words)
