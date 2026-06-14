8675efdbea944a2bb606a0f23a2a60a2399b411d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-14 19:56:39 +0530

    Update Maximum Twin Sum of a Linked List.py

--- Maximum Twin Sum of a Linked List/Maximum Twin Sum of a Linked List.py
+++ Maximum Twin Sum of a Linked List/Maximum Twin Sum of a Linked List.py
@@ -1,1 +1,12 @@
 # Solution for Maximum Twin Sum of a Linked List in PY
+
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+
+        vals = []
+
+        while head:
+            vals.append(head.val)
+            head = head.next
+
+        return max(vals[i] + vals[~i] for i in range(len(vals) // 2))
