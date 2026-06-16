781e2af207e23d6735c01a190448e4809cbbaacb
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-16 12:32:21 +0530

    Update Process String with Special Operations I.py

--- Process String with Special Operations I/Process String with Special Operations I.py
+++ Process String with Special Operations I/Process String with Special Operations I.py
@@ -1,1 +1,23 @@
 # Solution for Process String with Special Operations I in PY
+
+class Solution:
+    def processStr(self, s: str) -> str:
+
+        result = []
+
+        for ch in s:
+
+            if ch == '*':
+                if result:
+                    result.pop()
+
+            elif ch == '#':
+                result += result
+
+            elif ch == '%':
+                result.reverse()
+
+            else:
+                result.append(ch)
+
+        return ''.join(result)
