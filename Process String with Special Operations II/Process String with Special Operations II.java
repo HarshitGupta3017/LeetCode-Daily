82ac240012896baa6f6d72aec0c07c449ce87bee
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-17 15:23:37 +0530

    Update Process String with Special Operations II.java

--- Process String with Special Operations II/Process String with Special Operations II.java
+++ Process String with Special Operations II/Process String with Special Operations II.java
@@ -1,1 +1,34 @@
 // Solution for Process String with Special Operations II in JAVA
+
+class Solution {
+    public char processStr(String s, long k) {
+        long len = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                if (len > 0) len--;
+            } else if (c == '#') {
+                len *= 2;
+            } else if (c != '%') {
+                len++;
+            }
+        }
+
+        if (k >= len) return '.';
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+
+            if (c == '*') len++;
+            else if (c == '%') k = len - k - 1;
+            else if (c == '#') {
+                len /= 2;
+                if (k >= len) k -= len;
+            } else {
+                len--;
+                if (k == len) return c;
+            }
+        }
+        return '.';
+    }
+}
