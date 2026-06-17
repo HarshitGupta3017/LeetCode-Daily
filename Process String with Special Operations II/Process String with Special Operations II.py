2a576dba56a97c6b29ffb66f9d7045a14542b43b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-17 15:23:09 +0530

    Update Process String with Special Operations II.py

--- Process String with Special Operations II/Process String with Special Operations II.py
+++ Process String with Special Operations II/Process String with Special Operations II.py
@@ -1,1 +1,32 @@
 # Solution for Process String with Special Operations II in PY
+
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        n = 0
+
+        for c in s:
+            if c == '*':
+                n -= n > 0
+            elif c == '#':
+                n *= 2
+            elif c != '%':
+                n += 1
+
+        if k >= n:
+            return '.'
+
+        for c in reversed(s):
+            if c == '*':
+                n += 1
+            elif c == '%':
+                k = n - k - 1
+            elif c == '#':
+                n //= 2
+                if k >= n:
+                    k -= n
+            else:
+                n -= 1
+                if k == n:
+                    return c
+
+        return '.'
