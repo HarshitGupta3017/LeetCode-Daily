98aac415fff6feb74cb20f1469dfb68f50a7c916
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-21 23:08:11 +0530

    Update Maximum Ice Cream Bars.py

--- Maximum Ice Cream Bars/Maximum Ice Cream Bars.py
+++ Maximum Ice Cream Bars/Maximum Ice Cream Bars.py
@@ -1,1 +1,23 @@
 # Solution for Maximum Ice Cream Bars in PY
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+
+        freq = [0] * (max(costs) + 1)
+
+        for cost in costs:
+            freq[cost] += 1
+
+        count = 0
+
+        for price in range(1, len(freq)):
+
+            can_buy = min(freq[price], coins // price)
+
+            count += can_buy
+            coins -= can_buy * price
+
+            if coins < price:
+                break
+
+        return count
