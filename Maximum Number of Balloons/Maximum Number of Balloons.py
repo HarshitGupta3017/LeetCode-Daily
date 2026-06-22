64a6c605b2787cc6ce243ca7ae93b315e06021de
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-22 13:35:46 +0530

    Update Maximum Number of Balloons.py

--- Maximum Number of Balloons/Maximum Number of Balloons.py
+++ Maximum Number of Balloons/Maximum Number of Balloons.py
@@ -1,1 +1,6 @@
 # Solution for Maximum Number of Balloons in PY
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        c = Counter(text); return min(c['b'], c['a'], c['l']//2, c['o']//2, c['n'])
