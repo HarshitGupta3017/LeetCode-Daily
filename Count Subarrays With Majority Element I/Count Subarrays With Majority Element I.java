03e3691c5e57a142b01fe16c0f15af6ad56bd84b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-25 12:42:47 +0530

    Update Count Subarrays With Majority Element I.java

--- Count Subarrays With Majority Element I/Count Subarrays With Majority Element I.java
+++ Count Subarrays With Majority Element I/Count Subarrays With Majority Element I.java
@@ -1,1 +1,27 @@
 // Solution for Count Subarrays With Majority Element I in JAVA
+
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int result = 0;
+
+        for (int left = 0; left < n; left++) {
+            int targetCount = 0;
+
+            for (int right = left; right < n; right++) {
+                if (nums[right] == target) {
+                    targetCount++;
+                }
+
+                int subarrayLen = right - left + 1;
+
+                // target is a majority if it appears more than half the time
+                if (targetCount * 2 > subarrayLen) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
