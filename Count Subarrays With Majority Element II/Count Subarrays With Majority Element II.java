b224fbc9e156c860fc87954abbfde9fba1f1ee7e
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-26 22:32:48 +0530

    Update Count Subarrays With Majority Element II.java

--- Count Subarrays With Majority Element II/Count Subarrays With Majority Element II.java
+++ Count Subarrays With Majority Element II/Count Subarrays With Majority Element II.java
@@ -1,1 +1,27 @@
 // Solution for Count Subarrays With Majority Element II in JAVA
+
+class Solution {
+    public long countMajoritySubarrays(int[] nums, int target) {
+
+        Map<Integer, Integer> prefixFreq = new HashMap<>();
+        prefixFreq.put(0, 1);
+
+        int prefixSum = 0, validLeftCount = 0;
+        long result = 0;
+
+        for (int num : nums) {
+            if (num == target) {
+                validLeftCount += prefixFreq.getOrDefault(prefixSum, 0);
+                prefixSum++;
+            } else {
+                prefixSum--;
+                validLeftCount -= prefixFreq.getOrDefault(prefixSum, 0);
+            }
+
+            prefixFreq.put(prefixSum, prefixFreq.getOrDefault(prefixSum, 0) + 1);
+            result += validLeftCount;
+        }
+
+        return result;
+    }
+}
