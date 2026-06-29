0cb93d2eb99427424ab7397946c4197a836a7afc
Harshit Gupta <harshitgupta3017@gmail.com>
2026-06-29 11:54:34 +0530

    Update Number of Strings That Appear as Substrings in Word.cpp

--- Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.cpp
+++ Number of Strings That Appear as Substrings in Word/Number of Strings That Appear as Substrings in Word.cpp
@@ -1,1 +1,16 @@
 // Solution for Number of Strings That Appear as Substrings in Word in CPP
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int count = 0;
+
+        for (const string& pattern : patterns) {
+            if (word.find(pattern) != string::npos) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
