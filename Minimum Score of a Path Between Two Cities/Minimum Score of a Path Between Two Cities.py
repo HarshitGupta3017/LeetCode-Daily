a7cc93c96d6f0f646a4748ceb162e8ab81539fbd
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-04 14:49:13 +0530

    Update Minimum Score of a Path Between Two Cities.py

--- Minimum Score of a Path Between Two Cities/Minimum Score of a Path Between Two Cities.py
+++ Minimum Score of a Path Between Two Cities/Minimum Score of a Path Between Two Cities.py
@@ -1,1 +1,22 @@
 # Solution for Minimum Score of a Path Between Two Cities in PY
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        g = defaultdict(list)
+        for u, v, w in roads:
+            g[u].append((v, w))
+            g[v].append((u, w))
+
+        vis = set()
+        ans = float('inf')
+
+        def dfs(u):
+            nonlocal ans
+            vis.add(u)
+            for v, w in g[u]:
+                ans = min(ans, w)
+                if v not in vis:
+                    dfs(v)
+
+        dfs(1)
+        return ans
