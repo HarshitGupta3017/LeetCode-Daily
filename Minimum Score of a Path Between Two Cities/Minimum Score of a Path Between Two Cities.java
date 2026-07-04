d84e3c2cd79bc4865925e088a7489f21fbbb9f34
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-04 14:49:24 +0530

    Update Minimum Score of a Path Between Two Cities.java

--- Minimum Score of a Path Between Two Cities/Minimum Score of a Path Between Two Cities.java
+++ Minimum Score of a Path Between Two Cities/Minimum Score of a Path Between Two Cities.java
@@ -1,1 +1,24 @@
 // Solution for Minimum Score of a Path Between Two Cities in JAVA
+
+class Solution {
+    int ans = Integer.MAX_VALUE;
+
+    void dfs(int u, List<int[]>[] g, boolean[] vis) {
+        vis[u] = true;
+        for (int[] e : g[u]) {
+            ans = Math.min(ans, e[1]);
+            if (!vis[e[0]]) dfs(e[0], g, vis);
+        }
+    }
+
+    public int minScore(int n, int[][] roads) {
+        List<int[]>[] g = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) g[i] = new ArrayList<>();
+        for (int[] r : roads) {
+            g[r[0]].add(new int[]{r[1], r[2]});
+            g[r[1]].add(new int[]{r[0], r[2]});
+        }
+        dfs(1, g, new boolean[n + 1]);
+        return ans;
+    }
+}
