14074c0b59883184e946081d21b9c5b0d8787bea
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-05 21:46:42 +0530

    Update Number of Paths with Max Score.py

--- Number of Paths with Max Score/Number of Paths with Max Score.py
+++ Number of Paths with Max Score/Number of Paths with Max Score.py
@@ -1,1 +1,34 @@
 # Solution for Number of Paths with Max Score in PY
+
+class Solution:
+    MOD = 10**9 + 7
+
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        n = len(board)
+
+        @lru_cache(None)
+        def solve(i, j):
+            if board[i][j] == 'E':
+                return (0, 1)
+            if board[i][j] == 'X':
+                return (0, 0)
+
+            best_score = best_paths = 0
+
+            for ni, nj in ((i - 1, j), (i, j - 1), (i - 1, j - 1)):
+                if 0 <= ni < n and 0 <= nj < n and board[ni][nj] != 'X':
+                    score, paths = solve(ni, nj)
+                    if paths:
+                        score += 0 if board[i][j] == 'S' else int(board[i][j])
+
+                    if score > best_score:
+                        best_score, best_paths = score, paths
+                    elif score == best_score and paths:
+                        best_paths = (best_paths + paths) % self.MOD
+
+            if best_paths == 0:
+                best_score = 0
+
+            return (best_score, best_paths)
+
+        return list(solve(n - 1, n - 1))
