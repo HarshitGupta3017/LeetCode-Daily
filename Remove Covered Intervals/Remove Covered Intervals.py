309b2d259daa6195caa099e91baf1e3f205cdb51
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-06 12:14:14 +0530

    Update Remove Covered Intervals.py

--- Remove Covered Intervals/Remove Covered Intervals.py
+++ Remove Covered Intervals/Remove Covered Intervals.py
@@ -1,1 +1,16 @@
 # Solution for Remove Covered Intervals in PY
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        # Sort by left endpoint ascending; if equal, right endpoint descending.
+        intervals.sort(key=lambda x: (x[0], -x[1]))
+
+        max_right = 0
+        remaining = 0
+
+        for left, right in intervals:
+            if right > max_right:
+                max_right = right
+                remaining += 1
+
+        return remaining
