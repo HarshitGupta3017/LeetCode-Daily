0725297a0611cc90a9000152adefe40374f2cc2d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-23 15:24:43 +0530

    Update Number of Unique XOR Triplets I.py

--- Number of Unique XOR Triplets I/Number of Unique XOR Triplets I.py
+++ Number of Unique XOR Triplets I/Number of Unique XOR Triplets I.py
@@ -1,1 +1,11 @@
 # Solution for Number of Unique XOR Triplets I in PY
+
+class Solution:
+    def uniqueXorTriplets(self, nums):
+        n = len(nums)
+        if n <= 2:
+            return n
+        p = 1
+        while p <= n:
+            p <<= 1
+        return p
