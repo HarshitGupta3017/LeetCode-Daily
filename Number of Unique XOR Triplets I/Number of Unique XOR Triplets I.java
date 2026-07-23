683d414bb1bbe3ef1335f4d286030646ed29a22f
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-23 15:24:58 +0530

    Update Number of Unique XOR Triplets I.java

--- Number of Unique XOR Triplets I/Number of Unique XOR Triplets I.java
+++ Number of Unique XOR Triplets I/Number of Unique XOR Triplets I.java
@@ -1,1 +1,11 @@
 // Solution for Number of Unique XOR Triplets I in JAVA
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) return n;
+        int p = 1;
+        while (p <= n) p <<= 1;
+        return p;
+    }
+}
