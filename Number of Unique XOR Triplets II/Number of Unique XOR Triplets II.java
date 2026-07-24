2a9510e15db1fe3205f1b864ffc89f3c906b6ab9
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-24 14:29:51 +0530

    Update Number of Unique XOR Triplets II.java

--- Number of Unique XOR Triplets II/Number of Unique XOR Triplets II.java
+++ Number of Unique XOR Triplets II/Number of Unique XOR Triplets II.java
@@ -1,1 +1,50 @@
 // Solution for Number of Unique XOR Triplets II in JAVA
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+
+        // Find the maximum value
+        int maximumValue = 0;
+        for (int num : nums) {
+            maximumValue = Math.max(maximumValue, num);
+        }
+
+        // Smallest power of two greater than maximumValue
+        int xorRange = 1;
+        while (xorRange <= maximumValue) {
+            xorRange <<= 1;
+        }
+
+        boolean[] pairXorExists = new boolean[xorRange];
+        boolean[] tripletXorExists = new boolean[xorRange];
+
+        // Generate all pair XORs
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pairXorExists[nums[i] ^ nums[j]] = true;
+            }
+        }
+
+        // Generate all triplet XORs
+        for (int xor = 0; xor < xorRange; xor++) {
+            if (!pairXorExists[xor]) {
+                continue;
+            }
+
+            for (int value : nums) {
+                tripletXorExists[xor ^ value] = true;
+            }
+        }
+
+        // Count distinct triplet XOR values
+        int count = 0;
+        for (boolean exists : tripletXorExists) {
+            if (exists) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
