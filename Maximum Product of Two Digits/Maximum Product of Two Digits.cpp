a1c098ef13e9fbdc7bac9600005497c1301e3513
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-25 15:17:45 +0530

    Update Maximum Product of Two Digits.cpp

--- Maximum Product of Two Digits/Maximum Product of Two Digits.cpp
+++ Maximum Product of Two Digits/Maximum Product of Two Digits.cpp
@@ -1,1 +1,19 @@
 // Solution for Maximum Product of Two Digits in CPP
+
+class Solution {
+public:
+    int maxProduct(int n) {
+        int first = 0, second = 0;
+        while (n > 0) {
+            int x = n % 10;
+            if (x > first) {
+                second = first;
+                first = x;
+            } else if (x > second) {
+                second = x;
+            }
+            n /= 10;
+        }
+        return first * second;
+    }
+};
