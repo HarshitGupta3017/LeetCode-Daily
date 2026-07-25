52be171ad49922c1890b662056bff37bf589dadd
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-25 15:17:27 +0530

    Update Maximum Product of Two Digits.py

--- Maximum Product of Two Digits/Maximum Product of Two Digits.py
+++ Maximum Product of Two Digits/Maximum Product of Two Digits.py
@@ -1,1 +1,18 @@
 # Solution for Maximum Product of Two Digits in PY
+
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        first = second = 0
+
+        while n > 0:
+            x = n % 10
+
+            if x > first:
+                second = first
+                first = x
+            elif x > second:
+                second = x
+
+            n //= 10
+
+        return first * second
