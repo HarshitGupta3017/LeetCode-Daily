16f0a1c4f993b71e0009e66a97b28e2356ef25a6
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-26 16:20:54 +0530

    Update Maximum Product of Three Numbers.py

--- Maximum Product of Three Numbers/Maximum Product of Three Numbers.py
+++ Maximum Product of Three Numbers/Maximum Product of Three Numbers.py
@@ -1,1 +1,15 @@
 # Solution for Maximum Product of Three Numbers in PY
+
+class Solution:
+    def maximumProduct(self, nums):
+        nums.sort()
+        n = len(nums)
+
+        # Case 1: Product of the three largest numbers
+        top_three = nums[n - 1] * nums[n - 2] * nums[n - 3]
+
+        # Case 2: Product of the two smallest (most negative)
+        # numbers and the largest number
+        two_neg_one_pos = nums[0] * nums[1] * nums[n - 1]
+
+        return max(top_three, two_neg_one_pos)
