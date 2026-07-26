841c91aea5b14d10fb4ac7afd50951704cf231eb
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-26 16:21:05 +0530

    Update Maximum Product of Three Numbers.java

--- Maximum Product of Three Numbers/Maximum Product of Three Numbers.java
+++ Maximum Product of Three Numbers/Maximum Product of Three Numbers.java
@@ -1,1 +1,17 @@
 // Solution for Maximum Product of Three Numbers in JAVA
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        // Case 1: Product of the three largest numbers
+        int topThree = nums[n - 1] * nums[n - 2] * nums[n - 3];
+
+        // Case 2: Product of the two smallest (most negative) numbers
+        // and the largest number
+        int twoNegOnePos = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(topThree, twoNegOnePos);
+    }
+}
