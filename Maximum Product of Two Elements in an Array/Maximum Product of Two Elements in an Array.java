6d0ff83ec890902cdf6704dc3b6867a1fca9bf2e
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-27 13:37:59 +0530

    Update Maximum Product of Two Elements in an Array.java

--- Maximum Product of Two Elements in an Array/Maximum Product of Two Elements in an Array.java
+++ Maximum Product of Two Elements in an Array/Maximum Product of Two Elements in an Array.java
@@ -1,1 +1,9 @@
 // Solution for Maximum Product of Two Elements in an Array in JAVA
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+    }
+}
