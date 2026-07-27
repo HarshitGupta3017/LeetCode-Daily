6ce996483003d97a1abb996fb4d716bd40d2b9b1
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-27 13:37:43 +0530

    Update Maximum Product of Two Elements in an Array.py

--- Maximum Product of Two Elements in an Array/Maximum Product of Two Elements in an Array.py
+++ Maximum Product of Two Elements in an Array/Maximum Product of Two Elements in an Array.py
@@ -1,1 +1,6 @@
 # Solution for Maximum Product of Two Elements in an Array in PY
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        *_, a, b = sorted(nums)
+        return (a - 1) * (b - 1)
