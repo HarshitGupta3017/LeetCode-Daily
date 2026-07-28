f0d2cd2ba7d350aa333eb24f903493f03bdba3f7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-28 15:31:08 +0530

    Update Smallest Palindromic Rearrangement I.py

--- Smallest Palindromic Rearrangement I/Smallest Palindromic Rearrangement I.py
+++ Smallest Palindromic Rearrangement I/Smallest Palindromic Rearrangement I.py
@@ -1,1 +1,10 @@
 # Solution for Smallest Palindromic Rearrangement I in PY
+
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        s = list(s)
+        half = len(s) // 2
+        s[:half] = sorted(s[:half])
+        for i in range(half):
+            s[-1 - i] = s[i]
+        return "".join(s)
