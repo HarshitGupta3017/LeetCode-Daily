118aadd397bcb3b8b78122318c41b02553af6248
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-28 15:31:19 +0530

    Update Smallest Palindromic Rearrangement I.java

--- Smallest Palindromic Rearrangement I/Smallest Palindromic Rearrangement I.java
+++ Smallest Palindromic Rearrangement I/Smallest Palindromic Rearrangement I.java
@@ -1,1 +1,15 @@
 // Solution for Smallest Palindromic Rearrangement I in JAVA
+
+class Solution {
+    public String smallestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length, half = n / 2;
+
+        Arrays.sort(arr, 0, half);
+
+        for (int i = 0; i < half; i++)
+            arr[n - 1 - i] = arr[i];
+
+        return new String(arr);
+    }
+}
