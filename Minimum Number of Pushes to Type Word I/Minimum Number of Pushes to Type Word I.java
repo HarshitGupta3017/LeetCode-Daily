3de3000e2e350200b185528b8cbc8ea7a91e409d
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-30 15:22:25 +0530

    Update Minimum Number of Pushes to Type Word I.java

--- Minimum Number of Pushes to Type Word I/Minimum Number of Pushes to Type Word I.java
+++ Minimum Number of Pushes to Type Word I/Minimum Number of Pushes to Type Word I.java
@@ -1,1 +1,15 @@
 // Solution for Minimum Number of Pushes to Type Word I in JAVA
+
+class Solution {
+    public int minimumPushes(String word) {
+        // We have 8 keys (2–9), each of which can hold any number of letters.
+        // The first 8 letters cost 1 push, the next 8 cost 2 pushes, and so on.
+        int totalPushes = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            totalPushes += (i / 8) + 1;
+        }
+
+        return totalPushes;
+    }
+}
