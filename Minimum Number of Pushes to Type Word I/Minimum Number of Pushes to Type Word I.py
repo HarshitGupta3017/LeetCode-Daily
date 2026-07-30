7c243c2cf3f12a17c76a234cca26119675040174
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-30 15:22:12 +0530

    Update Minimum Number of Pushes to Type Word I.py

--- Minimum Number of Pushes to Type Word I/Minimum Number of Pushes to Type Word I.py
+++ Minimum Number of Pushes to Type Word I/Minimum Number of Pushes to Type Word I.py
@@ -1,1 +1,12 @@
 # Solution for Minimum Number of Pushes to Type Word I in PY
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        # We have 8 keys (2–9), each of which can hold any number of letters.
+        # The first 8 letters cost 1 push, the next 8 cost 2 pushes, and so on.
+        total_pushes = 0
+
+        for i in range(len(word)):
+            total_pushes += (i // 8) + 1
+
+        return total_pushes
