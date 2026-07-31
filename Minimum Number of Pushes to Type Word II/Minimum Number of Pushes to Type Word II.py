fcb2edc1e35a5cfa0a4430121987a700865b51c6
Harshit Gupta <harshitgupta3017@gmail.com>
2026-07-31 18:33:29 +0530

    Update Minimum Number of Pushes to Type Word II.py

--- Minimum Number of Pushes to Type Word II/Minimum Number of Pushes to Type Word II.py
+++ Minimum Number of Pushes to Type Word II/Minimum Number of Pushes to Type Word II.py
@@ -1,1 +1,17 @@
 # Solution for Minimum Number of Pushes to Type Word II in PY
+
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        # Count frequency of each letter
+        freq = [0] * 26
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        # Sort frequencies in descending order
+        freq.sort(reverse=True)
+
+        total_pushes = 0
+        for i in range(26):
+            total_pushes += freq[i] * (i // 8 + 1)
+
+        return total_pushes
