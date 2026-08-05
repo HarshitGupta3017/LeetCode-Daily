f62e7b4c8d006e13e10236175325eace1241870a
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-05 17:54:42 +0530

    Update Remove Methods From Project.py

--- Remove Methods From Project/Remove Methods From Project.py
+++ Remove Methods From Project/Remove Methods From Project.py
@@ -1,1 +1,30 @@
 # Solution for Remove Methods From Project in PY
+
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(n)]
+        in_degree = [0] * n
+
+        for caller, callee in invocations:
+            adj[caller].append(callee)
+            in_degree[callee] += 1
+
+        suspicious = [False] * n
+        queue = deque([k])
+        suspicious[k] = True
+
+        while queue:
+            method = queue.popleft()
+
+            for callee in adj[method]:
+                in_degree[callee] -= 1
+
+                if not suspicious[callee]:
+                    suspicious[callee] = True
+                    queue.append(callee)
+
+        for method in range(n):
+            if suspicious[method] and in_degree[method] > 0:
+                return list(range(n))
+
+        return [method for method in range(n) if not suspicious[method]]
