0c0863b6e75494d7850ca0515a6ade80721a7741
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-06 05:42:32 +0530

    Update Smallest Divisible Digit Product I.java

--- Smallest Divisible Digit Product I/Smallest Divisible Digit Product I.java
+++ Smallest Divisible Digit Product I/Smallest Divisible Digit Product I.java
@@ -1,1 +1,20 @@
 // Solution for Smallest Divisible Digit Product I in JAVA
+
+class Solution {
+    public int digitProduct(int n) {
+        int product = 1;
+        while (n > 0) {
+            product *= (n % 10);
+            n /= 10;
+        }
+        return product;
+    }
+
+    public int smallestNumber(int n, int t) {
+        for (int candidate = n; ; candidate++) {
+            if (digitProduct(candidate) % t == 0) {
+                return candidate;
+            }
+        }
+    }
+}
