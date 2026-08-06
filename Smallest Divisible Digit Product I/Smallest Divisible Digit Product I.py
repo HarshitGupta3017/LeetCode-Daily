bcab38df42f090d0b2570b543d99568197ea0186
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-06 05:42:21 +0530

    Update Smallest Divisible Digit Product I.py

--- Smallest Divisible Digit Product I/Smallest Divisible Digit Product I.py
+++ Smallest Divisible Digit Product I/Smallest Divisible Digit Product I.py
@@ -1,1 +1,16 @@
 # Solution for Smallest Divisible Digit Product I in PY
+
+class Solution:
+    def digitProduct(self, n: int) -> int:
+        product = 1
+        while n > 0:
+            product *= n % 10
+            n //= 10
+        return product
+
+    def smallestNumber(self, n: int, t: int) -> int:
+        candidate = n
+        while True:
+            if self.digitProduct(candidate) % t == 0:
+                return candidate
+            candidate += 1
