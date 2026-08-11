e053eb60531860df5fc15f272010275c3d90f618
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-11 12:21:34 +0530

    Update Smallest Missing Integer Greater Than Sequential Prefix Sum.java

--- Smallest Missing Integer Greater Than Sequential Prefix Sum/Smallest Missing Integer Greater Than Sequential Prefix Sum.java
+++ Smallest Missing Integer Greater Than Sequential Prefix Sum/Smallest Missing Integer Greater Than Sequential Prefix Sum.java
@@ -1,1 +1,29 @@
 // Solution for Smallest Missing Integer Greater Than Sequential Prefix Sum in JAVA
+
+class Solution {
+    public int missingInteger(int[] nums) {
+        // Compute the sum of the longest sequential prefix.
+        int prefixSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                break;
+            }
+            prefixSum += nums[i];
+        }
+
+        // Store all values for O(1) average membership checks.
+        HashSet<Integer> present = new HashSet<>();
+
+        for (int num : nums) {
+            present.add(num);
+        }
+
+        // Find the smallest integer >= prefixSum not present in nums.
+        while (present.contains(prefixSum)) {
+            prefixSum++;
+        }
+
+        return prefixSum;
+    }
+}
