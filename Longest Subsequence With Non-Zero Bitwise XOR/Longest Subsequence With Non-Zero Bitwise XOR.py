06ca9c0b461053450806e1b05b2b289d6e844994
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-15 21:39:53 +0530

    Update Longest Subsequence With Non-Zero Bitwise XOR.py

--- Longest Subsequence With Non-Zero Bitwise XOR/Longest Subsequence With Non-Zero Bitwise XOR.py
+++ Longest Subsequence With Non-Zero Bitwise XOR/Longest Subsequence With Non-Zero Bitwise XOR.py
@@ -1,1 +1,11 @@
 # Solution for Longest Subsequence With Non-Zero Bitwise XOR in PY
+
+class Solution:
+    def longestSubsequence(self, nums):
+        xor = 0
+        for x in nums:
+            xor ^= x
+
+        if xor != 0:
+            return len(nums)
+        return 0 if all(x == 0 for x in nums) else len(nums) - 1
