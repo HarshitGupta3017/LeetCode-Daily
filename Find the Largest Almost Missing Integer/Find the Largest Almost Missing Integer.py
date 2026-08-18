379fd9e3ba5b885f19d6e9ad6ff32ad23d125938
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-18 14:09:39 +0530

    Update Find the Largest Almost Missing Integer.py

--- Find the Largest Almost Missing Integer/Find the Largest Almost Missing Integer.py
+++ Find the Largest Almost Missing Integer/Find the Largest Almost Missing Integer.py
@@ -1,1 +1,26 @@
 # Solution for Find the Largest Almost Missing Integer in PY
+
+class Solution:
+    def largestInteger(self, nums, k):
+        n = len(nums)
+        freq = [0] * 51
+
+        for x in nums:
+            freq[x] += 1
+
+        if k == n:
+            return max(nums)
+
+        if k == 1:
+            for x in range(50, -1, -1):
+                if freq[x] == 1:
+                    return x
+            return -1
+
+        ans = -1
+        if freq[nums[0]] == 1:
+            ans = max(ans, nums[0])
+        if freq[nums[-1]] == 1:
+            ans = max(ans, nums[-1])
+
+        return ans
