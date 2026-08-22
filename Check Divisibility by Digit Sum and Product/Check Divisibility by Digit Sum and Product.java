9bc9fbf146f82acf6e1e0d2797e21cfb47623584
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-22 19:36:20 +0530

    Update Check Divisibility by Digit Sum and Product.java

--- Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.java
+++ Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.java
@@ -1,1 +1,16 @@
 // Solution for Check Divisibility by Digit Sum and Product in JAVA
+
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int original = n, sum = 0, product = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+            sum += digit;
+            product *= digit;
+        }
+
+        return original % (sum + product) == 0;
+    }
+}
