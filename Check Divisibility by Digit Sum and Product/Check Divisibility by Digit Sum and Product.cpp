937cda9067d01d3c156d5d163bed6578faa7a197
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-22 19:36:30 +0530

    Update Check Divisibility by Digit Sum and Product.cpp

--- Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.cpp
+++ Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.cpp
@@ -1,1 +1,20 @@
 // Solution for Check Divisibility by Digit Sum and Product in CPP
+
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int digitSum = 0;
+        int digitProduct = 1;
+        int original = n;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            digitSum += digit;
+            digitProduct *= digit;
+        }
+
+        return original % (digitSum + digitProduct) == 0;
+    }
+};
