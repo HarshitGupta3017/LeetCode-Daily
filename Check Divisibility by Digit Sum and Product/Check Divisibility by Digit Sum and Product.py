e39e47b6e10deafb656b1beee5b1c45417799705
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-22 19:36:09 +0530

    Update Check Divisibility by Digit Sum and Product.py

--- Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.py
+++ Check Divisibility by Digit Sum and Product/Check Divisibility by Digit Sum and Product.py
@@ -1,1 +1,10 @@
 # Solution for Check Divisibility by Digit Sum and Product in PY
+
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        original, total, product = n, 0, 1
+        while n:
+            n, d = divmod(n, 10)
+            total += d
+            product *= d
+        return original % (total + product) == 0
