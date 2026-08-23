e40ee4ce92728529d2b020057e6782b72153fe53
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-23 16:31:12 +0530

    Update Sum Game.java

--- Sum Game/Sum Game.java
+++ Sum Game/Sum Game.java
@@ -1,1 +1,20 @@
 // Solution for Sum Game in JAVA
+
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length(), h = n / 2;
+        int ls = 0, rs = 0, lw = 0, rw = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                if (i < h) lw++; else rw++;
+            } else {
+                if (i < h) ls += num.charAt(i) - '0';
+                else rs += num.charAt(i) - '0';
+            }
+        }
+
+        if ((lw + rw) % 2 != 0) return true;
+        return 2 * (ls - rs) + 9 * (lw - rw) != 0;
+    }
+};
