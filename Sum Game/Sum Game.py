a7027eb8a3fe10cc8f5c7e380d4091fa2a000499
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-23 16:31:02 +0530

    Update Sum Game.py

--- Sum Game/Sum Game.py
+++ Sum Game/Sum Game.py
@@ -1,1 +1,20 @@
 # Solution for Sum Game in PY
+
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        h = len(num) // 2
+        ls = rs = lw = rw = 0
+
+        for i, c in enumerate(num):
+            if c == '?':
+                if i < h: lw += 1
+                else: rw += 1
+            elif i < h:
+                ls += int(c)
+            else:
+                rs += int(c)
+
+        if (lw + rw) % 2:
+            return True
+
+        return 2 * (ls - rs) + 9 * (lw - rw) != 0
