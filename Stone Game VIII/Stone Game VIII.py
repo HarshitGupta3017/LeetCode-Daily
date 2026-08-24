841bfcef0ae10c9d3baf09b11020de3c7de3c148
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-24 15:45:39 +0530

    Update Stone Game VIII.py

--- Stone Game VIII/Stone Game VIII.py
+++ Stone Game VIII/Stone Game VIII.py
@@ -1,1 +1,30 @@
 # Solution for Stone Game VIII in PY
+
+class Solution:
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+
+        # prefix[i] = sum of stones[0..i]
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+
+        # dp[i] = best score difference from position i onward
+        dp = [0] * n
+
+        # Base case
+        dp[n - 1] = prefix[n - 1]
+
+        for i in range(n - 2, 0, -1):
+            # Pick up to index i
+            pick_here = prefix[i] - dp[i + 1]
+
+            # Skip index i
+            skip_here = dp[i + 1]
+
+            dp[i] = max(pick_here, skip_here)
+
+        # First move must take at least 2 stones
+        return dp[1]
