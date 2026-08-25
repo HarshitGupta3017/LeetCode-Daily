b5aea916e8eb2092307c38689789afad55356be4
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-25 18:55:23 +0530

    Update Smallest Missing Multiple of K.cpp

--- Smallest Missing Multiple of K/Smallest Missing Multiple of K.cpp
+++ Smallest Missing Multiple of K/Smallest Missing Multiple of K.cpp
@@ -1,1 +1,16 @@
 // Solution for Smallest Missing Multiple of K in CPP
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> present(nums.begin(), nums.end());
+
+        // Scan multiples of k in ascending order (k, 2k, 3k, ...)
+        // and return the first one not found in the set.
+        int candidate = k;
+        while (present.count(candidate))
+            candidate += k;
+
+        return candidate;
+    }
+};
