b75f5e5cec9795d8dc63139df5bd96cc3ab5a40b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-25 18:54:36 +0530

    Update Smallest Missing Multiple of K.py

--- Smallest Missing Multiple of K/Smallest Missing Multiple of K.py
+++ Smallest Missing Multiple of K/Smallest Missing Multiple of K.py
@@ -1,1 +1,9 @@
 # Solution for Smallest Missing Multiple of K in PY
+
+class Solution:
+    def missingMultiple(self, nums: List[int], k: int) -> int:
+        s = set(nums)
+        candidate = k
+        while candidate in s:
+            candidate += k
+        return candidate
