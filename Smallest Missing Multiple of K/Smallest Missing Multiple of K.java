3fe676d9eea76d4552deba672cf68c792a23ea1b
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-25 18:55:11 +0530

    Update Smallest Missing Multiple of K.java

--- Smallest Missing Multiple of K/Smallest Missing Multiple of K.java
+++ Smallest Missing Multiple of K/Smallest Missing Multiple of K.java
@@ -1,1 +1,14 @@
 // Solution for Smallest Missing Multiple of K in JAVA
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int candidate = k;
+        while (set.contains(candidate))
+            candidate += k;
+
+        return candidate;
+    }
+}
