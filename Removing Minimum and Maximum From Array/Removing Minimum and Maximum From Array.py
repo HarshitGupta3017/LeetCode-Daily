c3489b5ae927fc69669c681dd7ac0ba24e28031c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-08-30 17:03:17 +0530

    Update Removing Minimum and Maximum From Array.py

--- Removing Minimum and Maximum From Array/Removing Minimum and Maximum From Array.py
+++ Removing Minimum and Maximum From Array/Removing Minimum and Maximum From Array.py
@@ -1,1 +1,11 @@
 # Solution for Removing Minimum and Maximum From Array in PY
+
+class Solution:
+    def minimumDeletions(self, nums):
+        n = len(nums)
+        a = nums.index(min(nums))
+        b = nums.index(max(nums))
+
+        l, r = min(a, b), max(a, b)
+
+        return min(r + 1, n - l, l + 1 + n - r)
