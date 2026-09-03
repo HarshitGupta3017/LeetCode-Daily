010886f9ddeef2046b64e01044e33467cdce8ef7
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-03 12:40:06 +0530

    Update Construct Uniform Parity Array II.py

--- Construct Uniform Parity Array II/Construct Uniform Parity Array II.py
+++ Construct Uniform Parity Array II/Construct Uniform Parity Array II.py
@@ -1,1 +1,14 @@
 # Solution for Construct Uniform Parity Array II in PY
+
+class Solution:
+    def uniformArray(self, nums1):
+        min_el = min(nums1)
+
+        if min_el % 2 == 1:
+            return True
+
+        for num in nums1:
+            if num % 2 == 1:
+                return False
+
+        return True
