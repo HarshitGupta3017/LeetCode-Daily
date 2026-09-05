c534ed0de1155b39b9c26b65cd6353e30e771ec1
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-05 11:53:35 +0530

    Update Smallest Stable Index II.java

--- Smallest Stable Index II/Smallest Stable Index II.java
+++ Smallest Stable Index II/Smallest Stable Index II.java
@@ -1,1 +1,36 @@
 // Solution for Smallest Stable Index II in JAVA
+
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        // suffixMinimum[i] = minimum value in nums[i..n-1]
+        int[] suffixMinimum = new int[n];
+
+        int currentMinimum = Integer.MAX_VALUE;
+
+        // Build suffix minimum from right to left.
+        for (int index = n - 1; index >= 0; --index) {
+            currentMinimum = Math.min(currentMinimum, nums[index]);
+            suffixMinimum[index] = currentMinimum;
+        }
+
+        // Maximum value in nums[0..index]
+        int currentMaximum = Integer.MIN_VALUE;
+
+        // Scan from left to right to find the first stable index.
+        for (int index = 0; index < n; index++) {
+            currentMaximum = Math.max(currentMaximum, nums[index]);
+
+            int suffixMinValue = suffixMinimum[index];
+
+            // Instability score:
+            // max(nums[0..index]) - min(nums[index..n-1])
+            if (currentMaximum - suffixMinValue <= k) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+}
