8df5fb4ffdd2df9354a06a3a7e7918e7467ce3eb
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-09 05:42:20 +0530

    Update Count Commas in Range II.java

--- Count Commas in Range II/Count Commas in Range II.java
+++ Count Commas in Range II/Count Commas in Range II.java
@@ -1,1 +1,15 @@
 // Solution for Count Commas in Range II in JAVA
+
+class Solution {
+    public long countCommas(long n) {
+        long totalCommas = 0;
+        long threshold = 1000;
+
+        while (threshold <= n) {
+            totalCommas += (n - threshold + 1);
+            threshold *= 1000;
+        }
+
+        return totalCommas;
+    }
+}
