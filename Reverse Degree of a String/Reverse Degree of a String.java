173cace2297dba1f9ad6819156e651cff34ffa1c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-20 15:30:53 +0530

    Update Reverse Degree of a String.java

--- Reverse Degree of a String/Reverse Degree of a String.java
+++ Reverse Degree of a String/Reverse Degree of a String.java
@@ -1,1 +1,13 @@
 // Solution for Reverse Degree of a String in JAVA
+
+class Solution {
+    public int reverseDegree(String s) {
+        int ans = 0;
+
+        for (int i = 1; i <= s.length(); i++) {
+            ans += (26 - (s.charAt(i - 1) - 'a')) * i;
+        }
+
+        return ans;
+    }
+}
