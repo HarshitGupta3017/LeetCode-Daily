7f0309884f23a105e6c5ee510e12996702db8021
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-20 15:30:38 +0530

    Update Reverse Degree of a String.py

--- Reverse Degree of a String/Reverse Degree of a String.py
+++ Reverse Degree of a String/Reverse Degree of a String.py
@@ -1,1 +1,10 @@
 # Solution for Reverse Degree of a String in PY
+
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        ans = 0
+
+        for i in range(1, len(s) + 1):
+            ans += (26 - (ord(s[i - 1]) - ord('a'))) * i
+
+        return ans
