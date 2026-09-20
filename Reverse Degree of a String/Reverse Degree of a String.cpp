937cc38d99362eda0704049b6d1a2357c5418c39
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-20 15:31:16 +0530

    Update Reverse Degree of a String.cpp

--- Reverse Degree of a String/Reverse Degree of a String.cpp
+++ Reverse Degree of a String/Reverse Degree of a String.cpp
@@ -1,1 +1,12 @@
 // Solution for Reverse Degree of a String in CPP
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for (int i = 1; i <= s.size(); i++) {
+            ans += (26 - (s[i - 1] - 'a')) * i;
+        }
+        return ans;
+    }
+};
