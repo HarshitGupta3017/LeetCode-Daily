cadb82c0dee0a33e765c9a2627d6ae7a90dce20c
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-24 17:48:26 +0530

    Update Smallest Index With Digit Sum Equal to Index.cpp

--- Smallest Index With Digit Sum Equal to Index/Smallest Index With Digit Sum Equal to Index.cpp
+++ Smallest Index With Digit Sum Equal to Index/Smallest Index With Digit Sum Equal to Index.cpp
@@ -1,1 +1,25 @@
 // Solution for Smallest Index With Digit Sum Equal to Index in CPP
+
+class Solution {
+public:
+    int smallestIndex(const vector<int>& nums) {
+        auto getDigitSum = [](int num) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        };
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (getDigitSum(nums[i]) == i) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
