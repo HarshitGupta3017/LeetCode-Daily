e68bda3da7ee2c799de83a3cbb25f7ddb2b8649e
Harshit Gupta <harshitgupta3017@gmail.com>
2026-09-24 17:48:04 +0530

    Update Smallest Index With Digit Sum Equal to Index.py

--- Smallest Index With Digit Sum Equal to Index/Smallest Index With Digit Sum Equal to Index.py
+++ Smallest Index With Digit Sum Equal to Index/Smallest Index With Digit Sum Equal to Index.py
@@ -1,1 +1,15 @@
 # Solution for Smallest Index With Digit Sum Equal to Index in PY
+
+class Solution:
+    def smallestIndex(self, nums):
+        for i, num in enumerate(nums):
+            digit_sum = 0
+
+            while num > 0:
+                digit_sum += num % 10
+                num //= 10
+
+            if digit_sum == i:
+                return i
+
+        return -1
